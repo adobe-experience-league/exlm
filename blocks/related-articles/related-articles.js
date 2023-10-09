6726fadd05602dea3e8bdcffe97248f326f7bce2
gauthamksn <gsnair@adobe.com>
2023-10-09 18:56:40 +0530

    remove chaining

--- blocks/related-articles/related-articles.js
+++ blocks/related-articles/related-articles.js
@@ -1,13 +1,12 @@
 export default function decorate(block) {
   const blockHeading = document.createElement('div');
   const linksContainer = document.createElement('ul');
-  linksContainer.classList.add('related-article-links-container');
+  linksContainer.classList.add('related-article-links');
 
   [...block.children].forEach((row, i) => {
     if (i === 0) blockHeading.innerHTML = row.textContent;
     else {
       const link = document.createElement('li');
-      link.classList.add('related-article-link');
       link.appendChild(row.querySelector('a'));
       linksContainer.appendChild(link);
     }
