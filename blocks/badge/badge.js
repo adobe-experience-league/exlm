c6aa4e622c5260dffa22f769f9ccb4cfc7f1d9cd
Noor Mohamed <nmohamme@adobe.com>
2023-10-13 18:30:42 +0530

    Added Target Attribute - Code cleanup

--- blocks/badge/badge.js
+++ blocks/badge/badge.js
@@ -2,31 +2,32 @@ export default function decorate(block) {
   const wrapper = block.closest('.badge-wrapper');
   const firstChildDiv = block.querySelector('div:nth-child(1) > div');
   const secondChildDiv = block.querySelector('div:nth-child(2) > div');
-  const anchorElement = firstChildDiv.querySelector('a');
+  const anchorElement = firstChildDiv?.querySelector('a');
   const classes = block.getAttribute('class');
 
-  let content = '';
+  try {
+    if (firstChildDiv) {
+      const firstDivContent = firstChildDiv.textContent.trim();
+      const titleAttribute = secondChildDiv
+        ? `title="${secondChildDiv.textContent.trim()}"`
+        : '';
 
-  if (firstChildDiv) {
-    const firstDivContent = firstChildDiv.textContent.trim();
-    const titleAttribute = secondChildDiv
-      ? `title="${secondChildDiv.textContent.trim()}"`
-      : '';
+      if (anchorElement) {
+        const href = anchorElement.getAttribute('href');
 
-    if (anchorElement) {
-      const href = anchorElement.getAttribute('href');
-      const key = '#_blank';
-      if (href.includes(key)) {
-        const parts = href.split(key);
-        const url = parts[0];
-        content = `<a href="${url}" class="${classes}" target="_blank" ${titleAttribute}>${firstDivContent}</a>`;
+        if (href) {
+          const key = '#_blank';
+          const url = href.includes(key) ? href.split(key)[0] : href;
+          wrapper.innerHTML = `<a href="${url}" class="${classes}" target="_blank" ${titleAttribute}>${firstDivContent}</a>`;
+        } else {
+          wrapper.innerHTML = `<div class="${classes}" ${titleAttribute}>${firstDivContent}</div>`;
+        }
       } else {
-        content = `<a href="${href}" class="${classes}" ${titleAttribute}>${firstDivContent}</a>`;
+        wrapper.innerHTML = `<div class="${classes}" ${titleAttribute}>${firstDivContent}</div>`;
       }
-    } else {
-      content = `<div class="${classes}" ${titleAttribute}>${firstDivContent}</div>`;
     }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error | Badge ::', error);
   }
-
-  wrapper.innerHTML = content;
 }
