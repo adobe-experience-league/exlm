e741f94a98135e924b1039325564dd90eb5e4f94
Thomas AS <thomas.jsm13@gmail.com>
2023-10-13 21:27:25 +0530

    typography to match exlm doc website

--- sass-compile.js
+++ sass-compile.js
@@ -7,7 +7,7 @@ import {
 } from './build/sass-util.js';
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
-const IGNORED_FILES = ['_breakpoints.scss']; // list of files to ignore from compilation
+const IGNORED_FILES = ['_breakpoints.scss', '_typography.scss']; // list of files to ignore from compilation
 const SASS_FOLDERS = ['styles', 'blocks']; // list of folders to compile
 
 // MAIN
