0f38e48370b2fc4c7a81327bac50bcb1f9f34e49
Noor Mohamed <nmohamme@adobe.com>
2023-10-23 11:46:10 +0530

    Footer Initial Changes

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,58 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
+function decorateMenu(footer) {  
+  const childElements = footer.querySelectorAll('.footer-item');
+  const groupDiv = document.createElement('div');
+  groupDiv.classList.add('footer-menu');
+  childElements.forEach((child) => {
+    const h2Elements = Array.from(child.querySelectorAll('h2'));
+    const ulElements = Array.from(child.querySelectorAll('ul'));
+    const divWrapper = document.createElement('div');
+    if (h2Elements.length > 0 && ulElements.length > 0) {
+      h2Elements.forEach((h2Element, index) => {
+        const divPair = document.createElement('div');
+        divPair.setAttribute('class', 'footer-item-list');
+        divPair.appendChild(h2Element);
+        divPair.appendChild(ulElements[index]);
+        divWrapper.appendChild(divPair);
+      });
+    }
+    child.innerHTML = divWrapper.innerHTML;
+    groupDiv.appendChild(child);
+    const hElements = Array.from(child.querySelectorAll('h2'));
+    hElements.forEach((hElement) => {
+      hElement.addEventListener('click', () => {
+        const footerMenuList = hElement.closest('.footer-item-list');
+        if (footerMenuList) {
+          if (footerMenuList.classList.contains('footer-item-active')) {
+            footerMenuList.classList.remove('footer-item-active');
+          }
+          else {
+            const menuList = Array.from(groupDiv.querySelectorAll('.footer-item-list'));
+            menuList.forEach((element) => {
+              element.classList.remove('footer-item-active');
+            });
+            footerMenuList.classList.add('footer-item-active');
+          }
+        }
+      });
+    });
+  });
+  const elem = footer.children[0];
+  elem.insertBefore(groupDiv, elem.children[1]);
+}
+
+function decorateSocial(footer) {
+  const languageSelector = footer.querySelector('.language-selector');
+  const social = footer.querySelector('.social');
+  const groupDiv = document.createElement('div');
+  groupDiv.classList.add('footer-lang-social');
+  groupDiv.appendChild(languageSelector);
+  groupDiv.appendChild(social);
+  const elem = footer.children[0];
+  elem.insertBefore(groupDiv, elem.children[2]);
+}
+
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
@@ -9,7 +62,7 @@ export default async function decorate(block) {
   block.textContent = '';
 
   // fetch footer content
-  const footerPath = cfg.footer || '/footer';
+  const footerPath = cfg.footer || 'http://127.0.0.1:5500/footer-new';
   const resp = await fetch(
     `${footerPath}.plain.html`,
     window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {},
@@ -21,7 +74,8 @@ export default async function decorate(block) {
     // decorate footer DOM
     const footer = document.createElement('div');
     footer.innerHTML = html;
-
+    decorateMenu(footer);
+    decorateSocial(footer);
     decorateIcons(footer);
     block.append(footer);
   }
