e065c803678a30cc21dcbb029ff63971b72a074d
msagolj <msagolj@adobe.com>
2023-11-02 17:38:42 +0100

    added all options to rendering

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,16 +1,21 @@
 export default function decorate(block) {
   // 1. extract properties (always same order as in model)
-  // const image = block.querySelector("div:nth-child(1) > div").innerHTML;
-  // const imageDescr = block.querySelector("div:nth-child(2) > div").textContent.trim();
+  const image = block.querySelector('div:nth-child(1) > div').innerHTML.trim();
+  const imageDescr = block
+    .querySelector('div:nth-child(2) > div')
+    .textContent.trim();
   const eyebrow = block
     .querySelector('div:nth-child(3) > div')
-    .textContent.trim();
+    .textContent.trim()
+    .toUpperCase();
   const title = block
     .querySelector('div:nth-child(4) > div')
     .textContent.trim();
   const longDescr = block.querySelector('div:nth-child(5) > div').innerHTML;
-  // const shortDescr = block.querySelector("div:nth-child(6) > div").innerHTML;
-  // const backgroundColor = block.querySelector("div:nth-child(7) > div").textContent.trim();
+  const shortDescr = block.querySelector('div:nth-child(6) > div').innerHTML;
+  const backgroundColor = block
+    .querySelector('div:nth-child(7) > div')
+    .textContent.trim();
   const firstCTAType = block
     .querySelector('div:nth-child(8) > div')
     .textContent.trim();
@@ -40,30 +45,46 @@ export default function decorate(block) {
   const teaserDOM = document.createRange().createContextualFragment(`
     <div class='teaser'>
       <div class='text'>
-        ${eyebrow ? `<div class='eyebrow'>${eyebrow}</div>` : ''}
-        <div class='title'>${title}</div>
-        <div class='description'>${longDescr}</div>
+        ${
+          eyebrow
+            ? `<div itemprop='eyebrow' itemtype='text' class='eyebrow'>${eyebrow}</div>`
+            : ''
+        }
+        <div itemprop='title' itemtype='text' class='title'>${title}</div>
+        <div itemprop='longDescr' itemtype='text' class='long-description'>${longDescr}</div>
+        <div itemprop='shortDescr' itemtype='text' class='short-description'>${shortDescr}</div>
         <div class='cta'>
           ${
-            (firstCTAText !== '') !== (firstCTALink !== '')
-              ? `button text or link missing!`
-              : `<button><a class='${firstCTAType}' ${
+            firstCTAText !== '' && firstCTALink !== ''
+              ? `<a itemprop='cta1Text' itemtype='text' class='button ${firstCTAType}' ${
                   firstCTANewTab === 'true' ? `target='_blank'` : ''
-                } href='${firstCTALink}'>${firstCTAText}</a></button>`
+                } 
+            href='${firstCTALink}'>${firstCTAText}</a>`
+              : ``
           }
           ${
             secondCTAText !== '' && secondCTALink !== ''
-              ? `<button><a class='${secondCTAType}' ${
+              ? `<a itemprop='cta2Text' itemtype='text' class='button ${secondCTAType}' ${
                   secondCTANewTab === 'true' ? `target='_blank'` : ''
-                } href='${secondCTALink}'>${secondCTAText}</a></button>`
+                } 
+           href='${secondCTALink}'>${secondCTAText}</a>`
               : ``
           }
         </div>
       </div>
       <div class='image'>
+          ${image}
       </div>
     </div>
   `);
+
+  // add the image description
+  teaserDOM.querySelector('.image picture img').setAttribute('alt', imageDescr);
+
+  // set the mobile background
+  teaserDOM
+    .querySelector('.teaser')
+    .style.setProperty('--teaser-background-color', `var(${backgroundColor})`);
 
   // 3. add DOM
   block.textContent = '';
