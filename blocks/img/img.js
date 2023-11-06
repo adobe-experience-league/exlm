9d40f0e887928a591a44eb3f00e3f472af295f0d
Nitesh Pandey <adobenitesh@gmail.com>
2023-11-06 14:38:35 +0530

    image-zoomable

--- blocks/img/img.js
+++ blocks/img/img.js
@@ -1,4 +1,73 @@
-export default function decorate(block) {
+import { createTag } from '../../scripts/scripts.js';
+
+function imgZoomable() {
+  const modalImage = Array.from(document?.querySelectorAll('.img.modal-image'));
+
+  // Functions to open and close a modal
+  function openModal(el) {
+    el.classList.add('is-active');
+  }
+
+  function closeModal(el) {
+    el.classList.remove('is-active');
+    el.remove();
+  }
+
+  function closeAllModals() {
+    (document?.querySelectorAll('.modal') || []).forEach((modal) => {
+      closeModal(modal);
+    });
+  }
+
+  function insertModalTemplate() {
+    if (!document.querySelector('.modal')) {
+      const modalTemplate = createTag(
+        'div',
+        { class: 'modal' },
+        `<div class="modal-background"></div><div class="modal-content"><div class="img-container"></div><span class="modal-close" aria-label="close"></span></div>`,
+      );
+      document.body.prepend(modalTemplate);
+    }
+  }
+
+  // Add a click event on images to open a specific modal
+  modalImage.forEach((img) => {
+    img.addEventListener('click', () => {
+      insertModalTemplate();
+      const modalContent = img.outerHTML;
+      const target = document?.querySelector('.modal');
+      const targetContent = target.querySelector('.img-container');
+      targetContent.innerHTML = modalContent;
+      openModal(target);
+
+      // Add a click event on various child elements to close the parent modal
+      const modalActions = document.querySelectorAll(
+        '.modal-background, .modal-close',
+      );
+      if (modalActions.length > 0) {
+        modalActions.forEach((close) => {
+          const closestModal = close.closest('.modal');
+          close.addEventListener('click', () => {
+            closeModal(closestModal);
+          });
+        });
+      }
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+      closeAllModals();
+    }
+  });
+}
+
+/**
+ * decorates the image element block
+ * @param {Element} block the image element
+ */
+export default async function decorate(block) {
   const img = block?.querySelector('img');
   if (!img) {
     return;
@@ -17,4 +86,6 @@ export default function decorate(block) {
       img.style.width = `${width}px`;
     }
   }
+
+  imgZoomable();
 }
