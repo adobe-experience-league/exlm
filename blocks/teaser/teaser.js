442f89eb8e4f183195daff43dc0b5263175e5039
msagolj <msagolj@adobe.com>
2023-11-07 15:24:03 +0100

    removed inline editing for rich text

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
   // 1. extract properties (always same order as in model)
-  const image = block.querySelector('div:nth-child(1) > div').innerHTML.trim();
+  const picture = block.querySelector('div:nth-child(1) > div picture');
   const imageDescr = block
     .querySelector('div:nth-child(2) > div')
     .textContent.trim();
@@ -45,18 +45,14 @@ export default function decorate(block) {
   const teaserDOM = document.createRange().createContextualFragment(`
     <div class='teaser'>
       <div class='text'>
-        ${
-          eyebrow
-            ? `<div itemprop='eyebrow' itemtype='text' class='eyebrow'>${eyebrow}</div>`
-            : ''
-        }
-        <div itemprop='title' itemtype='text' class='title'>${title}</div>
-        <div itemprop='longDescr' itemtype='text' class='long-description'>${longDescr}</div>
-        <div itemprop='shortDescr' itemtype='text' class='short-description'>${shortDescr}</div>
+        ${eyebrow ? `<div class='eyebrow'>${eyebrow}</div>` : ''}
+        <div class='title'>${title}</div>
+        <div class='long-description'>${longDescr}</div>
+        <div class='short-description'>${shortDescr}</div>
         <div class='cta'>
           ${
             firstCTAText !== '' && firstCTALink !== ''
-              ? `<a itemprop='cta1Text' itemtype='text' class='button ${firstCTAType}' ${
+              ? `<a class='button ${firstCTAType}' ${
                   firstCTANewTab === 'true' ? `target='_blank'` : ''
                 } 
             href='${firstCTALink}'>${firstCTAText}</a>`
@@ -64,7 +60,7 @@ export default function decorate(block) {
           }
           ${
             secondCTAText !== '' && secondCTALink !== ''
-              ? `<a itemprop='cta2Text' itemtype='text' class='button ${secondCTAType}' ${
+              ? `<a class='button ${secondCTAType}' ${
                   secondCTANewTab === 'true' ? `target='_blank'` : ''
                 } 
            href='${secondCTALink}'>${secondCTAText}</a>`
@@ -73,10 +69,12 @@ export default function decorate(block) {
         </div>
       </div>
       <div class='image'>
-          ${image}
       </div>
     </div>
   `);
+
+  // add image
+  teaserDOM.querySelector('.image').append(picture);
 
   // add the image description
   if (imageDescr) {
