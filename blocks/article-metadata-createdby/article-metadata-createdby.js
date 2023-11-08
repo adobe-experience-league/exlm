7cdcd9ad154c33e2190ea24ad939b3382e433333
Nitin <tac32570@adobe.com>
2023-11-08 18:48:02 +0530

    fix: css corrections & feedback changes EXLM-286 EXLM-293

--- blocks/article-metadata-createdby/article-metadata-createdby.js
+++ blocks/article-metadata-createdby/article-metadata-createdby.js
@@ -3,22 +3,15 @@ export default function decorate(block) {
 
   if (createdForUl) {
     createdForUl.classList.add('meta-created-for');
-    const { matches: isMobileRes } = window.matchMedia('(max-width: 768px)');
     const listItems = createdForUl.children;
     if (listItems?.length > 1) {
-      const wrapper = isMobileRes ? document.createElement('div') : null;
+      const wrapper = document.createElement('li');
+      wrapper.classList.add('meta-created-for-wrapper');
       for (let i = 1; i < listItems.length; ) {
         listItems[i].classList.add('meta-created-for-element');
-        if (wrapper) {
-          wrapper.appendChild(listItems[i]);
-        } else {
-          i += 1;
-        }
+        wrapper.appendChild(listItems[i]);
       }
-      if (wrapper) {
-        createdForUl.insertBefore(wrapper, listItems[1]);
-        wrapper.classList.add('meta-created-for-wrapper');
-      }
+      createdForUl.insertBefore(wrapper, listItems[1]);
     }
   }
 }
