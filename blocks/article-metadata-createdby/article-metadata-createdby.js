19d4093485ef2a3a4419cb262852639007b93ad4
Nitin <tac32570@adobe.com>
2023-11-08 10:26:53 +0530

    fix: article metadata mobile issues & hide mini-toc section on mobile EXLM-286, EXLM-293

--- blocks/article-metadata-createdby/article-metadata-createdby.js
+++ blocks/article-metadata-createdby/article-metadata-createdby.js
@@ -3,5 +3,22 @@ export default function decorate(block) {
 
   if (createdForUl) {
     createdForUl.classList.add('meta-created-for');
+    const { matches: isMobileRes } = window.matchMedia('(max-width: 768px)');
+    const listItems = createdForUl.children;
+    if (listItems?.length > 1) {
+      const wrapper = isMobileRes ? document.createElement('div') : null;
+      for (let i = 1; i < listItems.length; ) {
+        listItems[i].classList.add('meta-created-for-element');
+        if (wrapper) {
+          wrapper.appendChild(listItems[i]);
+        } else {
+          i += 1;
+        }
+      }
+      if (wrapper) {
+        createdForUl.insertBefore(wrapper, listItems[1]);
+        wrapper.classList.add('meta-created-for-wrapper');
+      }
+    }
   }
 }
