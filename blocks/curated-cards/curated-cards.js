20d9976770f509ce9a5b7de642c987c2f82fa618
Noor Mohamed <nmohamme@adobe.com>
2023-11-17 18:20:10 +0530

    Decoration Changes

--- blocks/curated-cards/curated-cards.js
+++ blocks/curated-cards/curated-cards.js
@@ -7,79 +7,85 @@ import API_COVEO from "../../scripts/constants.js";
  * @param {HTMLElement} block - The block of data to process.
  */
 export default async function decorate(block) {
-  // Extracting elements from the block
-  const headingElement = block.querySelector('div:nth-child(1) > div');
-  const toolTipElement = block.querySelector('div:nth-child(2) > div');
-  const linkTextElement = block.querySelector('div:nth-child(3) > div > a');
+	// Extracting elements from the block
+	const headingElement = block.querySelector('div:nth-child(1) > div');
+	const toolTipElement = block.querySelector('div:nth-child(2) > div');
+	const linkTextElement = block.querySelector('div:nth-child(3) > div > a');
+	const contentType = block.querySelector('div:nth-child(4) > div')?.textContent.trim();
 
-  // Creating header div
-  const headerDiv = document.createElement('div');
-  headerDiv.classList.add('curated-cards-header');
+	// Creating header div
+	const headerDiv = document.createElement('div');
+	headerDiv.classList.add('curated-cards-header');
 
-  // Creating title div
-  const titleDiv = document.createElement('div');
-  titleDiv.classList.add('curated-cards-title');
+	// Creating title div
+	const titleDiv = document.createElement('div');
+	titleDiv.classList.add('curated-cards-title');
 
-  // Creating and setting the title element
-  const title = document.createElement('h4');
-  title.textContent = headingElement?.textContent.trim();
-  titleDiv.appendChild(title);
+	// Creating and setting the title element
+	const title = document.createElement('h4');
+	title.textContent = headingElement?.textContent.trim();
+	titleDiv.appendChild(title);
 
-  // Creating tooltip div
-  const tooltipDiv = document.createElement('div');
-  tooltipDiv.classList.add('tooltip');
+	// Creating tooltip div
+	const tooltipDiv = document.createElement('div');
+	tooltipDiv.classList.add('tooltip');
 
-  // Creating tooltip icon
-  const tooltipIcon = document.createElement('span');
-  tooltipIcon.classList.add('icon');
-  tooltipIcon.classList.add('icon-info');
-  tooltipDiv.appendChild(tooltipIcon);
+	// Creating tooltip icon
+	const tooltipIcon = document.createElement('span');
+	tooltipIcon.classList.add('icon');
+	tooltipIcon.classList.add('icon-info');
+	tooltipDiv.appendChild(tooltipIcon);
 
-  // Creating span element for the tooltip text
-  const tooltipSpan = document.createElement('span');
-  tooltipSpan.classList.add('tooltip-text');
-  tooltipSpan.textContent = toolTipElement?.textContent.trim();
-  tooltipDiv.appendChild(tooltipSpan);
+	// Creating span element for the tooltip text
+	const tooltipSpan = document.createElement('span');
+	tooltipSpan.classList.add('tooltip-text');
+	tooltipSpan.textContent = toolTipElement?.textContent.trim();
+	tooltipDiv.appendChild(tooltipSpan);
 
-  // Appending tooltip div to title div
-  titleDiv.appendChild(tooltipDiv);
+	// Appending tooltip div to title div
+	titleDiv.appendChild(tooltipDiv);
 
-  // Creating view div
-  const viewDiv = document.createElement('div');
-  viewDiv.classList.add('curated-cards-view');
-  viewDiv.appendChild(linkTextElement);
+	// Creating view div
+	const viewDiv = document.createElement('div');
+	viewDiv.classList.add('curated-cards-view');
+	viewDiv.appendChild(linkTextElement);
 
-  // Appending title and view divs to header div
-  headerDiv.appendChild(titleDiv);
-  headerDiv.appendChild(viewDiv);
+	// Appending title and view divs to header div
+	headerDiv.appendChild(titleDiv);
+	headerDiv.appendChild(viewDiv);
 
-  // Creating content div
-  const contentDiv = document.createElement('div');
-  contentDiv.classList.add('curated-cards-content');
+	// Creating content div
+	const contentDiv = document.createElement('div');
+	contentDiv.classList.add('curated-cards-content');
 
-  // Clearing the block's content
-  block.innerHTML = '';
+	// Clearing the block's content
+	block.innerHTML = '';
 
-  // Appending header and content divs to the block
-  block.appendChild(headerDiv);
-  block.appendChild(contentDiv);
+	// Appending header and content divs to the block
+	block.appendChild(headerDiv);
+	block.appendChild(contentDiv);
 
-  decorateIcons(block);
+	decorateIcons(block);
 
-  // Defining data sources for CardsAbstraction
-  const dataSources = [{
-    name: API_COVEO.NAME,
-    url: API_COVEO.URL,
-    param: {
-      contentType: "course",
-      sort: "relevancy",
-    },
-  }];
+	if (contentType) {
+		var urlSearchParams = new URLSearchParams();
+		urlSearchParams.append('sortCriteria', 'relevancy');
+		urlSearchParams.append('facets', `[{"facetId":"@el_role","field":"el_role","type":"specific","injectionDepth":1000,"filterFacetCount":true,"currentValues":[],"numberOfValues":5,"freezeCurrentValues":false,"preventAutoSelect":false,"isFieldExpanded":false},{"facetId":"@el_contenttype","field":"el_contenttype","type":"hierarchical","injectionDepth":1000,"delimitingCharacter":"|","filterFacetCount":true,"basePath":[],"filterByBasePath":false,"currentValues":[{"value":"${contentType}","state":"selected","children":[],"retrieveChildren":true,"retrieveCount":5}],"preventAutoSelect":false,"numberOfValues":1,"isFieldExpanded":false},{"facetId":"el_product","field":"el_product","type":"hierarchical","injectionDepth":1000,"delimitingCharacter":"|","filterFacetCount":true,"basePath":[],"filterByBasePath":false,"currentValues":[],"preventAutoSelect":false,"numberOfValues":10000,"isFieldExpanded":false}]`);
+		urlSearchParams.append('numberOfResults', 4);
 
-  // Creating CardsAbstraction instance
-  const curatedCards = new CardsAbstraction(dataSources);
 
-  // Getting card data asynchronously
-  const data = await curatedCards.getCardData();
-  console.log(data);
+		// Defining data sources for CardsAbstraction
+		const dataSources = [{
+			name: API_COVEO.NAME,
+			url: API_COVEO.URL,
+			body: urlSearchParams
+		}];
+
+		// Creating CardsAbstraction instance
+		const curatedCards = new CardsAbstraction(dataSources);
+
+		// Getting card data asynchronously
+		const data = await curatedCards.getCardData();
+		console.log(data);
+	}
 }
