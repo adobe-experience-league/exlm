b4443dd78d4b015fae7ed0e723c70e0d8b4ccf8f
Nitin <tac32570@adobe.com>
2023-11-20 16:21:30 +0530

    feat: Cards UI exlm-391

--- blocks/curated-cards/curated-cards.js
+++ blocks/curated-cards/curated-cards.js
@@ -1,5 +1,5 @@
 import { decorateIcons } from '../../scripts/lib-franklin.js';
-import BrowseCardsCoveoSource from "../../scripts/browse-cards/BrowseCardsCoveoSource.js";
+import BrowseCardsCoveoSource from '../../scripts/browse-cards/BrowseCardsCoveoSource.js';
 import buildCard from '../../scripts/browseCard/browseCard.js';
 
 /**
@@ -7,78 +7,78 @@ import buildCard from '../../scripts/browseCard/browseCard.js';
  * @param {HTMLElement} block - The block of data to process.
  */
 export default async function decorate(block) {
-	// Extracting elements from the block
-	const headingElement = block.querySelector('div:nth-child(1) > div');
-	const toolTipElement = block.querySelector('div:nth-child(2) > div');
-	const linkTextElement = block.querySelector('div:nth-child(3) > div > a');
-	const contentType = block.querySelector('div:nth-child(4) > div')?.textContent.trim();
-	const noOfResults = 4;
+  // Extracting elements from the block
+  const headingElement = block.querySelector('div:nth-child(1) > div');
+  const toolTipElement = block.querySelector('div:nth-child(2) > div');
+  const linkTextElement = block.querySelector('div:nth-child(3) > div > a');
+  const contentType = block.querySelector('div:nth-child(4) > div')?.textContent.trim();
+  const noOfResults = 4;
 
-	// Creating header div
-	const headerDiv = document.createElement('div');
-	headerDiv.classList.add('curated-cards-header');
+  // Creating header div
+  const headerDiv = document.createElement('div');
+  headerDiv.classList.add('curated-cards-header');
 
-	// Creating title div
-	const titleDiv = document.createElement('div');
-	titleDiv.classList.add('curated-cards-title');
+  // Creating title div
+  const titleDiv = document.createElement('div');
+  titleDiv.classList.add('curated-cards-title');
 
-	// Creating and setting the title element
-	const title = document.createElement('h4');
-	title.textContent = headingElement?.textContent.trim();
-	titleDiv.appendChild(title);
+  // Creating and setting the title element
+  const title = document.createElement('h4');
+  title.textContent = headingElement?.textContent.trim();
+  titleDiv.appendChild(title);
 
-	// Creating tooltip div
-	const tooltipDiv = document.createElement('div');
-	tooltipDiv.classList.add('tooltip');
+  // Creating tooltip div
+  const tooltipDiv = document.createElement('div');
+  tooltipDiv.classList.add('tooltip');
 
-	// Creating tooltip icon
-	const tooltipIcon = document.createElement('span');
-	tooltipIcon.classList.add('icon');
-	tooltipIcon.classList.add('icon-info');
-	tooltipDiv.appendChild(tooltipIcon);
+  // Creating tooltip icon
+  const tooltipIcon = document.createElement('span');
+  tooltipIcon.classList.add('icon');
+  tooltipIcon.classList.add('icon-info');
+  tooltipDiv.appendChild(tooltipIcon);
 
-	// Creating span element for the tooltip text
-	const tooltipSpan = document.createElement('span');
-	tooltipSpan.classList.add('tooltip-text');
-	tooltipSpan.textContent = toolTipElement?.textContent.trim();
-	tooltipDiv.appendChild(tooltipSpan);
+  // Creating span element for the tooltip text
+  const tooltipSpan = document.createElement('span');
+  tooltipSpan.classList.add('tooltip-text');
+  tooltipSpan.textContent = toolTipElement?.textContent.trim();
+  tooltipDiv.appendChild(tooltipSpan);
 
-	// Appending tooltip div to title div
-	titleDiv.appendChild(tooltipDiv);
+  // Appending tooltip div to title div
+  titleDiv.appendChild(tooltipDiv);
 
-	// Creating view div
-	const viewDiv = document.createElement('div');
-	viewDiv.classList.add('curated-cards-view');
-	viewDiv.appendChild(linkTextElement);
+  // Creating view div
+  const viewDiv = document.createElement('div');
+  viewDiv.classList.add('curated-cards-view');
+  viewDiv.appendChild(linkTextElement);
 
-	// Appending title and view divs to header div
-	headerDiv.appendChild(titleDiv);
-	headerDiv.appendChild(viewDiv);
+  // Appending title and view divs to header div
+  headerDiv.appendChild(titleDiv);
+  headerDiv.appendChild(viewDiv);
 
-	// Creating content div
-	const contentDiv = document.createElement('div');
-	contentDiv.classList.add('curated-cards-content');
+  // Creating content div
+  const contentDiv = document.createElement('div');
+  contentDiv.classList.add('curated-cards-content');
 
-	// Clearing the block's content
-	block.innerHTML = '';
+  // Clearing the block's content
+  block.innerHTML = '';
 
-	// Appending header div to the block
-	block.appendChild(headerDiv);
+  // Appending header div to the block
+  block.appendChild(headerDiv);
 
-	const params = {
-		contentType,
-		noOfResults
-	}
-	const browseCards = new BrowseCardsCoveoSource(params);
-	const browseCardsContent = await browseCards.fetchBrowseCardsContent();
-	for (let i = 0; i < Math.min(noOfResults, browseCardsContent.length); i++) {
-		const cardData = browseCardsContent[i];
-		const cardDiv = document.createElement("div");
-		buildCard(cardDiv, cardData);
-		contentDiv.appendChild(cardDiv);
-	}
-	// Appending content divs to the block
-	block.appendChild(contentDiv);
+  const params = {
+    contentType,
+    noOfResults,
+  };
+  const browseCards = new BrowseCardsCoveoSource(params);
+  const browseCardsContent = await browseCards.fetchBrowseCardsContent();
+  for (let i = 0; i < Math.min(noOfResults, browseCardsContent.length); i += 1) {
+    const cardData = browseCardsContent[i];
+    const cardDiv = document.createElement('div');
+    buildCard(cardDiv, cardData);
+    contentDiv.appendChild(cardDiv);
+  }
+  // Appending content divs to the block
+  block.appendChild(contentDiv);
 
-	decorateIcons(block);
+  decorateIcons(block);
 }
