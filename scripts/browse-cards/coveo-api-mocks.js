e3d31c6951c8944c9ae6dab05f7393caa5b306e5
Noor Mohamed <nmohamme@adobe.com>
2023-11-20 16:41:23 +0530

    New Changes

---
+++ scripts/browse-cards/coveo-api-mocks.js
@@ -0,0 +1,10 @@
+const COVEO_API_MOCKS = {
+    "Course": "https://run.mocky.io/v3/c8b55a23-6abd-4f17-8f6d-53f298358031",
+    "Event": "https://run.mocky.io/v3/78901652-9b53-4b18-81a9-34eec0f59ab7",
+    "Community": "https://run.mocky.io/v3/3174a5ed-21b4-4691-a973-edb056608502",
+    "Documentation": "https://run.mocky.io/v3/11f21da4-c6ae-4366-9c17-d5549e6ff3a5",
+    "Tutorial": "https://run.mocky.io/v3/e010b048-2840-4a65-b27c-43d0e13c6fe1",
+    "Certification": "https://run.mocky.io/v3/f136125c-17d2-4d73-9cd3-643d1d525f71"//  Update the URL
+}
+
+export default COVEO_API_MOCKS;
