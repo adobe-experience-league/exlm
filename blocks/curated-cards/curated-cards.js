fc364370ea2e4379c508d7929012d3713b39e005
Nitin <tac32570@adobe.com>
2023-11-23 12:09:30 +0530

    feat: review change exlm-393

--- blocks/curated-cards/curated-cards.js
+++ blocks/curated-cards/curated-cards.js
@@ -2,6 +2,7 @@
 import { decorateIcons } from '../../scripts/lib-franklin.js';
 import BrowseCardsDelegate from '../../scripts/browse-card/browse-cards-delegate.js';
 import buildCard from '../../scripts/browse-card/browse-card.js';
+import { htmlToElement } from '../../scripts/scripts.js';
 
 /**
  * Decorate function to process and log the mapped data.
@@ -15,50 +16,20 @@ export default async function decorate(block) {
   const contentType = block.querySelector('div:nth-child(4) > div')?.textContent.trim();
   const noOfResults = 4;
 
-  // Creating header div
-  const headerDiv = document.createElement('div');
-  headerDiv.classList.add('curated-cards-header');
-
-  // Creating title div
-  const titleDiv = document.createElement('div');
-  titleDiv.classList.add('curated-cards-title');
-
-  // Creating and setting the title element
-  const title = document.createElement('h4');
-  title.textContent = headingElement?.textContent.trim();
-  titleDiv.appendChild(title);
-
-  // Creating tooltip div
-  const tooltipDiv = document.createElement('div');
-  tooltipDiv.classList.add('tooltip');
-
-  // Creating tooltip icon
-  const tooltipIcon = document.createElement('span');
-  tooltipIcon.classList.add('icon');
-  tooltipIcon.classList.add('icon-info');
-  tooltipDiv.appendChild(tooltipIcon);
-
-  // Creating span element for the tooltip text
-  const tooltipSpan = document.createElement('span');
-  tooltipSpan.classList.add('tooltip-text');
-  tooltipSpan.textContent = toolTipElement?.textContent.trim();
-  tooltipDiv.appendChild(tooltipSpan);
-
-  // Appending tooltip div to title div
-  titleDiv.appendChild(tooltipDiv);
-
-  // Creating view div
-  const viewDiv = document.createElement('div');
-  viewDiv.classList.add('curated-cards-view');
-  viewDiv.appendChild(linkTextElement);
-
-  // Appending title and view divs to header div
-  headerDiv.appendChild(titleDiv);
-  headerDiv.appendChild(viewDiv);
-
   // Clearing the block's content
   block.innerHTML = '';
 
+  const headerDiv = htmlToElement(`
+    <div class="curated-cards-header">
+      <div class="curated-cards-title">
+          <h4>${headingElement?.textContent.trim()}</h4>
+          <div class="tooltip">
+            <span class="icon icon-info"></span><span class="tooltip-text">${toolTipElement?.textContent.trim()}</span>
+          </div>
+      </div>
+      <div class="curated-cards-view">${linkTextElement?.outerHTML}</div>
+    </div>
+  `);
   // Appending header div to the block
   block.appendChild(headerDiv);
 
