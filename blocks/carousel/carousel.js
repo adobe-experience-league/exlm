a75000da9c3e00f5462fdbdb0257de0d3071aa87
Mirko Sagolj <37147400+msagolj@users.noreply.github.com>
2023-11-23 15:19:31 +0100

    Teaser updates based on new typo and spacing table in EXLM-9 (#170)

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,5 +1,25 @@
 import { generateTeaserDOM } from '../teaser/teaser.js';
 
+// callback for touch based scrolling event
+function updateButtons(entries) {
+  entries.forEach((entry) => {
+    // if panel has become > 60% visible
+    if (entry.isIntersecting) {
+      // get the buttons
+      const carouselButtons = entry.target.parentNode.parentNode.querySelector('.button-container');
+      // remove selected state from whatever button has it
+      [...carouselButtons.querySelectorAll(':scope button')].forEach((b) => b.classList.remove('selected'));
+      // add selected state to proper button
+      carouselButtons
+        .querySelector(`:scope button[data-panel='${entry.target.dataset.panel}']`)
+        .classList.add('selected');
+    }
+  });
+}
+
+// intersection observer for touch based scrolling detection
+const observer = new IntersectionObserver(updateButtons, { threshold: 0.6, rootMargin: '500px 0px' });
+
 export default function decorate(block) {
   // the panels container
   const panelContainer = document.createElement('div');
@@ -15,9 +35,11 @@ export default function decorate(block) {
   // loop through all teaser blocks
   [...panels].forEach((panel, i) => {
     // generate the teaser panel
-    const teaserDOM = generateTeaserDOM(panel.children);
+    const { teaserDOM, classes } = generateTeaserDOM(panel.children, true);
     panel.textContent = '';
     panel.classList.add('teaser', 'block');
+    classes.split(' ').map((c) => panel.classList.add(c));
+    panel.dataset.panel = `panel_${i}`;
     panel.append(teaserDOM);
     panelContainer.append(panel);
 
@@ -25,15 +47,14 @@ export default function decorate(block) {
     const button = document.createElement('button');
     buttonContainer.append(button);
     button.title = `Slide ${i + 1}`;
+    button.dataset.panel = `panel_${i}`;
     if (!i) button.classList.add('selected');
+
+    observer.observe(panel);
 
     // add event listener to button
     button.addEventListener('click', () => {
       panelContainer.scrollTo({ top: 0, left: panel.offsetLeft - panel.parentNode.offsetLeft, behavior: 'smooth' });
-
-      // updated button selected states
-      [...buttonContainer.children].forEach((b) => b.classList.remove('selected'));
-      button.classList.add('selected');
     });
   });
 
