67363fc4cda4f2ab97c5dd8dc53d4b4b8fe01657
Duy Nguyen <duy.phnguyen@gmail.com>
2023-11-30 20:35:37 +0100

    chore: test reload affinity

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -1,44 +1,41 @@
-import { decorateBlock, decorateButtons, decorateIcons, loadBlock } from './lib-franklin.js';
-
-const connectionPrefix = 'urn:aemconnection:';
+import {
+  decorateBlock, decorateButtons, decorateIcons, loadBlock,
+} from './lib-franklin.js';
 
 function handleEditorUpdate(event) {
-  const {
-    detail: { itemids },
-  } = event;
-  Promise.all(
-    itemids
-      .map((itemId) => document.querySelector(`[itemid="${itemId}"]`))
-      .map(async (element) => {
-        const block = element.closest('.block');
-        const blockItemId = block?.getAttribute('itemid');
-        if (block && blockItemId?.startsWith(connectionPrefix)) {
-          const path = blockItemId.substring(connectionPrefix.length);
-          const resp = await fetch(`${path}.html${window.location.search}`);
-          if (resp.ok) {
-            const text = await resp.text();
-            const newBlock = new DOMParser().parseFromString(text, 'text/html').body.firstElementChild;
-            // hide the new block, and insert it after the existing one
-            newBlock.style.display = 'none';
-            block.insertAdjacentElement('afterend', newBlock);
-            // decorate buttons and icons
-            decorateButtons(newBlock);
-            decorateIcons(newBlock);
-            // decorate and load the block
-            decorateBlock(newBlock);
-            await loadBlock(newBlock);
-            // remove the old block and show the new one
-            block.remove();
-            newBlock.style.display = null;
-            return Promise.resolve();
-          }
+  const { detail: { payload } } = event;
+  const updates = payload?.updates;
+  Promise.all(updates
+    .map(async (update) => {
+      const { itemid, content } = update;
+      const element = document.querySelector(`[itemid="${itemid}"]`);
+      const block = element.closest('.block');
+      const blockItemId = block?.getAttribute('itemid');
+      if (block && blockItemId?.startsWith('urn:aemconnection:')) {
+        const htmlContent = content?.html;
+        const main = new DOMParser().parseFromString(htmlContent, 'text/html');
+        const newBlock = main?.querySelector(`[itemid="${blockItemId}"]`);
+        if(newBlock) {
+          newBlock.style.display = 'none';
+          block.insertAdjacentElement('afterend', newBlock);
+          // decorate buttons and icons
+          decorateButtons(newBlock);
+          decorateIcons(newBlock);
+          // decorate and load the block
+          decorateBlock(newBlock);
+          await loadBlock(newBlock);
+          // remove the old block and show the new one
+          block.remove();
+          newBlock.style.display = 'unset';
+          return Promise.resolve();
         }
-        return Promise.reject();
-      }),
-  ).catch(() => {
-    // fallback to a full reload if any item could not be reloaded
-    window.location.reload();
-  });
+      }
+      return Promise.reject();
+    }))
+    .catch(() => {
+      // fallback to a full reload if any item could not be reloaded
+      window.location.reload();
+    });
 }
 
 document.addEventListener('editor-update', handleEditorUpdate);
