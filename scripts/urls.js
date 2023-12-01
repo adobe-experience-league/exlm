fdc8efa59396506e25d3e400572758489bdcbab2
DEVISREE RAVURI <dravuri@adobe.com>
2023-12-01 09:24:33 +0530

    EXLM 25 Updated URL

--- scripts/urls.js
+++ scripts/urls.js
@@ -1,3 +1,4 @@
 export const JWTTokenUrl = 'https://experienceleague.adobe.com/api/token?lang=en';
 export const coveoTokenUrl = 'https://experienceleague.adobe.com/api/coveo-token?lang=en';
 export const coveoSearchResultsUrl = 'https://platform.cloud.coveo.com/rest/search/v2';
+export const searchUrl = 'https://experienceleague.adobe.com/search.html';
