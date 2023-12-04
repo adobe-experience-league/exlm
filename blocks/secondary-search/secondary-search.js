d84ff683c5d36917ddf9ca8e5785c44b1a1b5f0d
gauthamksn <gsnair@adobe.com>
2023-12-05 01:24:24 +0530

    review changes

--- blocks/secondary-search/secondary-search.js
+++ blocks/secondary-search/secondary-search.js
@@ -1,41 +1,47 @@
 import { decorateIcons } from '../../scripts/lib-franklin.js';
+import { htmlToElement } from '../../scripts/scripts.js';
 import { redirectToSearchPage } from '../../scripts/search/search.js';
+
+function redirect(inputElement, event) {
+  event.preventDefault();
+  const SearchInputValue = inputElement?.value.trim();
+  redirectToSearchPage(SearchInputValue);
+}
 
 export default function decorate(block) {
   const heading = block.children[0].textContent.trim();
   const placeholder = block.children[1].textContent.trim();
 
   const search = `
+  <div>
     <h2><label for="secondary-search">${heading}</label></h2>
     <form role="search">
-        <button title="Search Icon" type="submit"><span class="icon icon-search"></button>
-        <input
-        autocomplete="off"
-        type="search"
-        role="searchbox"
-        id="secondary-search"
-        value=""
-        placeholder="${placeholder}" 
-        />
-  </form>`;
+      <button title="Search Icon" type="submit"><span class="icon icon-search"></button>
+      <input
+      autocomplete="off"
+      type="search"
+      role="searchbox"
+      id="secondary-search"
+      value=""
+      placeholder="${placeholder}" 
+      />
+    </form>
+  </div>`;
 
-  block.innerHTML = search;
+  block.innerHTML = '';
+  block.append(htmlToElement(search));
   decorateIcons(block);
 
   const searchInput = block.querySelector('#secondary-search');
   const searchIcon = block.querySelector('.icon-search');
 
   searchIcon?.addEventListener('click', (e) => {
-    e.preventDefault();
-    const SearchInputValue = searchInput?.value.trim();
-    redirectToSearchPage(SearchInputValue);
+    redirect(searchInput, e);
   });
 
   searchInput?.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
-      e.preventDefault();
-      const SearchInputValue = searchInput?.value.trim();
-      redirectToSearchPage(SearchInputValue);
+      redirect(searchInput, e);
     }
   });
 }
