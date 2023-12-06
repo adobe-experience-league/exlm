9f58fcd373b73649c3ae0f23bbb1760bd6daa962
Nitesh Pandey <147729087+adobenitesh@users.noreply.github.com>
2023-12-06 13:17:36 +0530

    feature/EXLM-175 (#200)
    * feature/EXLM-175
    
    * updateProfile
    
    * remove Reflect ref
    
    * remove reflect and adjust api calls
    
    check if meta is an empty object
    
    Revert "seems unnecessary"
    
    This reverts commit ac44a5e3b511c98b32b64bfa87fca6b869a8eed8.
    
    check if params has any enumerable keys
    
    * get-profile
    
    * code-cleanup
    
    * code-cleanup
    
    * remove-label-fix
    
    * remove-cached-key
    
    ---------
    
    Co-authored-by: Ahmed Musallam <musallam@Ahmeds-MacBook-Pro.local>
    Co-authored-by: Ahmed <musallam@adobe.com>

---
+++ blocks/doc-actions/doc-actions.js
@@ -0,0 +1,130 @@
+import { isDocPage } from '../../scripts/scripts.js';
+import loadJWT from '../../scripts/auth/jwt.js';
+import { adobeIMS, profile, updateProfile } from '../../scripts/data-service/profile.js';
+
+const CONFIG = {
+  BOOKMARK_SET: 'Success! This is bookmarked to your profile.',
+  BOOKMARK_UNSET: 'Success! This is no longer bookmarked to your profile.',
+  BOOKMARK_UNAUTH_LABEL: 'Sign-in to bookmark',
+  BOOKMARK_UNAUTH_TIPTEXT: 'Bookmark',
+  BOOKMARK_AUTH_LABEL_SET: 'Bookmark page',
+  BOOKMARK_AUTH_LABEL_REMOVE: 'Remove Bookmark',
+  BOOKMARK_AUTH_TIPTEXT: 'Bookmark',
+  NOTICE_LABEL: 'Copy link',
+  NOTICE_TIPTEXT: 'Copy link URL',
+  NOTICE_SET: 'URL copied',
+};
+
+const tooltipTemplate = (sel, label, tiptext) => {
+  const tooltipContent = `<div class="exl-tooltip">
+        <span class="icon ${sel}"></span>
+        <span class="exl-tooltip-label">${tiptext}</span></div>
+        <span class="exl-link-label">${label}</span>`;
+  return tooltipContent;
+};
+
+const noticeTemplate = (info) => {
+  const noticeContent = document.createElement('div');
+  noticeContent.className = 'exl-notice';
+  noticeContent.innerHTML = `<div class="icon-info"></div>
+        <div class="exl-notice-content">${info}</div>
+        <div class="icon-close"></div>`;
+  return noticeContent;
+};
+
+const sendNotice = (noticelabel) => {
+  const sendNoticeContent = noticeTemplate(noticelabel);
+  document.body.prepend(sendNoticeContent);
+  const isExlNotice = document.querySelector('.exl-notice');
+  if (isExlNotice) {
+    document.querySelector('.exl-notice .icon-close').addEventListener('click', () => {
+      isExlNotice.remove();
+    });
+
+    setTimeout(() => {
+      isExlNotice.remove();
+    }, 3000);
+  }
+};
+
+const isSignedIn = adobeIMS?.isSignedInUser();
+
+export function decorateBookmark(block) {
+  const id = ((document.querySelector('meta[name="id"]') || {}).content || '').trim();
+  const unAuthBookmark = document.createElement('div');
+  unAuthBookmark.className = 'bookmark';
+  unAuthBookmark.innerHTML = tooltipTemplate(
+    'bookmark-icon',
+    CONFIG.BOOKMARK_UNAUTH_TIPTEXT,
+    CONFIG.BOOKMARK_UNAUTH_LABEL,
+  );
+
+  const authBookmark = document.createElement('div');
+  authBookmark.className = 'bookmark auth';
+  authBookmark.innerHTML = tooltipTemplate(
+    'bookmark-icon',
+    CONFIG.BOOKMARK_AUTH_TIPTEXT,
+    CONFIG.BOOKMARK_AUTH_LABEL_SET,
+  );
+
+  if (isSignedIn) {
+    block.appendChild(authBookmark);
+    const bookmarkAuthed = block.querySelector('.bookmark.auth');
+    if (bookmarkAuthed) {
+      const bookmarkAuthedToolTipLabel = bookmarkAuthed.querySelector('.exl-tooltip-label');
+      const bookmarkAuthedToolTipIcon = bookmarkAuthed.querySelector('.icon.bookmark-icon');
+      if (id.length === 0) {
+        // eslint-disable-next-line
+        console.log('Hooking bookmark failed. No id present.');
+      } else {
+        loadJWT().then(async () => {
+          profile().then(async (data) => {
+            if (data.bookmarks.includes(id)) {
+              bookmarkAuthedToolTipIcon.classList.add('authed');
+              bookmarkAuthedToolTipLabel.innerHTML = CONFIG.BOOKMARK_AUTH_LABEL_REMOVE;
+            }
+          });
+
+          bookmarkAuthed.addEventListener('click', async () => {
+            if (bookmarkAuthedToolTipIcon.classList.contains('authed')) {
+              await updateProfile('bookmarks', id);
+              bookmarkAuthedToolTipLabel.innerHTML = CONFIG.BOOKMARK_AUTH_LABEL_SET;
+              bookmarkAuthedToolTipIcon.classList.remove('authed');
+              sendNotice(CONFIG.BOOKMARK_UNSET);
+            } else {
+              await updateProfile('bookmarks', id);
+              bookmarkAuthedToolTipLabel.innerHTML = CONFIG.BOOKMARK_AUTH_LABEL_REMOVE;
+              bookmarkAuthedToolTipIcon.classList.add('authed');
+              sendNotice(CONFIG.BOOKMARK_SET);
+            }
+          });
+        });
+      }
+    }
+  } else {
+    block.appendChild(unAuthBookmark);
+  }
+}
+
+export function decorateCopyLink(block) {
+  const copyLinkDivNode = document.createElement('div');
+  copyLinkDivNode.className = 'copy-link';
+  copyLinkDivNode.innerHTML = tooltipTemplate('copy-link-url', CONFIG.NOTICE_LABEL, CONFIG.NOTICE_TIPTEXT);
+
+  block.appendChild(copyLinkDivNode);
+  const copyLinkIcon = block.querySelector('.icon.copy-link-url');
+  if (copyLinkIcon) {
+    copyLinkIcon.addEventListener('click', (e) => {
+      e.preventDefault();
+      navigator.clipboard.writeText(window.location.href);
+      sendNotice(CONFIG.NOTICE_SET);
+    });
+  }
+}
+
+export default async function decorateDocActions(block) {
+  if (isDocPage) {
+    decorateBookmark(block);
+    decorateCopyLink(block);
+  }
+}
--- scripts/auth/session-keys.js
+++ scripts/auth/session-keys.js
@@ -1,2 +1,4 @@
 export const JWT = 'JWT';
 export const COVEO_TOKEN = 'coveoToken';
+export const Profile = 'profile';
+export const ProfileAttributes = 'attributes';
