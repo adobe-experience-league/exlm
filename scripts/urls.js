4aea9c7f9d283be65487c5c36e8b72061db2dfb1
DEVISREE RAVURI <dravuri@adobe.com>
2023-12-20 18:28:11 +0530

    ADLS Redirection Issue

--- scripts/urls.js
+++ scripts/urls.js
@@ -7,3 +7,4 @@ export const adlsUrl = 'https://learning.adobe.com/catalog.result.json';
 export const searchUrl = 'https://experienceleague.adobe.com/search.html';
 export const articleUrl = 'https://experienceleague.adobe.com/api/articles/';
 export const solutionsUrl = 'https://experienceleague.adobe.com/api/solutions?page_size=100';
+export const adlsRedirectUrl = 'https://learning.adobe.com';
