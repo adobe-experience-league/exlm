c7fbd17f4361870a4876d2133b1f3cbc0b1b20ed
Ruhi Singh <ruhis@adobe.com>
2024-01-05 18:29:38 +0530

    Code formatting

--- blocks/browse-topics/browse-topics.js
+++ blocks/browse-topics/browse-topics.js
@@ -1,6 +1,16 @@
 import { decorateIcons } from '../../scripts/lib-franklin.js';
 import { htmlToElement } from '../../scripts/scripts.js';
 
+function getTags(item) {
+  let actualTagBase64Encoded = '';
+  const lastIndex = item.lastIndexOf('/');
+  if (lastIndex !== -1) {
+    actualTagBase64Encoded = item.substring(lastIndex + 1);
+    return actualTagBase64Encoded;
+  }
+
+  return actualTagBase64Encoded;
+}
 
 /**
  * formattedTopicsTags returns the array of base64 encoded tags after extracting from the tags selected in dialog
@@ -9,29 +19,15 @@ import { htmlToElement } from '../../scripts/scripts.js';
  */
 function formattedTopicsTags(inputString) {
   let base64EncodedTagsArray = [];
-  if (inputString.includes(',')){
+  if (inputString.includes(',')) {
     const splitArray = inputString.split(',');
-     base64EncodedTagsArray = splitArray.map(function(item) {
+    base64EncodedTagsArray = splitArray.map((item) => {
       getTags(item);
-
     });
     return base64EncodedTagsArray;
-  } else{
-
-    return [getTags(inputString)];
   }
 
-}
-
-function getTags(item){
-  let actualTagBase64Encoded = '';
-  const lastIndex = item.lastIndexOf('/');
-                if (lastIndex !== -1) {
-                    actualTagBase64Encoded = item.substring(lastIndex + 1);
-                    return actualTagBase64Encoded;
-                }
-
-                return actualTagBase64Encoded;
+  return [getTags(inputString)];
 }
 
 export default async function decorate(block) {
@@ -39,7 +35,7 @@ export default async function decorate(block) {
   const headingElement = block.querySelector('div:nth-child(1) > div');
   const topics = block.querySelector('div:nth-child(2) > div').textContent.trim();
   const allTopicsTags = topics !== '' ? formattedTopicsTags(topics) : '';
-  console.log(allTopicsTags);
+  // console.log(allTopicsTags);
 
   // Clearing the block's content
   block.innerHTML = '';
@@ -59,15 +55,14 @@ export default async function decorate(block) {
   const contentDiv = document.createElement('div');
   contentDiv.classList.add('browse-topics-block-content');
 
-  allTopicsTags.forEach(function(topicsButtonTitle) {
-          const topicsButtonDiv = document.createElement("div");
-          topicsButtonDiv.classList.add('browse-topics');
-          topicsButtonDiv.classList.add('topic');
-          // decode tags here using atob
-          topicsButtonDiv.innerHTML = atob(topicsButtonTitle);
-          //click event goes here
-          contentDiv.appendChild(topicsButtonDiv);
-      });
-      block.appendChild(contentDiv);
-
+  allTopicsTags.forEach((topicsButtonTitle) => {
+    const topicsButtonDiv = document.createElement('div');
+    topicsButtonDiv.classList.add('browse-topics');
+    topicsButtonDiv.classList.add('topic');
+    // decode tags here using atob
+    topicsButtonDiv.innerHTML = atob(topicsButtonTitle);
+    // click event goes here
+    contentDiv.appendChild(topicsButtonDiv);
+  });
+  block.appendChild(contentDiv);
 }
