792194f1b76a693d3dd96983fcb643c04f8d1017
Your Name <you@example.com>
2024-01-05 18:08:01 +0530

    added browse topics block

---
+++ blocks/browse-topics/browse-topics.js
@@ -0,0 +1,73 @@
+import { decorateIcons } from '../../scripts/lib-franklin.js';
+import { htmlToElement } from '../../scripts/scripts.js';
+
+
+/**
+ * formattedTopicsTags returns the array of base64 encoded tags after extracting from the tags selected in dialog
+ * @param {string} inputString - The topics tag. E.g. exl-encoded:topic/QXBwIEJ1aWxkZXI=
+ * @returns the topic tag. E.g. QXBwIEJ1aWxkZXI=
+ */
+function formattedTopicsTags(inputString) {
+  let base64EncodedTagsArray = [];
+  if (inputString.includes(',')){
+    const splitArray = inputString.split(',');
+     base64EncodedTagsArray = splitArray.map(function(item) {
+      getTags(item);
+
+    });
+    return base64EncodedTagsArray;
+  } else{
+
+    return [getTags(inputString)];
+  }
+
+}
+
+function getTags(item){
+  let actualTagBase64Encoded = '';
+  const lastIndex = item.lastIndexOf('/');
+                if (lastIndex !== -1) {
+                    actualTagBase64Encoded = item.substring(lastIndex + 1);
+                    return actualTagBase64Encoded;
+                }
+
+                return actualTagBase64Encoded;
+}
+
+export default async function decorate(block) {
+  // Extracting elements from the block
+  const headingElement = block.querySelector('div:nth-child(1) > div');
+  const topics = block.querySelector('div:nth-child(2) > div').textContent.trim();
+  const allTopicsTags = topics !== '' ? formattedTopicsTags(topics) : '';
+  console.log(allTopicsTags);
+
+  // Clearing the block's content
+  block.innerHTML = '';
+  block.classList.add('browse-topics-block');
+
+  const headerDiv = htmlToElement(`
+    <div class="browse-topics-block-header">
+      <div class="browse-topics-block-title">
+          <h2>${headingElement?.textContent.trim()}</h2>
+      </div>
+    </div>
+  `);
+  // Appending header div to the block
+  block.appendChild(headerDiv);
+  await decorateIcons(headerDiv);
+
+  const contentDiv = document.createElement('div');
+  contentDiv.classList.add('browse-topics-block-content');
+
+  allTopicsTags.forEach(function(topicsButtonTitle) {
+          const topicsButtonDiv = document.createElement("div");
+          topicsButtonDiv.classList.add('browse-topics');
+          topicsButtonDiv.classList.add('topic');
+          // decode tags here using atob
+          topicsButtonDiv.innerHTML = atob(topicsButtonTitle);
+          //click event goes here
+          contentDiv.appendChild(topicsButtonDiv);
+      });
+      block.appendChild(contentDiv);
+
+}
