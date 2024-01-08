4eb948e508f85e44086abfae1b75d118d59c564b
Ruhi Singh <ruhis@adobe.com>
2024-01-08 12:29:55 +0530

    Updated atob function

--- blocks/browse-topics/browse-topics.js
+++ blocks/browse-topics/browse-topics.js
@@ -48,7 +48,7 @@ export default async function decorate(block) {
     topicsButtonDiv.classList.add('browse-topics');
     topicsButtonDiv.classList.add('topic');
     // decode tags here using atob
-    topicsButtonDiv.innerHTML = atob(topicsButtonTitle);
+    topicsButtonDiv.innerHTML = window.atob(topicsButtonTitle);
     // click event goes here
     contentDiv.appendChild(topicsButtonDiv);
   });
