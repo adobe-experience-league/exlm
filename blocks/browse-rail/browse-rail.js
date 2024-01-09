81b0f6b6fd3d366d3f9b5ccec22f64de1f29b6ad
Ruhi Singh <ruhis@adobe.com>
2024-01-09 19:23:43 +0530

    Added sub-pages to left rail

--- blocks/browse-rail/browse-rail.js
+++ blocks/browse-rail/browse-rail.js
@@ -48,53 +48,176 @@ function handleViewLessClick() {
   setLinkVisibility('viewLessLink', false);
 }
 
+// Function to check if current page has sub-pages
+function hasDirectLeafNodes(jsonData, currentPage) {
+  const directLeafNodes = jsonData.filter(
+    (item) =>
+      item.path.startsWith(currentPage) &&
+      item.path !== currentPage &&
+      !item.path.substring(currentPage.length + 1).includes('/'),
+  );
+
+  return directLeafNodes.length > 0;
+}
+
+function getPathUntilLevel(originalUrl, levels) {
+  const pathSegments = originalUrl.split('/');
+  const resultPath = pathSegments.slice(0, levels + 1).join('/');
+  return resultPath;
+}
+
+// Function to filter sub-pages under a given path
+const filterSubPages = (data, basePath) =>
+  data.filter((page) => page.path.startsWith(basePath) && page.path !== basePath);
+
+// Function to build a multi-map from the filtered sub-pages
+function convertToMultiMap(jsonData, page) {
+  const multiMap = new Map();
+
+  jsonData.forEach((item) => {
+    const pathSegments = item.path.split('/').filter((segment) => segment !== '');
+    const category = pathSegments[pathSegments.indexOf(page) + 1];
+    if (!multiMap.has(category)) {
+      multiMap.set(category, []);
+    }
+    multiMap.get(category).push(item);
+  });
+  return multiMap;
+}
+
+// Function to convert multi-map to nested list
+function convertToULList(multiMap) {
+  const ulList = document.createElement('ul');
+  ulList.classList.add('subPages');
+  multiMap.forEach((value) => {
+    const liItem = document.createElement('li');
+    const anchor = document.createElement('a');
+    if (value.length > 0) {
+      anchor.href = value[0].path;
+      anchor.textContent = value[0].title;
+
+      liItem.appendChild(anchor);
+
+      const subUlList = document.createElement('ul');
+
+      value.slice(1).forEach((item) => {
+        const subLiItem = document.createElement('li');
+        const subAnchor = document.createElement('a');
+        subAnchor.href = item.path;
+        subAnchor.textContent = item.title;
+
+        subLiItem.appendChild(subAnchor);
+        subUlList.appendChild(subLiItem);
+      });
+
+      liItem.appendChild(subUlList);
+    }
+
+    ulList.appendChild(liItem);
+  });
+
+  return ulList;
+}
+
 export default async function decorate(block) {
   const theme = getMetadata('theme');
   const label = getMetadata('og:title');
 
-  // Browse By
-  const browseByUL = document.createElement('ul');
-  browseByUL.classList.add('browse-by');
-  const browseByLI = document.createElement('li');
-  const browseByLinkText = theme === 'browse-all' ? `${label} content` : `All ${label} Content`;
-  browseByLI.innerHTML = `<a href="#">${placeholders.browseBy}</a><ul><li><a href="#">${browseByLinkText}</a></li></ul>`;
-  browseByUL.append(browseByLI);
+  const results = await ffetch('/browse-index.json').all();
+  const currentPagePath = window.location.pathname;
 
-  // Products
-  const productsUL = document.createElement('ul');
-  productsUL.classList.add('products');
-  const productsLI = document.createElement('li');
-  productsLI.innerHTML = `<a href="#">${placeholders.products}</a>`;
+  // For Browse All Page
+  if (theme === 'browse-all') {
+    // Browse By
+    const browseByUL = document.createElement('ul');
+    browseByUL.classList.add('browse-by');
+    const browseByLI = document.createElement('li');
+    const browseByLinkText = `${label} content`;
+    browseByLI.innerHTML = `<a href="#">${placeholders.browseBy}</a><ul><li><a href="#">${browseByLinkText}</a></li></ul>`;
+    browseByUL.append(browseByLI);
+    block.append(browseByUL);
 
-  const results = await ffetch('/query-index.json').all();
-  const ul = document.createElement('ul');
+    // Products
+    const directChildNodes = results.filter((item) => {
+      const pathParts = item.path.split('/');
+      return pathParts.length === 4 && pathParts[2] === currentPagePath.split('/')[2];
+    });
 
-  // Add each item to the list
-  results.forEach((item) => {
-    const li = createListItem(item);
-    ul.appendChild(li);
-  });
+    if (directChildNodes.length > 0) {
+      const productsUL = document.createElement('ul');
+      productsUL.classList.add('products');
+      const productsLI = document.createElement('li');
+      productsLI.innerHTML = `<a href="#">${placeholders.products}</a>`;
 
-  productsLI.append(ul);
-  productsUL.append(productsLI);
-  block.append(browseByUL, productsUL);
+      const ul = document.createElement('ul');
+      const sortedResults = directChildNodes.sort((a, b) => {
+        const titleA = a.title.toLowerCase();
+        const titleB = b.title.toLowerCase();
+        return titleA.localeCompare(titleB);
+      });
 
-  // "View More" and "View Less" links
-  const viewMoreDiv = document.createElement('div');
-  viewMoreDiv.innerHTML = `<a id="viewMoreLink">${placeholders.viewMore}</a>`;
-  block.append(viewMoreDiv);
+      Object.values(sortedResults).forEach((item) => {
+        const li = createListItem(item);
+        ul.appendChild(li);
+      });
 
-  const viewLessDiv = document.createElement('div');
-  viewLessDiv.innerHTML = `<a id="viewLessLink" style="display: none;">${placeholders.viewLess}</a>`;
-  block.append(viewLessDiv);
+      productsLI.append(ul);
+      productsUL.append(productsLI);
 
-  toggleItemVisibility(ul.children, 12, false);
+      block.append(productsUL);
+      // Check if there are less than 12 items, and hide the "View More" link accordingly
+      if (ul.children.length <= 12) {
+        document.getElementById('viewMoreLink').style.display = 'none';
+      }
 
-  // Event listeners for "View More" and "View Less" links
-  document.getElementById('viewMoreLink').addEventListener('click', handleViewMoreClick);
-  document.getElementById('viewLessLink').addEventListener('click', handleViewLessClick);
+      toggleItemVisibility(ul.children, 12, false);
 
+      // "View More" and "View Less" links
+      const viewMoreDiv = document.createElement('div');
+      viewMoreDiv.innerHTML = `<a id="viewMoreLink">${placeholders.viewMore}</a>`;
+      block.append(viewMoreDiv);
+
+      const viewLessDiv = document.createElement('div');
+      viewLessDiv.innerHTML = `<a id="viewLessLink" style="display: none;">${placeholders.viewLess}</a>`;
+      block.append(viewLessDiv);
+
+      // Event listeners for "View More" and "View Less" links
+      document.getElementById('viewMoreLink').addEventListener('click', handleViewMoreClick);
+      document.getElementById('viewLessLink').addEventListener('click', handleViewLessClick);
+    }
+  }
+
+  // For Browse Product Page
   if (theme !== 'browse-all') {
+    // Browse By
+    const browseByUL = document.createElement('ul');
+    browseByUL.classList.add('browse-by');
+    const browseByLI = document.createElement('li');
+    const browseByLinkText = `All ${label} Content`;
+    browseByLI.innerHTML = `<a href="#">${placeholders.browseBy}</a><ul><li><a href="#">${browseByLinkText}</a></li></ul>`;
+    browseByUL.append(browseByLI);
+    block.append(browseByUL);
+
+    // Products/Sub-pages
+
+    const result = hasDirectLeafNodes(results, currentPagePath);
+    if (result) {
+      const subPages = filterSubPages(results, currentPagePath);
+      const resultMultiMap = convertToMultiMap(subPages, currentPagePath.split('/')[3]);
+      const htmlList = convertToULList(resultMultiMap);
+      block.appendChild(htmlList);
+    }
+
+    // For sub-pages
+    const parts = currentPagePath.split('/');
+    if (parts.length > 5 && parts[3] === currentPagePath.split('/')[3]) {
+      const pagePath = getPathUntilLevel(currentPagePath, 3);
+      const subPages = filterSubPages(results, pagePath);
+      const resultMultiMap = convertToMultiMap(subPages, currentPagePath.split('/')[3]);
+      const htmlList = convertToULList(resultMultiMap);
+      block.appendChild(htmlList);
+    }
+
     // Topics
     const browseTopicsContainer = document.querySelector('.browse-topics-container');
     if (browseTopicsContainer !== null) {
@@ -119,6 +242,7 @@ export default async function decorate(block) {
       topicsUL.append(topicsLI);
       block.append(topicsUL);
     }
+
     // Add "Browse more products" link
     const browseMoreProducts = document.createElement('div');
     browseMoreProducts.classList.add('browse-more-products');
