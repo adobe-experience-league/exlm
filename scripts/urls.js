607f8a143def9c1309711ab54483f22bef406cc1
Noor Mohamed <nmohamme@adobe.com>
2024-01-09 17:44:12 +0530

    Code Cleanup

--- scripts/urls.js
+++ scripts/urls.js
@@ -1,6 +1,7 @@
-export const profileUrl = 'https://experienceleague.adobe.com/api/profile?lang=en';
-export const JWTTokenUrl = 'https://experienceleague.adobe.com/api/token?lang=en';
-export const coveoTokenUrl = 'https://experienceleague.adobe.com/api/coveo-token?lang=en';
+const lang = document.querySelector('html').lang || 'en';
+export const profileUrl = `https://experienceleague.adobe.com/api/profile?lang=${lang}`;
+export const JWTTokenUrl = `https://experienceleague.adobe.com/api/token?lang=${lang}`;
+export const coveoTokenUrl = `https://experienceleague.adobe.com/api/coveo-token?lang=${lang}`;
 export const coveoSearchResultsUrl = 'https://adobesystemsincorporatednonprod1.org.coveo.com/rest/search/v2';
 export const liveEventsUrl = 'https://cdn.experienceleague.adobe.com/thumb/upcoming-events.json';
 export const adlsUrl = 'https://learning.adobe.com/catalog.result.json';
