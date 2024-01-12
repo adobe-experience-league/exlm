48e008f9a8d0caf9ef61d71b70e50f119318c818
Ruhi Singh <ruhis@adobe.com>
2024-01-12 16:46:59 +0530

    Updated topics logic

--- blocks/browse-rail/browse-rail.js
+++ blocks/browse-rail/browse-rail.js
@@ -195,7 +195,8 @@ export default async function decorate(block) {
     if (browseTopicsContainer !== null) {
       const ulElement = document.createElement('ul');
       // Get all the topic elements inside the container
-      const topicElements = browseTopicsContainer.querySelectorAll('.browse-topics.topic');
+      const browseTopicsContent = document.querySelector('.browse-topics-block-content');
+      const topicElements = browseTopicsContent.querySelectorAll('.browse-topics.browse-topics-item');
       if (topicElements.length > 0) {
         topicElements.forEach((topicElement) => {
           const style = getComputedStyle(topicElement);
