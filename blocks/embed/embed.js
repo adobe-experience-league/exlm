18abfb616b63f7833e56569b1ffaf74b6a4e2c24
Noor Mohamed <nmohamme@adobe.com>
2024-01-16 12:37:50 +0530

    LHS Check

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -70,7 +70,9 @@ export default function decorate(block) {
     const observer = new IntersectionObserver((entries) => {
       if (entries.some((e) => e.isIntersecting)) {
         observer.disconnect();
-        loadEmbed(block, link);
+        setTimeout(() => {
+          loadEmbed(block, link);
+        }, 10);        
       }
     });
     observer.observe(block);
