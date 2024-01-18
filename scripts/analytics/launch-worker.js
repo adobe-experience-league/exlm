fc3c0116897f537b37698f97ee71ad75ce376065
roger blanton <blanton@adobe.com>
2024-01-18 10:04:16 -0500

    removing web worker

--- scripts/analytics/launch-worker.js
+++
@@ -1,7 +0,0 @@
-// Message handler for the web worker
-self.onmessage = function (event) {
-  if (event.data === 'loadAdobeLaunch') {
-    // Set up a callback for when the script is loaded
-    self.postMessage('adobeLaunchLoaded');
-  }
-};
