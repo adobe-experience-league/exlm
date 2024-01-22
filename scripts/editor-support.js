69bd1e54858d338e15e8e58a048c7344bb27b43a
Duy Nguyen <duy.phnguyen@gmail.com>
2024-01-22 19:33:18 +0100

    chore: refactor

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -22,36 +22,36 @@ function handleEditorUpdate(event) {
   const resource = detail?.requestData?.target?.resource;
   if (!resource) return;
 
+  const element = document.querySelector(`[data-aue-resource="${resource}"]`);
+  const block = element?.closest('.block');
+  const blockResource = block?.getAttribute('data-aue-resource');
+  if (!block || !blockResource?.startsWith(connectionPrefix)) return;
+
   const updates = detail?.responseData?.updates;
   Promise.all(updates
     .map(async (update) => {
       const { content } = update;
-      const element = document.querySelector(`[data-aue-resource="${resource}"]`);
-      const block = element?.closest('.block');
-      const blockResource = block?.getAttribute('data-aue-resource');
-      if (block && blockResource?.startsWith(connectionPrefix)) {
-        const newBlockDocument = new DOMParser().parseFromString(content, 'text/html');
-        const newBlock = newBlockDocument?.querySelector(`[data-aue-resource="${blockResource}"]`);
-        if(newBlock) {
-          // keep info about currently selected tab
-          const activeTabId = block.classList.contains('tabs') ? getSelectedTab(block) : null;
+      const newBlockDocument = new DOMParser().parseFromString(content, 'text/html');
+      const newBlock = newBlockDocument?.querySelector(`[data-aue-resource="${blockResource}"]`);
+      if(newBlock) {
+        // keep info about currently selected tab
+        const activeTabId = block.classList.contains('tabs') ? getSelectedTab(block) : null;
 
-          newBlock.style.display = 'none';
-          block.insertAdjacentElement('afterend', newBlock);
-          // decorate buttons and icons
-          decorateButtons(newBlock);
-          decorateIcons(newBlock);
-          // decorate and load the block
-          decorateBlock(newBlock);
-          await loadBlock(newBlock);
-          // remove the old block and show the new one
-          block.remove();
-          newBlock.style.display = null;
+        newBlock.style.display = 'none';
+        block.insertAdjacentElement('afterend', newBlock);
+        // decorate buttons and icons
+        decorateButtons(newBlock);
+        decorateIcons(newBlock);
+        // decorate and load the block
+        decorateBlock(newBlock);
+        await loadBlock(newBlock);
+        // remove the old block and show the new one
+        block.remove();
+        newBlock.style.display = null;
 
-          if (activeTabId) setSelectedTab(activeTabId, newBlock);
+        if (activeTabId) setSelectedTab(activeTabId, newBlock);
 
-          return Promise.resolve();
-        }
+        return Promise.resolve();
       }
       return Promise.reject();
     }))
