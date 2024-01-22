bafa314c419f3fbb8612569ffc0591e0c692c04b
Dirk <drudolph@adobe.com>
2024-01-22 14:38:15 +0100

    fix: update teaser.js to new content model

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -4,19 +4,14 @@ export function generateTeaserDOM(props, isChild) {
   let classes = null;
   let count = 0;
   const picture = props[count++].innerHTML.trim();
-  const imageDescr = props[count++].textContent.trim();
   if (isChild) classes = props[count++].textContent.trim();
   const eyebrow = props[count++].textContent.trim();
   const title = props[count++].textContent.trim();
   const longDescr = props[count++].innerHTML.trim();
   const shortDescr = props[count++];
   const backgroundColor = props[count++].textContent.trim();
-  const firstCTAType = props[count++].textContent.trim();
-  const firstCTAText = props[count++].textContent.trim();
-  const firstCTALink = props[count++].textContent.trim();
-  const secondCTAType = props[count++].textContent.trim();
-  const secondCTAText = props[count++].textContent.trim();
-  const secondCTALink = props[count++].textContent.trim();
+  const firstCTA = props[count++].firstElementChild;
+  const secondCTA = props[count++].firstElementChild;
 
   // Build DOM
   const teaserDOM = document.createRange().createContextualFragment(`
@@ -29,15 +24,10 @@ export function generateTeaserDOM(props, isChild) {
         <div class='short-description'>${
           shortDescr.textContent.trim() !== '' ? shortDescr.innerHTML.trim() : longDescr
         }</div>
-        <div class='cta'>${
-          firstCTAText && firstCTALink
-            ? `<a class='button ${firstCTAType}' href='${firstCTALink}'>${firstCTAText}</a>`
-            : ``
-        }${
-          secondCTAText && secondCTALink
-            ? `<a class='button ${secondCTAType}' href='${secondCTALink}'>${secondCTAText}</a>`
-            : ``
-        }</div>
+        <div class='cta'>
+          ${firstCTA ? firstCTA.outerHTML : ``}
+          ${secondCTA ? secondCTA.outerHTML : ``}
+        </div>
       </div>
       <div class='spacer'>
       </div>
