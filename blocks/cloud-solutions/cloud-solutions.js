a503cc7adab79595709d078780130f587c32462d
Nitin <tac32570@adobe.com>
2024-01-24 15:16:30 +0530

    feat: solution docs page ui exlm-889

---
+++ blocks/cloud-solutions/cloud-solutions.js
@@ -0,0 +1,40 @@
+import { createTag } from '../../scripts/scripts.js';
+
+export default async function decorate(block) {
+  const wrapperEl = block.querySelector('h2').parentElement;
+  wrapperEl.classList.add('cloud-solutions-wrapper');
+  while (wrapperEl.querySelector(':scope > h3') !== null) {
+    const header = wrapperEl.querySelector(':scope > h3');
+    const contents = wrapperEl.querySelector(':scope > ul');
+    const container = createTag('div', { class: 'cloud-solutions-list-container' });
+    container.appendChild(header);
+    container.appendChild(contents);
+    const target = wrapperEl.querySelector(':scope > ul')?.previousElementSibling;
+    if (target) {
+      wrapperEl.insertBefore(container, target);
+    } else {
+      wrapperEl.appendChild(container);
+    }
+  }
+
+  while (wrapperEl.querySelector(':scope > h2') !== null) {
+    const header = wrapperEl.querySelector(':scope > h2');
+    const contents = [header.nextElementSibling];
+    while (contents[contents.length - 1]?.nextElementSibling?.classList?.contains('cloud-solutions-list-container')) {
+      contents.push(contents[contents.length - 1]?.nextElementSibling);
+    }
+    const container = createTag('div', { class: 'cloud-solutions-list' });
+    const listWrapper = createTag('div', { class: 'cloud-solutions-list-wrapper' });
+    container.appendChild(header);
+    contents.forEach((content) => {
+      listWrapper.appendChild(content);
+    });
+    container.appendChild(listWrapper);
+    const target = wrapperEl.querySelector(':scope > h2');
+    if (target) {
+      wrapperEl.insertBefore(container, target);
+    } else {
+      wrapperEl.appendChild(container);
+    }
+  }
+}
---
+++ blocks/related-resources/related-resources.js
@@ -0,0 +1,17 @@
+export default async function decorate(block) {;
+  const containerEl = block.querySelector('ul');
+  const wrapperEl = containerEl?.parentElement;
+  if (!wrapperEl) {
+    return;
+  }
+  const blockParent = block.parentElement;
+  wrapperEl.classList.add('related-resources-wrap');
+  containerEl.classList.add('related-resources-container');
+  const headerElement = blockParent?.previousElementSibling?.querySelector('h2');
+  if (headerElement) {
+    headerElement.classList.add('related-resources-header');
+    if (headerElement.parentElement) {
+      headerElement.parentElement.classList.add('related-resources-header-wrap');
+    }
+  }
+}
