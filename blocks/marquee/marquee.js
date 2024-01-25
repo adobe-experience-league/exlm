ab8699d8908c742244e4d28a9b9522657d1b3caf
msagolj <msagolj@adobe.com>
2024-01-25 18:44:11 +0100

    fix for exlm-979

--- blocks/marquee/marquee.js
+++ blocks/marquee/marquee.js
@@ -19,13 +19,25 @@ function decorateButtons(...buttons) {
     .join('');
 }
 
+function getSignInButton(signInText) {
+  const secondCta = document.createElement('div');
+  const link = document.createElement('a');
+  link.classList.add('signin');
+  link.setAttribute('href', '#')
+  link.setAttribute('title', signInText);
+  link.textContent = signInText;
+  secondCta.append(link);
+  return secondCta;
+}
+
 export default async function decorate(block) {
   // Extract properties
   // always same order as in model, empty string if not set
-  const [img, eyebrow, title, longDescr, firstCta, secondCta] = block.querySelectorAll(':scope div > div');
+  const [img, eyebrow, title, longDescr, firstCta, confSignInText] = block.querySelectorAll(':scope div > div');
   const subjectPicture = img.querySelector('picture');
   const bgColorCls = [...block.classList].find((cls) => cls.startsWith('bg-'));
   const bgColor = bgColorCls ? `--${bgColorCls.substr(3)}` : '--spectrum-gray-700';
+  const signInText = confSignInText.textContent.trim();
 
   // get signed in status
   try {
@@ -35,6 +47,9 @@ export default async function decorate(block) {
     console.warn('Adobe IMS not available.');
   }
   const isSignedIn = window.adobeIMS?.isSignedInUser();
+
+  // build sign in button if not in yet and button text is set
+  const secondCta = signInText && !isSignedIn ? getSignInButton(signInText):null;
 
   // Build DOM
   const marqueeDOM = document.createRange().createContextualFragment(`
@@ -47,7 +62,7 @@ export default async function decorate(block) {
         }
         <div class='marquee-title'>${title.innerHTML}</div>
         <div class='marquee-long-description'>${longDescr.innerHTML}</div>
-        <div class='marquee-cta'>${decorateButtons(firstCta, !isSignedIn && secondCta)}</div>
+        <div class='marquee-cta'>${decorateButtons(firstCta, secondCta)}</div>
       </div>
       ${
         subjectPicture
@@ -60,6 +75,13 @@ export default async function decorate(block) {
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1562 571.212"><path fill="#fff" d="M0 1.212h1562v570H0z" data-name="Rectangle 1"></path><path class="bg" fill="var(${bgColor})" d="M752.813-1495s115.146 210.072 471.053 309.516 291.355 261.7 291.355 261.7h150.039V-1495Z" data-name="Path 1" transform="translate(-103.26 1495)"></path></svg>
     </div>
   `);
+
+  // add sign in event handler for sign in if set
+  if (signInText && !isSignedIn) {
+    marqueeDOM.querySelector('.signin').addEventListener('click', async () => {
+      window.adobeIMS.signIn();
+    });
+  }
 
   block.textContent = '';
   if (!subjectPicture) {
