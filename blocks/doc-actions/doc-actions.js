b4646a39bf863c4b9c37b652d2c6610aba6d9db6
Gali Andal <gandal@adobe.com>
2024-01-29 17:41:22 -0500

    exlm-762: Unexpected string concatenation  prefer-template

--- blocks/doc-actions/doc-actions.js
+++ blocks/doc-actions/doc-actions.js
@@ -120,7 +120,7 @@ function decorateCopyLink(block) {
 }
 
 async function getTranslatedDocContent() {
-  const docPath = '/en/' + window.location.pathname.slice(4);
+  const docPath = `/en/${window.location.pathname.slice(4)}`;
   const docResponse = await fetch(`${docPath}.plain.html`);
   const translatedDoc = await docResponse.text();
   const docElement = htmlToElement(`<div>${translatedDoc}</div>`);
