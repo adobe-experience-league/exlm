d1c96a90184630ae5246fc6fd8fbaebf7a47e486
Nitin <tac32570@adobe.com>
2024-01-31 18:46:53 +0530

    feat: solution page block updates exlm-889

---
+++ blocks/list/list.js
@@ -0,0 +1,16 @@
+export default async function decorate(block) {
+  const containerEl = block.querySelector('ul');
+  const wrapperEl = containerEl?.parentElement;
+  if (!wrapperEl) {
+    return;
+  }
+  wrapperEl.classList.add('list-box');
+  containerEl.classList.add('list-container');
+  const headerElement = block.parentElement?.previousElementSibling?.querySelector('h2');
+  if (headerElement) {
+    headerElement.classList.add('list-header');
+    if (headerElement.parentElement) {
+      headerElement.parentElement.classList.add('list-header-box');
+    }
+  }
+}
