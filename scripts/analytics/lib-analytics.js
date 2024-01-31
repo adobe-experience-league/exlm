6bfa2aee13d15d521eb1ac8b76889ee119dc8b45
roger blanton <blanton@adobe.com>
2024-01-31 11:00:43 -0500

    adding logic for span buttons and href lookup

--- scripts/analytics/lib-analytics.js
+++ scripts/analytics/lib-analytics.js
@@ -138,7 +138,7 @@ export function linkClickModel(e) {
 
   let linkType = 'other';
 
-  if (e.target.href.match(/.(pdf|zip|dmg|exe)$/)) {
+  if (!viewMoreLess && e.target.href.match(/.(pdf|zip|dmg|exe)$/)) {
     linkType = 'download';
   } else if (viewMoreLess) {
     linkType = 'view more/less';
