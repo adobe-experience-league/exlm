94c5ae5e074b5a1baa8c27b494b04d47ac09a4fe
DEVISREE RAVURI <dravuri@adobe.com>
2024-02-08 00:31:00 +0530

    EXLM 1044 Feat Card Req Changes

--- blocks/featured-cards/featured-cards.js
+++ blocks/featured-cards/featured-cards.js
@@ -18,9 +18,8 @@ const DEFAULT_OPTIONS = Object.freeze({
  */
 export default async function decorate(block) {
   // Extracting elements from the block
-  const [headingElement, descriptionElement, contentType, linkTextElement, keyword, sortBy] = [...block.children].map(
-    (row) => row.firstElementChild,
-  );
+  const [headingElement, descriptionElement, ...configs] = [...block.children].map((row) => row.firstElementChild);
+  const [contentType, linkTextElement, keyword, sortBy] = configs.map((cell) => cell.textContent.trim());
 
   const sortCriteria = COVEO_SORT_OPTIONS[sortBy];
   const noOfResults = 16;
@@ -50,7 +49,7 @@ export default async function decorate(block) {
   contentDiv.classList.add('browse-cards-block-content');
 
   const param = {
-    contentType: contentType && contentType.split(','),
+    contentType: contentType && contentType.toLowerCase().split(','),
     role: [],
     product: [],
     q: keyword,
