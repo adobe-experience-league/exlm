7fd8079c88548cf01abcedca6528d8176310ea30
Ruhi Singh <ruhis@adobe.com>
2024-02-19 15:53:19 +0530

    Addressed review comments

--- scripts/qualtrics.js
+++ scripts/qualtrics.js
@@ -7,32 +7,32 @@ export default async function loadQualtrics() {
       throw new Error(`${resp.status}: ${resp.statusText}`);
     })
     .then((json) => {
-      let d;
-
       json.data.forEach((item) => {
         const interceptId = item['Intercept Id'];
         const interceptURL = item['Intercept URL'];
-        const id = interceptId;
-        const c = `${id}_container`;
-        const o = document.getElementById(c);
-        if (o) {
-          o.innerHTML = '';
-          d = o;
-        } else {
-          d = document.createElement('div');
-          d.id = c;
-        }
-        const s = document.createElement('script');
-        s.type = 'text/javascript';
-        s.src = interceptURL;
-        if (document.body) {
-          document.body.appendChild(s);
-          document.body.appendChild(d);
+        if (interceptId && interceptURL) {
+          const c = `${interceptId}_container`;
+          let d;
+          const o = document.getElementById(c);
+          if (o) {
+            o.innerHTML = '';
+            d = o;
+          } else {
+            d = document.createElement('div');
+            d.id = c;
+          }
+          const s = document.createElement('script');
+          s.type = 'text/javascript';
+          s.src = interceptURL;
+          if (document.body) {
+            document.body.appendChild(s);
+            document.body.appendChild(d);
+          }
         }
       });
     })
-    .catch((e) => {
+    .catch((error) => {
       // eslint-disable-next-line no-console
-      console.log(e);
+      console.log(error);
     });
 }
