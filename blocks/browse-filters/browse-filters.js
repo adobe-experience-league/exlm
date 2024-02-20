7c75fc1fe711d1484a45b0d7e5b22727a82eb8cb
Nitin <tac32570@adobe.com>
2024-02-20 20:20:04 +0530

    feat: solution filter impl on browse pages exlm-1012

--- blocks/browse-filters/browse-filters.js
+++ blocks/browse-filters/browse-filters.js
@@ -8,12 +8,13 @@ import {
   getFiltersPaginationText,
   getBrowseFiltersResultCount,
   getSelectedTopics,
+  getParsedSolutionsQuery,
 } from './browse-filter-utils.js';
 import initiateCoveoHeadlessSearch, { fragment } from '../../scripts/coveo-headless/index.js';
 import BrowseCardsCoveoDataAdaptor from '../../scripts/browse-card/browse-cards-coveo-data-adaptor.js';
 import { buildCard } from '../../scripts/browse-card/browse-card.js';
 import BuildPlaceholder from '../../scripts/browse-card/browse-card-placeholder.js';
-import { formattedTags, handleTopicSelection } from './browse-topics.js';
+import { formattedTags, handleTopicSelection, dispatchCoveoAdvancedQuery } from './browse-topics.js';
 
 const coveoFacetMap = {
   Role: 'headlessRoleFacet',
@@ -98,15 +99,23 @@ function updateClearFilterStatus(block) {
   const hasActiveTopics = block.querySelector('.browse-topics') !== null && selectedTopics.length > 0;
   const browseFiltersContainer = document.querySelector('.browse-filters-container');
   const browseFiltersSection = browseFiltersContainer.querySelector('.browse-filters-form');
+  const selectionContainer = browseFiltersSection.querySelector('.browse-filters-input-container');
+  const containsSelection = selectionContainer.classList.contains('browse-filters-input-selected');
   if (hasActiveTopics || tagsProxy.length !== 0 || searchEl.value) {
     clearFilterBtn.disabled = false;
     hideSectionsBelowFilter(block, false);
     browseFiltersContainer.classList.add('browse-filters-full-container');
+    selectionContainer.classList.add('browse-filters-input-selected');
+    if (!containsSelection && window.headlessBaseSolutionQuery) {
+      dispatchCoveoAdvancedQuery(window.headlessBaseSolutionQuery, false);
+    }
     hildeSectionsWithinFilter(browseFiltersSection, true);
   } else {
     clearFilterBtn.disabled = true;
     hideSectionsBelowFilter(block, true);
     browseFiltersContainer.classList.remove('browse-filters-full-container');
+    selectionContainer.classList.remove('browse-filters-input-selected');
+    dispatchCoveoAdvancedQuery('', true);
     hildeSectionsWithinFilter(browseFiltersSection, false);
   }
 }
@@ -774,6 +783,12 @@ function decorateBrowseTopics(block) {
   // eslint-disable-next-line no-unused-vars
   const allSolutionsTags = solutions !== '' ? formattedTags(solutions) : '';
   const allTopicsTags = topics !== '' ? formattedTags(topics) : '';
+  const supportedProducts = [];
+  if (allSolutionsTags.length) {
+    const { query: additionalQuery, products } = getParsedSolutionsQuery(allSolutionsTags);
+    products.forEach((p) => supportedProducts.push(p));
+    window.headlessBaseSolutionQuery = additionalQuery;
+  }
 
   const div = document.createElement('div');
   div.classList.add('browse-topics');
@@ -793,7 +808,16 @@ function decorateBrowseTopics(block) {
 
   if (allTopicsTags.length > 0) {
     allTopicsTags
-      .filter((value) => value !== undefined)
+      .filter((value) => {
+        if (!value) {
+          return false;
+        }
+        if (!supportedProducts.length) {
+          return true;
+        }
+        const [, topicProduct] = value.split('/');
+        return topicProduct ? supportedProducts.includes(topicProduct.toLowerCase()) : true;
+      })
       .forEach((topicsButtonTitle) => {
         const parts = topicsButtonTitle.split('/');
         const topicName = parts[parts.length - 1];
@@ -810,8 +834,8 @@ function decorateBrowseTopics(block) {
         } else {
           e.target.classList.add('browse-topics-item-active');
         }
-        updateClearFilterStatus(browseFiltersSection);
         handleTopicSelection(contentDiv);
+        updateClearFilterStatus(browseFiltersSection);
       }
     });
     const decodedHash = decodeURIComponent(window.location.hash);
@@ -822,7 +846,9 @@ function decorateBrowseTopics(block) {
       if (selectedTopics && selectedTopics.length > 0) {
         selectedTopics.forEach((topic) => {
           const element = contentDiv.querySelector(`.browse-topics-item[data-topicname*="${topic}"]`);
-          element.classList.add('browse-topics-item-active');
+          if (element) {
+            element.classList.add('browse-topics-item-active');
+          }
         });
         handleTopicSelection(contentDiv);
       }
