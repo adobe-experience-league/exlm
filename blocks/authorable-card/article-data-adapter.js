436a65eae8521f5477991303c1ace0b36b6e3673
Gautham Krishna <141001403+gauthamksn@users.noreply.github.com>
2024-02-20 11:19:29 +0530

    Header Profile Issues Fix (#500)
    * overlay in header
    
    * refactor
    
    * refactor 2
    
    * revert layout changes
    
    * format
    
    * sass compile

--- blocks/authorable-card/article-data-adapter.js
+++ blocks/authorable-card/article-data-adapter.js
@@ -8,9 +8,8 @@ function createThumbnailURL(result) {
   }
 
   if (result.contentType === 'Tutorial') {
-    const videoUrl = result['Full Body'].match(/embedded-video src\s*=\s*['"]?([^'"]*)['"]?/)[1];
-    result.videoUrl = videoUrl;
-    [, result.videoId] = videoUrl.match(/\/v\/([^/]*)/);
+    const videoUrl = result['Full Body'].match(/embedded-video src\s*=\s*['"]?([^'"]*)['"]?/);
+    result.videoId = videoUrl ? videoUrl[1].match(/\/v\/([^/]*)/)[1] : null;
     thumbnail = result.videoId ? `https://video.tv.adobe.com/v/${result.videoId}?format=jpeg` : '';
   }
   return thumbnail;
