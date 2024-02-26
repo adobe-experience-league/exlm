52343272a5f0241355be3e4d58a25c8d125307aa
DEVISREE RAVURI <dravuri@adobe.com>
2024-02-26 14:32:50 +0530

    EXLM 1137 Updated Url

--- scripts/urls.js
+++ scripts/urls.js
@@ -12,7 +12,6 @@ export const solutionsUrl = 'https://experienceleague.adobe.com/api/solutions?pa
 export const tocUrl = 'https://experienceleague.adobe.com/api/tocs/';
 export const pathsUrl = `https://experienceleague.adobe.com/api/paths`;
 export const exlmCDNUrl = 'https://cdn.experienceleague.adobe.com';
-export const recommendedCoursesUrl = `${environmentOrigin}/home?lang=${lang}#dashboard/learning`;
 // Khoros Proxy URL (Determine the environment based on the host name)
 const environment = window.location.hostname === 'experienceleague.adobe.com' ? '' : '-dev';
 export const khorosProxyProfileAPI = `https://51837-exlmconverter${environment}.adobeioruntime.net/api/v1/web/main/khoros/plugins/custom/adobe/adobedx/profile-menu-list?lang=${lang}`;
@@ -28,3 +27,5 @@ export const tabbedCardViewAllDocumentationLink = `/${lang}/docs`;
 export const tabbedCardViewAllCommunityLink = 'https://experienceleaguecommunities.adobe.com';
 export const tabbedCardViewAllCertificationLink = `/${lang}/docs/certification/program/overview`;
 export const tabbedCardViewAllOnDemandEventsLink = `/events?lang=${lang}#ondemandevents`;
+// Recommended Courses
+export const recommendedCoursesUrl = `${environmentOrigin}/home?lang=${lang}#dashboard/learning`;
