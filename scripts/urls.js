a6dffbfc604d4fb14669c4918ec912d21d725992
DEVISREE RAVURI <dravuri@adobe.com>
2024-02-26 13:53:58 +0530

    PR Review Comments

--- scripts/urls.js
+++ scripts/urls.js
@@ -11,7 +11,7 @@ export const solutionsUrl = 'https://experienceleague.adobe.com/api/solutions?pa
 export const tocUrl = 'https://experienceleague.adobe.com/api/tocs/';
 export const pathsUrl = `https://experienceleague.adobe.com/api/paths`;
 export const exlmCDNUrl = 'https://cdn.experienceleague.adobe.com';
-export const recommendedCoursesUrl = `/home?lang=${lang}#dashboard/learning`;
+export const recommendedCoursesUrl = `https://experienceleague.adobe.com/?lang=${lang}#dashboard/learning`;
 // Khoros Proxy URL (Determine the environment based on the host name)
 const environment = window.location.hostname === 'experienceleague.adobe.com' ? '' : '-dev';
 export const khorosProxyProfileAPI = `https://51837-exlmconverter${environment}.adobeioruntime.net/api/v1/web/main/khoros/plugins/custom/adobe/adobedx/profile-menu-list?lang=${lang}`;
