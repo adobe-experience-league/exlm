d9946acbe4695583688568abc5d2de0266d5d42a
Gautham Krishna <141001403+gauthamksn@users.noreply.github.com>
2024-03-01 11:06:24 +0530

    Header and Marquee signup logic rework (#592)
    * init
    
    * format
    
    * update scss

--- blocks/marquee/marquee.js
+++ blocks/marquee/marquee.js
@@ -78,7 +78,7 @@ export default async function decorate(block) {
     .then((isSignedInUser) => {
       if (!isSignedInUser) {
         block.classList.add('unauthenticated');
-        block.querySelector('.signin').addEventListener('click', () => window.adobeIMS.signIn());
+        block.querySelector('.signin').addEventListener('click', () => window.adobeIMS.signUp());
       }
     });
 
