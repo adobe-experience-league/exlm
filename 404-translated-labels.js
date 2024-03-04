35e9ba798e43e4064fbff3fb5bfd6561f7bad579
Ruhi Singh <ruhis@adobe.com>
2024-03-04 20:36:38 +0530

    Translated 404 pages

---
+++ 404-translated-labels.js
@@ -0,0 +1,119 @@
+export const OOPS_TEXT = Object.freeze({
+  en: 'Oops!',
+  de: 'Ups!',
+  fr: 'Oops!',
+  it: 'Ops!',
+  sv: 'Hoppsan!',
+  es: '¡Ups!',
+  nl: 'Oeps!',
+  ko: '이런!',
+  ja: '失敗しました',
+  'pt-br': 'Opa!',
+  'zh-hant': '糟糕！',
+  'zh-hans': '糟糕！',
+});
+
+export const PAGE_NOT_FOUND_TEXT = Object.freeze({
+  en: "This page isn't found.",
+  de: 'Diese Seite wurde nicht gefunden.',
+  fr: 'Cette page est introuvable.',
+  it: 'Questa pagina non è stata trovata.',
+  sv: 'Den här sidan hittades inte.',
+  es: 'No se encontró la página.',
+  nl: 'Deze pagina is niet gevonden.',
+  ko: '이 페이지를 찾을 수 없습니다.',
+  ja: 'ページが見つかりません（404）',
+  'pt-br': 'Esta página não foi encontrada.',
+  'zh-hant': '找不到此頁面。',
+  'zh-hans': '页面未找到。',
+});
+
+export const HELP_TEXT = Object.freeze({
+  en: 'Let us help you get back on the path.',
+  de: 'Lassen Sie uns Ihnen helfen, auf den Pfad zurückzukehren.',
+  fr: 'Laissez-nous vous remettre sur les rails.',
+  it: 'Permettici di aiutarti a riprendere il tuo percorso.',
+  sv: 'Låt oss hjälpa dig hitta rätt.',
+  es: 'Permita que lo ayudemos a volver a encaminarse.',
+  nl: 'We helpen u weer op weg.',
+  ko: '올바른 경로로 돌아가도록 도와 드리겠습니다.',
+  ja: 'この問題を解決するには、以下の操作をおこないます。',
+  'pt-br': 'Deixe-nos ajudá-lo a voltar ao caminho certo.',
+  'zh-hant': '讓我們協助您找到正確路徑。',
+  'zh-hans': '让我们来帮您重返学途吧。',
+});
+
+export const MOST_POPULAR_SECTION = Object.freeze({
+  en: 'Most popular sections',
+  de: 'BELIEBTESTE ABSCHNITTE',
+  fr: 'SECTIONS LES PLUS APPRÉCIÉES',
+  it: 'LE SEZIONI PIÙ POPOLARI',
+  sv: 'POPULÄRASTE AVSNITTEN',
+  es: 'SECCIONES MÁS POPULARES',
+  nl: 'POPULAIRSTE SECTIES',
+  ko: '추천 섹션',
+  ja: '一番人気のセクション',
+  'pt-br': 'SEÇÕES MAIS POPULARES',
+  'zh-hant': '最熱門的章節',
+  'zh-hans': '最受欢迎的版块',
+});
+
+export const EXL_HOME_PAGE = Object.freeze({
+  en: 'Experience League Homepage',
+  de: 'Experience League Homepage',
+  fr: 'Page d’accueil d’Experience League',
+  it: 'Homepage di Experience League',
+  sv: 'Experience Leagues startsida',
+  es: 'Página de inicio de Experience League',
+  nl: 'Experience League-startpagina',
+  ko: 'Experience League 홈페이지',
+  ja: 'Experience League のホームページ',
+  'pt-br': 'Página inicial da Experience League',
+  'zh-hant': 'Experience League 首頁',
+  'zh-hans': 'Experience League 主页',
+});
+
+export const DOCUMENTATION = Object.freeze({
+  en: 'Documentation',
+  de: 'Dokumentation',
+  fr: 'Documentation',
+  it: 'Documentazione',
+  sv: 'Dokumentation',
+  es: 'Documentación',
+  nl: 'Documentatie',
+  ko: '설명서',
+  ja: 'ドキュメント',
+  'pt-br': 'Documentação',
+  'zh-hant': '文件',
+  'zh-hans': '文档',
+});
+
+export const COURSES = Object.freeze({
+  en: 'Courses',
+  de: 'Kurse',
+  fr: 'Formations',
+  it: 'Corsi',
+  sv: 'Kurser',
+  es: 'Cursos',
+  nl: 'Cursussen',
+  ko: '과정',
+  ja: 'コース',
+  'pt-br': 'Cursos',
+  'zh-hant': '課程',
+  'zh-hans': '课程',
+});
+
+export const COMMUNITY = Object.freeze({
+  en: 'Community',
+  de: 'Community',
+  fr: 'Communauté',
+  it: 'Community',
+  sv: 'Community',
+  es: 'Comunidad',
+  nl: 'Community',
+  ko: '커뮤니티',
+  ja: 'コミュニティ',
+  'pt-br': 'Comunidade',
+  'zh-hant': '社群',
+  'zh-hans': '社区',
+});
