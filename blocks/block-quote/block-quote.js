4dacda4e952e4f1479b82720e92d263de84a565e
DEVISREE RAVURI <dravuri@adobe.com>
2024-04-13 01:12:34 +0530

    EXLM 1297 Spacing Issue

--- blocks/block-quote/block-quote.js
+++ blocks/block-quote/block-quote.js
@@ -2,6 +2,6 @@ import { getMetadata } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
   // Getting the theme from the Metadata Properties
-  const theme = getMetadata(article - theme) || 'external';
+  const theme = getMetadata('article-theme') || 'external';
   block.classList.add(`block-quote-content ${theme}`);
 }
