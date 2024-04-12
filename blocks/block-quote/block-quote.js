7037ae82f46ca19d26e577151a767ef849268444
DEVISREE RAVURI <dravuri@adobe.com>
2024-04-13 01:01:31 +0530

    EXLM 1297 Spacing Issue

--- blocks/block-quote/block-quote.js
+++ blocks/block-quote/block-quote.js
@@ -1,18 +1,7 @@
-import { htmlToElement } from '../../scripts/scripts.js';
+import { getMetadata } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
-  // Extracting elements from the block
-  const [blockQuoteTextElement] = [...block.children].map((row) => row.firstElementChild);
-  block.innerHTML = '';
   // Getting the theme from the Metadata Properties
-  const metaArticleTheme = document.querySelector('meta[name="article-theme"]');
-  const theme = metaArticleTheme ? metaArticleTheme.getAttribute('content') : 'external';
-  const headerDiv = htmlToElement(`
-      <div class="block-quote-content">
-        ${blockQuoteTextElement.innerHTML.trim()}
-    </div>
-  `);
-  block.classList.add(theme);
-
-  block.appendChild(headerDiv);
+  const theme = getMetadata(article - theme) || 'external';
+  block.classList.add(`block-quote-content ${theme}`);
 }
