91f44b3fccc35d265dff6e617f381fc30a0ea0f3
Victor Hernandez <end35203@adobe.com>
2024-04-17 12:43:50 -0500

    UGP-9828 Page titles to pagination buttons.

--- scripts/prev-next-btn.js
+++ scripts/prev-next-btn.js
@@ -6,7 +6,9 @@ export default async function loadPrevNextBtn() {
   if (!mainDoc) return;
 
   const prevPageMeta = document.querySelector('meta[name="prev-page"]');
+  const prevPageMetaTitle = document.querySelector('meta[name="prev-page-title"]')?.getAttribute('content') || '';
   const nextPageMeta = document.querySelector('meta[name="next-page"]');
+  const nextPageMetaTitle = document.querySelector('meta[name="next-page-title"]')?.getAttribute('content') || '';
   const prevPageMetaContent = prevPageMeta?.getAttribute('content').trim().split('.html')[0];
   const nextPageMetaContent = nextPageMeta?.getAttribute('content').trim().split('.html')[0];
   const PREV_PAGE = placeholders?.previousPage;
@@ -25,9 +27,10 @@ export default async function loadPrevNextBtn() {
     };
     const anchorLeft = createTag('a', anchorLeftAttr);
     const spanLeft = createTag('span', '', PREV_PAGE);
+    const titleLeft = prevPageMetaTitle ? createTag('span', { class: 'pagination-btn-title' }, prevPageMetaTitle) : '';
 
     anchorLeft.append(spanLeft);
-    btnGotoLeft.append(anchorLeft);
+    btnGotoLeft.append(anchorLeft, titleLeft);
 
     const btnGotoRight = createTag('div', {
       class: 'btn-goto is-right-desktop',
@@ -40,16 +43,19 @@ export default async function loadPrevNextBtn() {
     };
     const anchorRight = createTag('a', anchorRightAttr);
     const spanRight = createTag('span', '', NEXT_PAGE);
+    const titleRight = nextPageMetaTitle ? createTag('span', { class: 'pagination-btn-title' }, nextPageMetaTitle) : '';
 
     anchorRight.append(spanRight);
-    btnGotoRight.append(anchorRight);
+    btnGotoRight.append(anchorRight, titleRight);
 
     if (!prevPageMeta || prevPageMetaContent === '') {
       anchorLeft.classList.add('is-disabled');
+      titleLeft.classList.add('is-hidden');
     }
 
     if (!nextPageMeta || nextPageMetaContent === '') {
       anchorRight.classList.add('is-disabled');
+      titleRight.classList.add('is-hidden');
     }
 
     docPagination.append(btnGotoLeft, btnGotoRight);
