22c8b0957febfa282fce5a755aa69960b8a2b30d
mayukhs2904 <mayukhs@adobe.com>
2024-04-22 19:57:18 +0530

    review changes

--- blocks/article-tags/article-tags.js
+++ blocks/article-tags/article-tags.js
@@ -1,35 +1,39 @@
 import { htmlToElement } from '../../scripts/scripts.js';
 import { getMetadata } from '../../scripts/lib-franklin.js';
-
-function createTags(values) {
-  let tagsHTML = '';
-  values.forEach((value) => {
-      tagsHTML += `<div class="article-tags-name">${value.trim()}</div>`;
-  });
-  return tagsHTML;
-}
-
 export default function decorate(block) {
-  const solutions = getMetadata('coveo-solution')?.split(',').filter(Boolean);
-  const roles = getMetadata('role')?.split(',').filter(Boolean);
-  const experienceLevels = getMetadata('level')?.split(',').filter(Boolean);
+  const solutions = getMetadata('coveo-solution');
+  const roles = getMetadata('role');
+  const experienceLevels = getMetadata('level');
 
   const [articleTagHeading] = [...block.children].map((row) => row.firstElementChild);
+
   block.innerHTML = '';
   block.classList.add('article-tags');
 
   const headerDiv = htmlToElement(`
-  <div class="article-tags-header">
-    <div class="article-tags-title">
-      ${articleTagHeading.innerHTML}
-    </div>
-    <div class="article-tags-view">
-      ${createTags(solutions)}
-      ${createTags(roles)}
-      ${createTags(experienceLevels)}
-    </div>
-  </div>
-`);
+      <div class="article-tags">
+        <div class="article-tags-title">
+          ${articleTagHeading.innerHTML}
+        </div>
+        <div class="article-tags-view">
+        ${solutions
+          .split(',')
+          .filter(Boolean)
+          .map((value) => `<div class="article-tags-name">${value.trim()}</div>`)
+          .join('')}
+        ${roles
+          .split(',')
+          .filter(Boolean)
+          .map((value) => `<div class="article-tags-name">${value.trim()}</div>`)
+          .join('')}
+        ${experienceLevels
+          .split(',')
+          .filter(Boolean)
+          .map((value) => `<div class="article-tags-name">${value.trim()}</div>`)
+          .join('')}
+        </div>
+      </div>
+    `);
 
   block.append(headerDiv);
 }
