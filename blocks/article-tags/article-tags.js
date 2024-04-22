f5d08e722813a802e40aff075388d075370573e7
mayukhs2904 <mayukhs@adobe.com>
2024-04-22 19:25:00 +0530

    formatting fixes

--- blocks/article-tags/article-tags.js
+++ blocks/article-tags/article-tags.js
@@ -1,37 +1,35 @@
-import { htmlToElement } from "../../scripts/scripts.js";
-import { getMetadata } from "../../scripts/lib-franklin.js";
+import { htmlToElement } from '../../scripts/scripts.js';
+import { getMetadata } from '../../scripts/lib-franklin.js';
 
 function createTags(values) {
-  let tagsHTML = "";
+  let tagsHTML = '';
   values.forEach((value) => {
-    tagsHTML += `<div class="article-tags-name">${value.trim()}</div>`;
+      tagsHTML += `<div class="article-tags-name">${value.trim()}</div>`;
   });
   return tagsHTML;
 }
 
 export default function decorate(block) {
-  const solutions = getMetadata("coveo-solution")?.split(",").filter(Boolean);
-  const roles = getMetadata("role")?.split(",").filter(Boolean);
-  const experienceLevels = getMetadata("level")?.split(",").filter(Boolean);
+  const solutions = getMetadata('coveo-solution')?.split(',').filter(Boolean);
+  const roles = getMetadata('role')?.split(',').filter(Boolean);
+  const experienceLevels = getMetadata('level')?.split(',').filter(Boolean);
 
-  const [articleTagHeading] = [...block.children].map(
-    (row) => row.firstElementChild,
-  );
-  block.innerHTML = "";
-  block.classList.add("article-tags");
+  const [articleTagHeading] = [...block.children].map((row) => row.firstElementChild);
+  block.innerHTML = '';
+  block.classList.add('article-tags');
 
   const headerDiv = htmlToElement(`
-    <div class="article-tags-header">
-      <div class="article-tags-title">
-        ${articleTagHeading.innerHTML}
-      </div>
-      <div class="article-tags-view">
-        ${createTags(solutions)}
-        ${createTags(roles)}
-        ${createTags(experienceLevels)}
-      </div>
+  <div class="article-tags-header">
+    <div class="article-tags-title">
+      ${articleTagHeading.innerHTML}
     </div>
-  `);
+    <div class="article-tags-view">
+      ${createTags(solutions)}
+      ${createTags(roles)}
+      ${createTags(experienceLevels)}
+    </div>
+  </div>
+`);
 
   block.append(headerDiv);
 }
