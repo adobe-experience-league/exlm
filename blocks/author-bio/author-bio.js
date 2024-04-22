f01ba943aa55ead68eaf1c742ccaa27066eb5e51
DEVISREE RAVURI <dravuri@adobe.com>
2024-04-22 19:39:32 +0530

    EXLM 1283 Author Bio Pages Block

--- blocks/author-bio/author-bio.js
+++ blocks/author-bio/author-bio.js
@@ -14,18 +14,23 @@ export default function decorate(block) {
   const authorBioPicture = authorImage.querySelector('picture');
   const authorSocialLinkElement = authorSocialLinkURL.querySelector('a').href;
   const authorBioDOM = htmlToElement(`
-        <div class='author-bio-content ${authorCompany ? authorCompany.innerHTML : 'external'}'>
-        <div class='author-image'>${authorBioPicture ? authorBioPicture.outerHTML : ''}</div>
-        <div class="author-name-title-container">
-          <div class='author-name'>${authorName.innerHTML}</div>
-          <div class='author-title-company'>${authorTitle.innerHTML} ${authorCompany.innerHTML}</div>
-        </div>
-        <div class="author-description-socialLink-container">  
-          <div class='author-description'>${authorDescription.innerHTML}</div>
-          <a href='${authorSocialLinkElement}' class='author-socialLink' title='${authorSocialLinkElement}'>${authorSocialLinkText.innerHTML}</a>
-        </div>
-        </div>
-    `);
+  <div class='author-bio-content ${authorCompany ? authorCompany.innerHTML : 'external'}'>
+    <div class='author-image'>${authorBioPicture ? authorBioPicture.outerHTML : ''}</div>
+    <div class="author-name-title-container">
+      <div class='author-name'>${authorName ? authorName.innerHTML : ''}</div>
+      <div class='author-title-company'>
+        ${authorTitle ? authorTitle.innerHTML : ''} ${authorCompany ? authorCompany.innerHTML : ''}
+      </div>
+    </div>
+    <div class="author-description-socialLink-container">
+      <div class='author-description'>${authorDescription ? authorDescription.innerHTML : ''}</div>
+      <a href='${authorSocialLinkElement}' class='author-socialLink' title='${authorSocialLinkElement}'>
+        ${authorSocialLinkText ? authorSocialLinkText.innerHTML : ''}
+      </a>
+    </div>
+  </div>
+`);
+
   block.innerHTML = '';
   block.append(authorBioDOM);
 }
