f3ee279d2aa4875c5983eed7dcdf651d2069927a
Jo Ross <joross@adobe.com>
2024-04-29 14:14:51 -0400

    UGP-10163: Fixed dependency cycles breaking coveo-analytics.js, added hash function for id generation in crypto.js

--- scripts/coveo-analytics.js
+++ scripts/coveo-analytics.js
@@ -1,14 +1,36 @@
-import { SHA1, MD5 } from 'crypto-js';
-/*
-import { defaultProfileClient } from './auth/profile.js';
-import loadCoveoToken from './data-service/coveo/coveo-token-service.js';
-*/
+import { SHA1, MD5 } from './crypto.js';
 import { COVEO_TOKEN } from './session-keys.js';
 
 const COVEO_ANALYTICS_URL = {
   PROD: 'https://adobev2prod9e382h1q.analytics.org.coveo.com/rest/ua/v15/analytics/',
   STAGE: 'https://adobesystemsincorporatednonprod1.analytics.org.coveo.com/rest/ua/v15/analytics/',
 };
+
+const COVEO_SESSION_UUID = 'coveo-session-id';
+
+/**
+ * Gets a UUID for use with coveo events
+ *
+ * @param {*} profile - user profile data
+ * @returns
+ */
+function getCoveoSessionUUID(profile) {
+  let id = profile?.authId;
+
+  if (id === undefined) {
+    const fromSession = sessionStorage.getItem(COVEO_SESSION_UUID);
+
+    if (fromSession === null) {
+      id = window.crypto.randomUUID();
+      sessionStorage.setItem(COVEO_SESSION_UUID, id);
+    } else {
+      id = fromSession;
+    }
+  }
+
+  return id;
+}
+
 /**
  *
  * @param {*} type - The type of analytics event, always lower case ex: view, click
@@ -18,18 +40,21 @@ const COVEO_ANALYTICS_URL = {
 async function postCoveoAnalytics(type, body) {
   try {
     // Load the Coveo Token
-    // const coveoToken = await loadCoveoToken();
+    // const coveoToken = await loadCoveoToken(); - Causes cyclic dependency that breaks build
     const coveoToken = sessionStorage.getItem(COVEO_TOKEN);
 
-    const response = await fetch(`${COVEO_ANALYTICS_URL[window.exlm.config.currentEnv.env]}${type}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: `Bearer ${coveoToken}`,
+    const response = await fetch(
+      `${COVEO_ANALYTICS_URL[window.exlm.config?.currentEnv?.env] || COVEO_ANALYTICS_URL.STAGE}${type}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${coveoToken}`,
+        },
+        body,
       },
-      body,
-    });
+    );
     if (response.status === 200) {
       const data = await response.json();
       return data || [];
@@ -46,26 +71,28 @@ async function postCoveoAnalytics(type, body) {
 }
 
 async function getCoveoHashOfCurrentUrl() {
-  const md5Fragment = MD5(window.location.href).toString().slice(0, 30);
-  const sha1Fragment = SHA1(window.location.href).toString().slice(0, 30);
+  // The permenantid coveo assigned to docs is based off their original URL at time of indexing
+  // so for legacy docs pages it will be the existing publish-url and not the current page url
+  const url = document.querySelector("meta[name='publish-url']")?.content || window.location.href;
+
+  // Coveo, by default, combined two types of hashes of the url for it's permenantid format
+  // for legacy docs pages. If tracking on Universal Editor pages doesn't appear to be working correctly
+  // this will need to be updated based on whatever we observe.
+  const md5Fragment = MD5(url).toString().slice(0, 30);
+  const sha1Fragment = SHA1(url).toString().slice(0, 30);
+
   return `${md5Fragment}${sha1Fragment}`;
 }
 
 /**
  * For use when a searchable page is initially loaded
  *
+ * https://docs.coveo.com/en/2651/build-a-search-ui/log-view-events
  */
 export async function sendCoveoPageViewEvent() {
   const contentType = document.querySelector("meta[name='type']")?.content;
-
-  if (contentType === undefined) {
-    // eslint-disable-next-line no-console
-    console.error('Missing "type" meta. Not sending coveo page view');
-    return; // Missing meta type make this event useless
-  }
-
   const title = document.querySelector("meta[name='og:title']")?.content;
-  // const profileData = defaultProfileClient.getMergedProfile(false);
+  // const profileData = defaultProfileClient.getMergedProfile(false); - Causes cyclic dependency that breaks build
   const profileData = sessionStorage.getItem('profile');
   const customData =
     profileData === null
@@ -79,11 +106,11 @@ export async function sendCoveoPageViewEvent() {
 
   const baseData = {
     language: window.languageCode.substring(0, 2).toLowerCase(), // Two-letter codes only
-    clientId: profileData?.authId,
+    clientId: getCoveoSessionUUID(profileData),
     location: window.location.href, // Current url
     contentIdKey: 'permanentid',
     contentIdValue: getCoveoHashOfCurrentUrl(), // First 30 md5 + first 30 sha1 of coveo url
-    contentType, // If we need page views for non-doc pages we'll need an alternative
+    contentType: contentType || 'other', // We might need to expand 'other' for non-doc pages down the road
     outcome: 2, // -5 to 5, we might want to adjust this in the future
     title,
     anonymous: true,
@@ -98,17 +125,13 @@ export async function sendCoveoPageViewEvent() {
 /**
  * For use when the user clicks on an item that is populated by a coveo query.
  *
+ * https://docs.coveo.com/en/2064/build-a-search-ui/log-click-events
+ *
  * @param {*} raw - The 'raw' object on the associated coveo result item.
  */
 export async function sendCoveoClickEvent(source, model) {
-  /*
-  if(window.location.hostname !== 'experienceleague.adobe.com') {
-    return;
-  }
-  */
-
   const { index, permanentid, searchQueryUid, title, viewLink } = model;
-  // const profileData = defaultProfileClient.getMergedProfile(false);
+  // const profileData = defaultProfileClient.getMergedProfile(false); - Causes cyclic dependency that breaks build
   const profileData = sessionStorage.getItem('profile');
   const customData =
     profileData === null
@@ -127,8 +150,8 @@ export async function sendCoveoClickEvent(source, model) {
 
   const baseData = {
     anonymous: true,
-    clientId: profileData?.authId,
-    actionCause: 'documentOpen',
+    clientId: getCoveoSessionUUID(profileData),
+    actionCause: 'documentOpen', // This should probably never change, but https://docs.coveo.com/en/1389/build-a-search-ui/standard-actions-and-usage-analytics-reference
     documentPosition: index + 1,
     documentTitle: title,
     documentUrl: viewLink,
