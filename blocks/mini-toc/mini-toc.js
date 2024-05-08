e09c5457fc4fd787c5692d87dce8f39f89ae8407
Nitin <tac32570@adobe.com>
2024-05-08 11:55:36 +0530

    fix: mini-toc query selection fixes

--- blocks/mini-toc/mini-toc.js
+++ blocks/mini-toc/mini-toc.js
@@ -31,117 +31,114 @@ export default async function decorate(block) {
   const render = window.requestAnimationFrame;
   const levels = document.querySelector('meta[name="mini-toc-levels"]');
 
+  const miniTocQuerySelection = isArticlePage() ? '.article-content-section' : 'main';
+  const headingLevels = setLevels(
+    levels !== null && parseInt(levels.content, 10) > 0 ? parseInt(levels.content, 10) : undefined,
+  );
+  const selectorQuery = headingLevels
+    .split(',')
+    .map((query) => `${miniTocQuerySelection} ${query}`)
+    .join(',');
+  const baseEl = block.closest(miniTocQuerySelection) ?? document;
+  const headers = Array.from(baseEl.querySelectorAll(selectorQuery)).filter(headerExclusions);
 
-    const miniTocSectionEl = block.closest('.article-content-section');
-    const headingLevels = setLevels(
-      levels !== null && parseInt(levels.content, 10) > 0 ? parseInt(levels.content, 10) : undefined,
-    );
-    const selectorQuery = headingLevels
-      .split(',')
-      .map((query) => `.article-content-section ${query}`)
-      .join(',');
-    const headers = Array.from(miniTocSectionEl.querySelectorAll(selectorQuery)).filter(headerExclusions);
+  if (headers.length > 1) {
+    const html = headers.map((i) => `<li><a href="#${i.id}" class="${setPadding(i.nodeName)}">${i.innerText}</a></li>`);
+    // eslint-disable-next-line no-restricted-globals
+    const url = new URL(location.href);
+    const lhash = url.hash.length > 0;
 
-    if (headers.length > 1) {
-      const html = headers.map(
-        (i) => `<li><a href="#${i.id}" class="${setPadding(i.nodeName)}">${i.innerText}</a></li>`,
-      );
-      // eslint-disable-next-line no-restricted-globals
-      const url = new URL(location.href);
-      const lhash = url.hash.length > 0;
+    render(() => {
+      const tocHeadingDivNode = `<div><h2>${miniTOCHeading}</h2></div>`;
+      block.innerHTML = `${tocHeadingDivNode}\n<div class='scrollable-div'><ul>${html.join('\n')}</ul></div>`;
 
-      render(() => {
-        const tocHeadingDivNode = `<div><h2>${miniTOCHeading}</h2></div>`;
-        block.innerHTML = `${tocHeadingDivNode}\n<div class='scrollable-div'><ul>${html.join('\n')}</ul></div>`;
+      let lactive = false;
+      const anchors = Array.from(block.querySelectorAll('a'));
 
-        let lactive = false;
-        const anchors = Array.from(block.querySelectorAll('a'));
+      if (isArticlePage()) {
+        const anchorTexts = anchors.map((anchor) => {
+          const content = anchor.textContent;
+          return {
+            id: content,
+            value: anchor.href,
+            title: content,
+          };
+        });
+        // eslint-disable-next-line no-new
+        new Dropdown(block, 'Summary', anchorTexts, DROPDOWN_VARIANTS.ANCHOR); // Initialise mini-toc dropdown for mobile view
 
-        if (isArticlePage()) {
-          const anchorTexts = anchors.map((anchor) => {
-            const content = anchor.textContent;
-            return {
-              id: content,
-              value: anchor.href,
-              title: content,
-            };
+        window.addEventListener('hashchange', () => {
+          const { hash } = window.location;
+          const matchFound = anchorTexts.find((a) => {
+            const [, linkHash] = a.value.split('#');
+            return `#${linkHash}` === hash;
           });
-          // eslint-disable-next-line no-new
-          new Dropdown(block, 'Summary', anchorTexts, DROPDOWN_VARIANTS.ANCHOR); // Initialise mini-toc dropdown for mobile view
-
-          window.addEventListener('hashchange', () => {
-            const { hash } = window.location;
-            const matchFound = anchorTexts.find((a) => {
-              const [, linkHash] = a.value.split('#');
-              return `#${linkHash}` === hash;
-            });
-            if (matchFound) {
-              Dropdown.closeAllDropdowns();
-            }
-          });
-        }
-
-        if (anchors.length > 0) {
-          anchors.forEach((i, idx) => {
-            if (lhash === false && idx === 0) {
-              i.classList.add('is-active');
-              lactive = true;
-            } else if (lhash && i.hash === url.hash) {
-              i.classList.add('is-active');
-              lactive = true;
-            }
-
-            i.addEventListener(
-              'click',
-              () => {
-                const ahash = (i.href.length > 0 ? new URL(i.href).hash || '' : '').replace(/^#/, '');
-                let activeAnchor = i;
-                if (
-                  !i.classList.contains('is-padded-left-big') &&
-                  i.parentElement?.nextElementSibling?.firstElementChild?.classList?.contains('is-padded-left-big')
-                ) {
-                  activeAnchor = i.parentElement.nextElementSibling.firstChild;
-                }
-                render(() => {
-                  anchors.forEach((a) => a.classList.remove('is-active'));
-                  activeAnchor.classList.add('is-active');
-
-                  if (ahash.length > 0) {
-                    hashFragment(ahash);
-                  }
-                });
-              },
-              false,
-            );
-
-            if (lactive === false) {
-              highlight(false);
-            }
-          });
-
-          const anchor = anchors[0].parentElement;
-          const scrollableDiv = block.querySelector('.scrollable-div');
-          if (scrollableDiv) {
-            // dynamically make sure no item is partially visible
-            const anchorClientHeight = anchor.offsetHeight;
-            const anchorStyles = getComputedStyle(anchor);
-            const marginTop = parseInt(anchorStyles.marginTop || '0', 10);
-            const marginBottom = parseInt(anchorStyles.marginBottom || '0', 10);
-            const anchorHeight = anchorClientHeight + marginTop + marginBottom;
-            const halfWindowHeight = window.innerHeight / 2;
-            const visibleAnchorsCount = Math.floor(halfWindowHeight / anchorHeight);
-            if (visibleAnchorsCount && anchors.length > visibleAnchorsCount) {
-              // scrollableDiv.style.maxHeight = `${visibleAnchorsCount * anchorHeight}px`;
-            }
+          if (matchFound) {
+            Dropdown.closeAllDropdowns();
           }
-          window.addEventListener('scroll', () => debounce('scroll', () => highlight(), 16));
-          decorateIcons(block);
-        }
-      });
-    } else {
-      render(() => {
-        block.parentElement.parentElement.classList.add('is-hidden');
-      });
-    }
+        });
+      }
 
+      if (anchors.length > 0) {
+        anchors.forEach((i, idx) => {
+          if (lhash === false && idx === 0) {
+            i.classList.add('is-active');
+            lactive = true;
+          } else if (lhash && i.hash === url.hash) {
+            i.classList.add('is-active');
+            lactive = true;
+          }
+
+          i.addEventListener(
+            'click',
+            () => {
+              const ahash = (i.href.length > 0 ? new URL(i.href).hash || '' : '').replace(/^#/, '');
+              let activeAnchor = i;
+              if (
+                !i.classList.contains('is-padded-left-big') &&
+                i.parentElement?.nextElementSibling?.firstElementChild?.classList?.contains('is-padded-left-big')
+              ) {
+                activeAnchor = i.parentElement.nextElementSibling.firstChild;
+              }
+              render(() => {
+                anchors.forEach((a) => a.classList.remove('is-active'));
+                activeAnchor.classList.add('is-active');
+
+                if (ahash.length > 0) {
+                  hashFragment(ahash);
+                }
+              });
+            },
+            false,
+          );
+
+          if (lactive === false) {
+            highlight(false);
+          }
+        });
+
+        const anchor = anchors[0].parentElement;
+        const scrollableDiv = block.querySelector('.scrollable-div');
+        if (scrollableDiv) {
+          // dynamically make sure no item is partially visible
+          const anchorClientHeight = anchor.offsetHeight;
+          const anchorStyles = getComputedStyle(anchor);
+          const marginTop = parseInt(anchorStyles.marginTop || '0', 10);
+          const marginBottom = parseInt(anchorStyles.marginBottom || '0', 10);
+          const anchorHeight = anchorClientHeight + marginTop + marginBottom;
+          const halfWindowHeight = window.innerHeight / 2;
+          const visibleAnchorsCount = Math.floor(halfWindowHeight / anchorHeight);
+          if (visibleAnchorsCount && anchors.length > visibleAnchorsCount) {
+            // scrollableDiv.style.maxHeight = `${visibleAnchorsCount * anchorHeight}px`;
+          }
+        }
+        window.addEventListener('scroll', () => debounce('scroll', () => highlight(), 16));
+        decorateIcons(block);
+      }
+    });
+  } else {
+    render(() => {
+      block.parentElement.parentElement.classList.add('is-hidden');
+    });
+  }
 }
