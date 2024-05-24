4c4589513dd92c63d91466d983e796da10fe43a4
Gautham Krishna <141001403+gauthamksn@users.noreply.github.com>
2024-05-24 14:49:46 +0530

    EXLM 1508 - Profile page Theme (#894)
    * init
    
    * layout fix
    
    * refactor
    
    * init auto blocks
    
    format
    
    * border for blocks
    
    * format

---
+++ blocks/profile-tab/profile-tab.js
@@ -0,0 +1,7 @@
+export default function ProfileTab(block) {
+  // This is a placeholder. Replace with actual implementation.
+  block.innerHTML = `
+      <a href="#">Profile Settings</a>
+      <a href="#">Bookmarks & Achievments</a>
+    `;
+}
