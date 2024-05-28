2481448a64719c9f160648fa5cae77267f6af9e1
Rob Rusher <rrusher@adobe.com>
2024-05-28 16:51:29 -0600

    buildout html

--- blocks/featured-content/featured-content.js
+++ blocks/featured-content/featured-content.js
@@ -1,21 +1,20 @@
+import { div, h2 } from "../../scripts/dom-helpers";
+
 export async function getContentReference(link) {
   return fetch(link)
     .then((response) => response.text())
     .then((html) => {
       const parser = new DOMParser();
       const htmlDoc = parser.parseFromString(html, 'text/html');
-      const title = htmlDoc.querySelector("meta[name='og:title']")?.content;
-      const image =  htmlDoc.querySelector('.article-content-section picture img')?.src;
-      const description = htmlDoc.querySelector('article-content-section p')?.textContent;
+      const title = htmlDoc.querySelector('head title')?.content;
+      const description = htmlDoc.querySelector('main div p')?.textContent;
       // const authorInfo = [...htmlDoc.querySelector('.author-details')?.children].map((row) => row.firstElementChild);
       const authorInfo = htmlDoc.querySelector('.author-details');
 
       return {
         contentTitle: title,
-        contentImage: image,
         contentDescription: description,
-        authorName: authorInfo[1].textContent,
-        authorProfile: authorInfo[2].textContent,
+        authorName: authorInfo,
       };
     })
     .catch((error) => {
@@ -27,6 +26,28 @@ export async function getContentReference(link) {
 async function buildFeaturedContent(contentElem) {
   const link = contentElem.querySelectorAll('a');
   const contentInfo = await getContentReference(link[0].href);
+
+  contentElem.innerHTML = '';
+
+  const contentDiv = div({ class: 'content-details' },
+    h2(contentInfo.contentTitle),
+    div({ class: 'content-description' }, contentInfo.contentDescription),
+    div({ class: '.button-container' },
+      a({ href: link[0].href, class: 'button primary' }, 'Read More'),
+    ),
+    authorInfo.forEach((author) => {
+      const authorName = author.querySelector('.author-name')?.textContent;
+      const authorPicture = author.querySelector('.author-picture')?.src;
+
+      const authorDiv = div({ class: 'author' },
+        img({ src: authorPicture, alt: authorName }),
+        div( authorName),
+      );
+
+      contentDiv.appendChild(authorDiv);
+    })
+  );
+  contentElem.appendChild(contentDiv);
 }
 
 export default async function decorate(block) {
