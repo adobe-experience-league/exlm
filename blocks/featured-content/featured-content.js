f3611df74768f328520500adc5e709827ee0e282
Rob Rusher <rrusher@adobe.com>
2024-05-29 15:56:58 -0600

    more placeholders

--- blocks/featured-content/featured-content.js
+++ blocks/featured-content/featured-content.js
@@ -3,6 +3,12 @@ import { createOptimizedPicture } from "../../scripts/lib-franklin.js";
 import { fetchLanguagePlaceholders } from "../../scripts/scripts.js";
 
 let placeholders = {};
+try {
+  placeholders = await fetchLanguagePlaceholders();
+} catch (err) {
+  // eslint-disable-next-line no-console
+  console.error('Error fetching placeholders:', err);
+}
 
 /**
  * Fetches content from the provided link and extracts relevant information from the HTML response.
@@ -79,12 +85,6 @@ async function buildFeaturedContent(contentElem, isAdobe) {
  * @returns {Promise<void>} - A promise that resolves when the decoration is complete.
  */
 export default async function decorate(block) {
-  try {
-    placeholders = await fetchLanguagePlaceholders();
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.error('Error fetching placeholders:', err);
-  }
   const [image, content] = block.querySelectorAll(':scope div > div');
   const isAdobe = block.getAttribute('class').includes('adobe');
   image.classList.add('featured-content-image');
