d54be0cd43a65d8a92654e8381a69e4c1d59226e
Rob Rusher <rrusher@adobe.com>
2024-05-29 17:27:15 -0600

    Delete blocks/profile-tab directory

--- blocks/profile-tab/profile-tab.js
+++
@@ -1,7 +0,0 @@
-export default function ProfileTab(block) {
-  // This is a placeholder. Replace with actual implementation.
-  block.innerHTML = `
-      <a href="#">Profile Settings</a>
-      <a href="#">Bookmarks & Achievments</a>
-    `;
-}
