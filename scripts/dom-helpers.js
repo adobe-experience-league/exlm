2713fe7a943f38b076323686714355a7694508cb
Rob Rusher <rrusher@adobe.com>
2024-05-30 09:27:36 -0600

    Update dom-helpers.js
    prettier

--- scripts/dom-helpers.js
+++ scripts/dom-helpers.js
@@ -58,9 +58,7 @@ export function domEl(tag, ...items) {
   }
 
   items.forEach((item) => {
-    item = item instanceof Element || item instanceof HTMLElement
-      ? item
-      : document.createTextNode(item);
+    item = item instanceof Element || item instanceof HTMLElement ? item : document.createTextNode(item);
     element.appendChild(item);
   });
 
@@ -71,29 +69,81 @@ export function domEl(tag, ...items) {
   More short hand functions can be added for very common DOM elements below.
   domEl function from above can be used for one off DOM element occurrences.
 */
-export function div(...items) { return domEl('div', ...items); }
-export function p(...items) { return domEl('p', ...items); }
-export function a(...items) { return domEl('a', ...items); }
-export function h1(...items) { return domEl('h1', ...items); }
-export function h2(...items) { return domEl('h2', ...items); }
-export function h3(...items) { return domEl('h3', ...items); }
-export function h4(...items) { return domEl('h4', ...items); }
-export function h5(...items) { return domEl('h5', ...items); }
-export function h6(...items) { return domEl('h6', ...items); }
-export function ul(...items) { return domEl('ul', ...items); }
-export function ol(...items) { return domEl('ol', ...items); }
-export function li(...items) { return domEl('li', ...items); }
-export function i(...items) { return domEl('i', ...items); }
-export function img(...items) { return domEl('img', ...items); }
-export function span(...items) { return domEl('span', ...items); }
-export function form(...items) { return domEl('form', ...items); }
-export function input(...items) { return domEl('input', ...items); }
-export function label(...items) { return domEl('label', ...items); }
-export function button(...items) { return domEl('button', ...items); }
-export function iframe(...items) { return domEl('iframe', ...items); }
-export function nav(...items) { return domEl('nav', ...items); }
-export function fieldset(...items) { return domEl('fieldset', ...items); }
-export function article(...items) { return domEl('article', ...items); }
-export function strong(...items) { return domEl('strong', ...items); }
-export function select(...items) { return domEl('select', ...items); }
-export function option(...items) { return domEl('option', ...items); }
+export function div(...items) {
+  return domEl('div', ...items);
+}
+export function p(...items) {
+  return domEl('p', ...items);
+}
+export function a(...items) {
+  return domEl('a', ...items);
+}
+export function h1(...items) {
+  return domEl('h1', ...items);
+}
+export function h2(...items) {
+  return domEl('h2', ...items);
+}
+export function h3(...items) {
+  return domEl('h3', ...items);
+}
+export function h4(...items) {
+  return domEl('h4', ...items);
+}
+export function h5(...items) {
+  return domEl('h5', ...items);
+}
+export function h6(...items) {
+  return domEl('h6', ...items);
+}
+export function ul(...items) {
+  return domEl('ul', ...items);
+}
+export function ol(...items) {
+  return domEl('ol', ...items);
+}
+export function li(...items) {
+  return domEl('li', ...items);
+}
+export function i(...items) {
+  return domEl('i', ...items);
+}
+export function img(...items) {
+  return domEl('img', ...items);
+}
+export function span(...items) {
+  return domEl('span', ...items);
+}
+export function form(...items) {
+  return domEl('form', ...items);
+}
+export function input(...items) {
+  return domEl('input', ...items);
+}
+export function label(...items) {
+  return domEl('label', ...items);
+}
+export function button(...items) {
+  return domEl('button', ...items);
+}
+export function iframe(...items) {
+  return domEl('iframe', ...items);
+}
+export function nav(...items) {
+  return domEl('nav', ...items);
+}
+export function fieldset(...items) {
+  return domEl('fieldset', ...items);
+}
+export function article(...items) {
+  return domEl('article', ...items);
+}
+export function strong(...items) {
+  return domEl('strong', ...items);
+}
+export function select(...items) {
+  return domEl('select', ...items);
+}
+export function option(...items) {
+  return domEl('option', ...items);
+}
