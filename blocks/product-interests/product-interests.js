ef4b4b61e30da8d2f9c02e9d91983a7724b05f30
Gautham Krishna <141001403+gauthamksn@users.noreply.github.com>
2024-06-11 15:00:59 +0530

    EXLM 1597 - Product Interest block toast and styling (#976)
    * product interest init
    
    * format
    
    * styling product-interest

--- blocks/product-interests/product-interests.js
+++ blocks/product-interests/product-interests.js
@@ -1,6 +1,17 @@
 import { defaultProfileClient } from '../../scripts/auth/profile.js';
+import { sendNotice } from '../../scripts/toast/toast.js';
+import { fetchLanguagePlaceholders } from '../../scripts/scripts.js';
 
 const interestsUrl = 'https://experienceleague.adobe.com/api/interests?page_size=200&sort=Order&lang=en';
+
+/* Fetch data from the Placeholder.json */
+let placeholders = {};
+try {
+  placeholders = await fetchLanguagePlaceholders();
+} catch (err) {
+  // eslint-disable-next-line no-console
+  console.error('Error fetching placeholders:', err);
+}
 
 export async function fetchProfileData(url, cType) {
   try {
@@ -29,15 +40,20 @@ function decorateInterests(block) {
   const columnsContainer = document.createElement('ul');
   block.appendChild(columnsContainer);
   columnsContainer.classList.add('interests-container');
-  const userInterests = profileData.interests;
+  const userInterests = profileData?.interests ? profileData.interests : [];
   interests.data.forEach((interest) => {
     const column = document.createElement('li');
-    column.innerHTML = `<li class='row'><label class="checkbox">
+    column.innerHTML = `<label class="checkbox">
         <input title='${interest.Name}' type='checkbox' value='${interest.Name}'>
         <span class="subtext">${interest.Name}</span>
-    </label></li>`;
+    </label>`;
     if (interest.Nested) {
       let subColumnsContainer = columnsContainer.querySelector('li:last-child > ul');
+      const parentName = columnsContainer.querySelector('li.interest:last-child  > label').textContent.trim();
+      const childLabel = column.querySelector('span.subtext');
+      if (childLabel.textContent.trim().includes(parentName)) {
+        childLabel.textContent = childLabel.textContent.trim().replace(parentName, '');
+      }
       if (!subColumnsContainer) {
         subColumnsContainer = document.createElement('ul');
         columnsContainer.querySelector('li.interest:last-child').appendChild(subColumnsContainer);
@@ -69,9 +85,8 @@ function handleProductInterestChange(block) {
         block.querySelectorAll('li.row input:checked').forEach((input) => {
           newInterests.push(input.title);
         });
-        defaultProfileClient.updateProfile('interests', newInterests, true).then((response) => {
-          // TODO: Add toast Notification
-          console.log(response);
+        defaultProfileClient.updateProfile('interests', newInterests, true).then(() => {
+          sendNotice(placeholders.profileUpdated || 'Profile updated successfully');
         });
       }
     });
