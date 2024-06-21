0ef6d2d31749cb3775ed5ba676a49b66ed22e244
mayukhs2904 <mayukhs@adobe.com>
2024-06-21 13:36:51 +0530

    awards block changes

--- blocks/awards-block/awards-block.js
+++ blocks/awards-block/awards-block.js
@@ -1,26 +1,86 @@
-import AwardDataService from '../../scripts/data-service/award-data-service.js';
-import { getConfig } from '../../scripts/scripts.js';
+import { defaultProfileClient, isSignedInUser } from '../../scripts/auth/profile.js';
+import { decorateIcons } from '../../scripts/lib-franklin.js';
+import { fetchLanguagePlaceholders } from '../../scripts/scripts.js';
 
-const { awardUrl } = getConfig();
-
-// Function to call fetchDataFromSource and handle the data
-async function getAwardsData() {
-  const dataSource = {
-    url: awardUrl,
-  };
-  const awardDataService = new AwardDataService(dataSource);
-  try {
-    const data = await awardDataService.fetchDataFromSource();
-    if (data) {
-      console.log('Fetched data:', data);
-      // Process the data as needed
-    } else {
-      console.log('No data received');
-    }
-  } catch (error) {
-    console.error('Error fetching awards data:', error);
-  }
+let placeholders = {};
+try {
+  placeholders = await fetchLanguagePlaceholders();
+} catch (err) {
+  // eslint-disable-next-line no-console
+  console.error('Error fetching placeholders:', err);
 }
 
-// Call the function to fetch the data
-getAwardsData();
+const COMPLETED = placeholders?.completed || 'COMPLETED';
+const NO_AWARDS_YET =
+  placeholders?.noAwardsYet || 'No awards yet! Start exploring Experience League to discover what you can earn.';
+
+export default async function decorate(block) {
+  block.textContent = '';
+  const isSignedIn = await isSignedInUser();
+
+  function generateAwardCard(card) {
+    return `
+        <div class="awards-block-card">
+          <div class="awards-block-details">
+            <div class="awards-block-title">${card.Title}</div>
+            <div class="awards-block-time">${COMPLETED} ${card.Timestamp}</div>
+            <div class="awards-block-description">${card.Description}</div>
+          </div>
+          <span class="icon icon-book"></span>
+        </div>
+      `;
+  }
+
+  function generateAwardsBlock(awardDetails) {
+    return `
+        <div class="awards-block-holder">
+          ${awardDetails.map(generateAwardCard).join('')}
+        </div>
+      `;
+  }
+
+  function generateEmptyAwardsBlock() {
+    return `
+        <div class="awards-block-noAwardsYet">
+          ${NO_AWARDS_YET}
+        </div>
+      `;
+  }
+
+  function formatTimestampToMonthYear(timestamp) {
+    const date = new Date(timestamp);
+    const options = { year: 'numeric', month: 'short' };
+    const formattedMonth = date.toLocaleDateString(undefined, options).slice(0, 3).toUpperCase();
+    const year = date.getFullYear();
+    return `${formattedMonth} ${year}`;
+  }
+
+  if (isSignedIn) {
+    const profileData = await defaultProfileClient.getMergedProfile();
+    const skills = profileData?.skills;
+    const awardedSkills = skills.filter((skill) => skill.award === true);
+    const awardDetails = awardedSkills
+      .map((skill) => ({
+        originalTimestamp: skill.timestamp,
+        formattedTimestamp: formatTimestampToMonthYear(skill.timestamp),
+        title: skill.name,
+        description: skill.description,
+      }))
+      .sort((a, b) => new Date(a.originalTimestamp) - new Date(b.originalTimestamp))
+      .slice(-3)
+      .map((skill) => ({
+        Timestamp: skill.formattedTimestamp,
+        Title: skill.title,
+        Description: skill.description,
+      }));
+
+    if (awardDetails.length) {
+      const awardsBlockDiv = document.createRange().createContextualFragment(generateAwardsBlock(awardDetails));
+      block.append(awardsBlockDiv);
+    } else {
+      const awardsBlockEmptyDiv = document.createRange().createContextualFragment(generateEmptyAwardsBlock());
+      block.append(awardsBlockEmptyDiv);
+    }
+    decorateIcons(block);
+  }
+}
