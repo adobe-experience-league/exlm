68fdc981fac43b73f54fcf492fe118f2621609b9
gauthamksn <gsnair@adobe.com>
2024-06-28 22:46:57 +0530

    delete mini-toc if not needed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1077,6 +1077,9 @@ async function loadArticles() {
     }
     const contentContainer = document.createElement('div');
     contentContainer.classList.add('article-content-container');
+    if(!document.querySelector('main > .article-content-section, main > .tab-section')) {
+      document.querySelectorAll("main > .mini-toc-section").remove();
+    }
     document
       .querySelectorAll('main > .article-content-section, main > .tab-section, main > .mini-toc-section')
       .forEach((section) => {
