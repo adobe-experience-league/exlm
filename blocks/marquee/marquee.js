c7aa86b5843ffeffc7830356bb650ca75865ef17
Ruhi Singh <ruhis@adobe.com>
2024-07-03 16:11:26 +0530

    Fixed console errors for marquee and article-marquee

--- blocks/marquee/marquee.js
+++ blocks/marquee/marquee.js
@@ -112,7 +112,7 @@ export default async function decorate(block) {
     .then((isSignedInUser) => {
       if (!isSignedInUser) {
         block.classList.add('unauthenticated');
-        block.querySelector('.signin').addEventListener('click', () => window.adobeIMS.signUp());
+        block.querySelector('.signin')?.addEventListener('click', () => window.adobeIMS.signUp());
       }
     });
 
