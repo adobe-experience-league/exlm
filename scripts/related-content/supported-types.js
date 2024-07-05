9e5ecd9c89842fae4949f771a7aa4e40e11d666a
Lakshay Sharma <lakshays@adobe.com>
2024-07-05 11:29:24 -0500

    UGP-9003: small updates, cleanup, and animations

---
+++ scripts/related-content/supported-types.js
@@ -0,0 +1,12 @@
+const SUPPORTED_TYPES = {
+  Tutorial: {
+    icon: 'tutorial',
+    label: 'Tutorial'
+  },
+  Documentation: {
+    icon: 'documentation',
+    label: 'Documentation'
+  }
+}
+
+export default SUPPORTED_TYPES;
