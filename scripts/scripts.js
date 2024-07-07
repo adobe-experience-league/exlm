f81f8efe985b0beb8e8ea102f9d8d7aec56cdf1e
gauthamksn <gsnair@adobe.com>
2024-07-07 15:07:34 +0530

    mini toc issue

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1067,9 +1067,6 @@ export async function loadArticles() {
     if (!document.querySelector('main > .article-content-section, main > .tab-section')) {
       document.querySelector('main > .mini-toc-section').remove();
     } else {
-      if (document.querySelector('.mini-toc.block')) {
-        document.querySelector('.mini-toc.block').style.display = null;
-      }
       document
         .querySelectorAll('main > .article-content-section, main > .tab-section, main > .mini-toc-section')
         .forEach((section) => {
