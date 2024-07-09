ae7ccd94535d9fa3bc19686b4492fc60e3f7c6c8
Ahmed <musallam@adobe.com>
2024-07-09 15:51:56 -0500

    initial header webcomponent impl

---
+++ blocks/header/exl-header.js
@@ -0,0 +1,33 @@
+import { htmlToElement } from '../../scripts/scripts.js';
+
+const HEADER_CSS = `${window.hlx.codeBasePath}/blocks/header/header-old.css`;
+
+const headerHtml = fetch(`${window.hlx.codeBasePath}/blocks/header/header.html`);
+
+class CustomHeader extends HTMLElement {
+  constructor() {
+    super(); // Always call super first in constructor
+    // Attach a shadow root to the element.
+    this.attachShadow({ mode: 'open' });
+    this.loadStyles();
+    this.render();
+  }
+
+  loadStyles() {
+    const cssLink = htmlToElement(`<link rel="stylesheet" href="${HEADER_CSS}">`);
+    this.shadowRoot.appendChild(cssLink);
+  }
+
+  render() {
+    // DO NOT DO THIS.
+    // or you could, just besure to parameterize links, labels, sign-in behaviour ...etc
+    headerHtml.then((response) => {
+      response.text().then((html) => {
+        this.shadowRoot.appendChild(htmlToElement(html));
+      });
+    });
+    console.log('header rendered');
+  }
+}
+// Define the new element
+customElements.define('exl-header', CustomHeader);
