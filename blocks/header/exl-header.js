b2a14bbb3a075ea20a1b1a87af3cfa2b9df7452d
tbertazoli <tgerin@adobe.com>
2024-07-12 15:41:03 -0500

    created a new component and attached Shadow DOM

--- blocks/header/exl-header.js
+++ blocks/header/exl-header.js
@@ -2,31 +2,23 @@ import { htmlToElement } from '../../scripts/scripts.js';
 
 const HEADER_CSS = `${window.hlx.codeBasePath}/blocks/header/header-old.css`;
 
-const headerHtml = fetch(`${window.hlx.codeBasePath}/blocks/header/header.html`);
+/**
+ *
+ * @param {HTMLElement} headerBlock
+ */
 
 class CustomHeader extends HTMLElement {
   constructor() {
-    super(); // Always call super first in constructor
-    // Attach a shadow root to the element.
+    super();
     this.attachShadow({ mode: 'open' });
     this.loadStyles();
-    this.render();
   }
 
   loadStyles() {
     const cssLink = htmlToElement(`<link rel="stylesheet" href="${HEADER_CSS}">`);
     this.shadowRoot.appendChild(cssLink);
   }
-
-  render() {
-    // DO NOT DO THIS.
-    // or you could, just besure to parameterize links, labels, sign-in behaviour ...etc
-    headerHtml.then((response) => {
-      response.text().then((html) => {
-        this.shadowRoot.appendChild(htmlToElement(html));
-      });
-    });
-  }
 }
+
 // Define the new element
 customElements.define('exl-header', CustomHeader);
