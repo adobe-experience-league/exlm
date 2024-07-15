6397a9f714872be90137893675ce27198ff5e15c
gauthamksn <gsnair@adobe.com>
2024-07-15 15:02:11 +0530

    format

--- blocks/authorable-card/authorable-card.js
+++ blocks/authorable-card/authorable-card.js
@@ -90,7 +90,9 @@ const getCardData = async (articlePath, placeholders) => {
     thumbnail: createThumbnailURL(doc, type) || '',
     product: solutions,
     authorInfo: {
-      name: getMetadata('author-name', doc).split(',').map((name) => name.trim()),
+      name: getMetadata('author-name', doc)
+        .split(',')
+        .map((name) => name.trim()),
       type: [getMetadata('author-type', doc)],
     },
     tags: [],
