e666027a83181204afdcd6855b9da46b16cd39c5
Ahmed <musallam@adobe.com>
2024-08-19 16:16:53 -0500

    incomplete work for code sharing

--- scripts/language.js
+++ scripts/language.js
@@ -3,21 +3,44 @@ import { loadCSS } from './lib-franklin.js';
 import { htmlToElement, getPathDetails } from './scripts.js';
 
 const pathDetails = getPathDetails();
-/**
- * loads the one and only language fragment.
- */
-const loadLanguageFragment = async () => {
-  window.languagePromise =
-    window.languagePromise ||
-    new Promise((resolve) => {
-      fetch(`/fragments/en/languages/languages.plain.html`)
-        .then((response) => response.text())
-        .then((text) => {
-          resolve(text);
-        });
-    });
-  return window.languagePromise;
-};
+const defaultLanguages = [
+  {
+    lang: 'de',
+    title: 'Deutsch',
+  },
+  {
+    lang: 'fr',
+    title: 'Français',
+  },
+  {
+    lang: 'es',
+    title: 'Español',
+  },
+  {
+    lang: 'it',
+    title: 'Italiano',
+  },
+  {
+    lang: 'ja',
+    title: '日本語',
+  },
+  {
+    lang: 'ko',
+    title: '한국어',
+  },
+  {
+    lang: 'pt',
+    title: 'Português',
+  },
+  {
+    lang: 'ru',
+    title: 'Русский',
+  },
+  {
+    lang: 'zh',
+    title: '中文',
+  },
+];
 
 export const getLanguagePath = (language) => {
   const { prefix, suffix } = pathDetails;
@@ -37,37 +60,30 @@ const switchLanguage = (language) => {
 /**
  * Decoration for language popover - shared between header and footer
  */
-export const buildLanguagePopover = async (position, popoverId) => {
-  loadCSS(`${window.hlx.codeBasePath}/styles/language.css`);
+export const buildLanguagePopover = async (
+  position,
+  popoverId,
+  languages = defaultLanguages,
+  onLanguageChange = switchLanguage,
+) => {
   const popoverClass =
     position === 'top' ? 'language-selector-popover language-selector-popover--top' : 'language-selector-popover';
-  let languagesEl = htmlToElement(await loadLanguageFragment());
-  languagesEl = languagesEl.querySelector('ul');
-  const languageOptions = languagesEl?.children || [];
-  const languages = [...languageOptions].map((option) => ({
-    title: option.textContent,
-    lang: option?.firstElementChild?.getAttribute('href'),
-  }));
+  loadCSS(`${window.hlx.codeBasePath}/styles/language.css`);
 
-  const { lang: currentLang } = getPathDetails();
-  const options = languages
-    .map((option) => {
-      const lang = option.lang?.toLowerCase();
-      const selected = currentLang === lang ? 'selected' : '';
-      return `<span class="language-selector-label" data-value="${lang}" ${selected}>${option.title}</span>`;
-    })
-    .join('');
-  const popover = htmlToElement(`
-    <div class="${popoverClass}" id="${popoverId}" style="display:none">
-      ${options}
-    </div>`);
-
+  const spans = languages.map(
+    ({ lang, title }) => `<span class="language-selector-label" data-value="${lang}">${title}</span>`,
+  );
+  const popover = htmlToElement(
+    `<div class="${popoverClass}" id="${popoverId}"style="display:none">
+      ${spans.join('')}
+    </div>`,
+  );
   popover.addEventListener('click', (e) => {
     const { target } = e;
     if (target.classList.contains('language-selector-label')) {
       target.setAttribute('selected', 'true');
       const lang = target.getAttribute('data-value');
-      switchLanguage(lang);
+      onLanguageChange(lang);
     }
   });
   return {
