7f95105379718ea865c649421b6e0c63d5741259
mayukhs2904 <mayukhs@adobe.com>
2024-08-22 09:54:51 +0530

    ue changes in logic

--- blocks/profile-welcome/profile-welcome.js
+++ blocks/profile-welcome/profile-welcome.js
@@ -26,60 +26,77 @@ export default async function decorate(block) {
     block.querySelectorAll(':scope div > div');
 
   const isSignedIn = await isSignedInUser();
+
+  if (!isSignedIn) return;
+
+  let profileData = '';
+  let ppsProfileData = '';
+  let communityProfileData = '';
+  let adobeDisplayName = '';
+  let adobeFirstName = '';
+  let industry = '';
+  let roles = '';
+  let interests = '';
+  let profilePicture = '';
+  let company = '';
+  let communityUserName = '';
+  let communityUserTitle = '';
+  let communityUserLocation = '';
+  let industryContent = '';
+
   if (isSignedIn) {
-    const profileData = await defaultProfileClient.getMergedProfile();
-    const ppsProfileData = await defaultProfileClient.getPPSProfile();
-    const communityProfileData = await defaultProfileClient.fetchCommunityProfileDetails();
+    profileData = await defaultProfileClient.getMergedProfile();
+    ppsProfileData = await defaultProfileClient.getPPSProfile();
+    communityProfileData = await defaultProfileClient.fetchCommunityProfileDetails();
 
-    const adobeDisplayName = profileData?.displayName || '';
-    const adobeFirstName = profileData?.first_name || '';
-    const industry = profileData?.industryInterests || [];
-    const roles = profileData?.role || [];
-    const interests = profileData?.interests || [];
+    adobeDisplayName = profileData?.displayName || '';
+    adobeFirstName = profileData?.first_name || '';
+    industry = profileData?.industryInterests || [];
+    roles = profileData?.role || [];
+    interests = profileData?.interests || [];
 
-    const profilePicture = ppsProfileData?.images?.['100'] || '';
-    const company = ppsProfileData?.company || '';
+    profilePicture = ppsProfileData?.images?.['100'] || '';
+    company = ppsProfileData?.company || '';
 
-    const communityUserName = communityProfileData?.username || '';
-    const communityUserTitle = communityProfileData?.title || '';
-    const communityUserLocation = communityProfileData?.location || '';
+    communityUserName = communityProfileData?.username || '';
+    communityUserTitle = communityProfileData?.title || '';
+    communityUserLocation = communityProfileData?.location || '';
+  }
 
-    const roleMappings = {
-      Developer: placeholders?.roleCardDeveloperTitle || 'Developer',
-      User: placeholders?.roleCardUserTitle || 'Business User',
-      Leader: placeholders?.roleCardBusinessLeaderTitle || 'Business Leader',
-      Admin: placeholders?.roleCardAdministratorTitle || 'Administrator',
-    };
+  const roleMappings = {
+    Developer: placeholders?.roleCardDeveloperTitle || 'Developer',
+    User: placeholders?.roleCardUserTitle || 'Business User',
+    Leader: placeholders?.roleCardBusinessLeaderTitle || 'Business Leader',
+    Admin: placeholders?.roleCardAdministratorTitle || 'Administrator',
+  };
 
-    const hasInterests =
-      interests &&
-      ((Array.isArray(interests) && interests.length > 0) ||
-        (typeof interests === 'string' && interests.trim() !== ''));
+  const hasInterests =
+    interests &&
+    ((Array.isArray(interests) && interests.length > 0) || (typeof interests === 'string' && interests.trim() !== ''));
 
-    const hasIndustry =
-      industry &&
-      ((Array.isArray(industry) && industry.length > 0) || (typeof industry === 'string' && industry.trim() !== ''));
+  const hasIndustry =
+    industry &&
+    ((Array.isArray(industry) && industry.length > 0) || (typeof industry === 'string' && industry.trim() !== ''));
 
-    let industryContent = '';
-    if (hasIndustry) {
-      industryContent = `
+  if (hasIndustry) {
+    industryContent = `
           <div class="profile-user-card-industry">
             <span class="industry-heading">${placeholders?.myIndustry || 'MY INDUSTRY'}: </span>
             <span class="${!hasInterests ? 'incomplete-profile' : ''}">
               ${Array.isArray(industry) ? industry.join(', ') : industry}
             </span>
           </div>`;
-    } else if (!hasInterests) {
-      industryContent = `
+  } else if (!hasInterests) {
+    industryContent = `
           <div class="profile-user-card-industry">
             <span class="industry-heading">${placeholders?.myIndustry || 'MY INDUSTRY'}: </span>
             <span class="${!hasInterests ? 'incomplete-profile' : ''}">
               ${placeholders?.unknown || 'Unknown'}
             </span>
           </div>`;
-    }
+  }
 
-    const profileWelcomeBlock = document.createRange().createContextualFragment(`
+  const profileWelcomeBlock = document.createRange().createContextualFragment(`
         <div class="profile-curated-card">
                 <div class="profile-curated-eyebrowtext">
                 ${profileEyebrowText.innerHTML.replace('{adobeIMS.first_name}', adobeFirstName)}
@@ -91,7 +108,55 @@ export default async function decorate(block) {
                 ${profileDescription.innerHTML}
                 </div>
             </div>
-            <div class="profile-user-card">
+            ${
+              document.documentElement.classList.contains('adobe-ue-edit')
+                ? `
+              <div class="profile-user-card">
+                <div class="profile-user-card-left">
+                  <div class="profile-user-card-avatar-company-info">
+                        <div class="profile-user-card-avatar">
+                        <span class="icon icon-profile"></span>
+                        </div>
+                        <div class="profile-user-card-info">
+                            <div class="profile-user-card-name">User Name</div>
+                            ${communityUserName ? `<div class="profile-user-card-tag">User Tag</div>` : ''}
+                            <div class="profile-user-card-org">User Company</div>
+                        </div> 
+                  </div>
+                    <div class="profile-user-card-communityInfo">
+                      <div class="profile-user-card-title">
+                      <span class="heading">${
+                        placeholders?.title || 'TITLE'
+                      }: </span><span class="content"></span></div>
+                      <div class="profile-user-card-location"><span class="heading">${
+                        placeholders?.location || 'LOCATION'
+                      }: </span><span class="content"></span></div>
+                      </div>
+                      <div class="profile-user-card-incomplete">
+                        <span class="icon icon-exclamation"></span>
+                        ${incompleteProfileText.innerHTML}
+                      </div>
+                </div>
+                <div class="profile-user-card-right">
+                  <div class="profile-user-card-details">
+                    <div class="profile-user-card-role">
+                    <span class="heading">${placeholders?.myRole || 'MY ROLE'}: </span>
+                    <span class="content">User Roles</span>
+                    </div>
+                    <div class="profile-user-card-industry">
+                    <span class="heading">${placeholders?.myIndustry || 'MY INDUSTRY'}: </span>
+                    <span class="content">User Industry</span>
+                  </div>
+                    <div class="profile-user-card-interests">
+                      <span class="heading">${placeholders?.myInterests || 'MY INTERESTS'}: </span>
+                      <span class="content">User Interests</span>
+                    </div>
+                  </div>
+                    <div class="profile-user-card-cta">${decorateButton(profileCta)}</div>
+                </div>
+              </div>
+              `
+                : `<div class="profile-user-card">
                 <div class="profile-user-card-left">
                   <div class="profile-user-card-avatar-company-info">
                         <div class="profile-user-card-avatar">
@@ -154,11 +219,11 @@ export default async function decorate(block) {
                   </div>
                     <div class="profile-user-card-cta">${decorateButton(profileCta)}</div>
                 </div>    
-        </div>
+              </div>`
+            }
         `);
 
-    block.textContent = '';
-    block.append(profileWelcomeBlock);
-    decorateIcons(block);
-  }
+  block.textContent = '';
+  block.append(profileWelcomeBlock);
+  decorateIcons(block);
 }
