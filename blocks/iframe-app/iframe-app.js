e7ae54ed47e48b23d778b7d3cfbc903d7bb51135
gauthamksn <gsnair@adobe.com>
2024-08-30 15:21:47 +0530

    Iframe app init

--- blocks/iframe-app/iframe-app.js
+++ blocks/iframe-app/iframe-app.js
@@ -1,39 +1,42 @@
-// import { defaultProfileClient, isSignedInUser } from '../../scripts/auth/profile.js';
+import { getPathDetails } from '../../scripts/scripts.js';
+import { defaultProfileClient, isSignedInUser } from '../../scripts/auth/profile.js';
 
-// const authEventType = 'REQUEST_AUTH_STATE';
+const authEventType = 'REQUEST_AUTH_STATE';
 
-// TODO : Once Iframe starts working, Use this function to auth details to iframe
-// function postMessageToIframe(iframe, message) {
-//   window.addEventListener('message', async (event) => {
-//     if (event.origin === new URL(iframe.src).origin && event.data.type === authEventType) {
-//       // get current auth state
-//       const isAuthenticated = await isSignedInUser();
+const { lang } = getPathDetails();
 
-//       let imsToken;
-//       let imsProfile;
-//       if (isAuthenticated) {
-//         // yes, return the whole object response for these two, not just the token and not just the requested profile attributes. Everything.
-//         imsToken = adobeIMS.getAccessToken(); // or better add method for this in defaultProfileClient and use it here.
-//         imsProfile = await adobeIMS.getProfile(); // or better use defaultProfileClient, do not use the merged profile here. only the IMS profile.
-//       }
+async function getIMSInfo() {
+  const isAuthenticated = await isSignedInUser();
+  let imsToken;
+  let imsProfile;
+  if (isAuthenticated) {
+    imsToken = await defaultProfileClient.getIMSaccessToken();
+    imsProfile = await defaultProfileClient.getIMSProfile();
+  }
+  return {
+    isAuthenticated,
+    imsToken,
+    imsProfile,
+    locale: lang,
+  };
+}
 
-//       // Send the authenticated state back to the child window
-//       event.source.postMessage(
-//         {
-//           type: 'AUTH_STATE_RESPONSE',
-//           requestId: event.data?.payload?.requestId, // Match the request ID if provided
-//           payload: {
-//             isAuthenticated, // boolean, indicates auth state
-//             imsToken,
-//             imsProfile,
-//             locale: '', // get the current page language code
-//           },
-//         },
-//         event.origin,
-//       );
-//     }
-//   });
-// }
+function listenToMessages(iframe) {
+  window.addEventListener('message', (event) => {
+    if (event.origin === new URL(iframe.src).origin && event.data.type === authEventType) {
+      getIMSInfo().then((payload) => {
+        event.source.postMessage(
+          {
+            type: 'AUTH_STATE_RESPONSE',
+            requestId: event.data?.requestId,
+            payload,
+          },
+          event.origin,
+        );
+      });
+    }
+  });
+}
 
 export default function decorate(block) {
   const props = [...block.children].map((row) => row.firstElementChild);
@@ -45,4 +48,5 @@ export default function decorate(block) {
   iframe.style.border = 'none';
   block.textContent = '';
   block.append(iframe);
+  listenToMessages(iframe);
 }
