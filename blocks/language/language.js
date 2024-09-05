6e4559ee675f3e27bb8cd6895e8209c3c9c08586
Ahmed <musallam@adobe.com>
2024-09-05 12:14:21 -0500

    refactor, remove unnecessary type param

--- blocks/language/language.js
+++ blocks/language/language.js
@@ -2,63 +2,27 @@ import { decorateIcons, loadCSS } from '../../scripts/lib-franklin.js';
 import { htmlToElement, getPathDetails } from '../../scripts/scripts.js';
 import { getCell } from '../header/header-utils.js';
 
+/**
+ * @typedef {Object} Language
+ * @property {string} lang
+ * @property {string} title
+ * @property {boolean} isCommunity
+ */
+
 const pathDetails = getPathDetails();
 const defaultLanguages = [
-  {
-    lang: 'de',
-    title: 'Deutsch',
-    isCommunity: true,
-  },
-  {
-    lang: 'en',
-    title: 'English',
-    isCommunity: true,
-  },
-  {
-    lang: 'es',
-    title: 'Español',
-    isCommunity: true,
-  },
-  {
-    lang: 'fr',
-    title: 'Français',
-    isCommunity: true,
-  },
-  {
-    lang: 'it',
-    title: 'Italiano',
-  },
-  {
-    lang: 'nl',
-    title: 'Nederlands',
-  },
-  {
-    lang: 'pt-br',
-    title: 'Português',
-    isCommunity: true,
-  },
-  {
-    lang: 'sv',
-    title: 'Svenska',
-  },
-  {
-    lang: 'zh-hans',
-    title: '中文 (简体)',
-  },
-  {
-    lang: 'zh-hant',
-    title: '中文 (繁體)',
-  },
-  {
-    lang: 'ja',
-    title: '日本語',
-    isCommunity: true,
-  },
-  {
-    lang: 'ko',
-    title: '한국어',
-    isCommunity: true,
-  },
+  { lang: 'de', title: 'Deutsch', isCommunity: true },
+  { lang: 'en', title: 'English', isCommunity: true },
+  { lang: 'es', title: 'Español', isCommunity: true },
+  { lang: 'fr', title: 'Français', isCommunity: true },
+  { lang: 'it', title: 'Italiano' },
+  { lang: 'nl', title: 'Nederlands' },
+  { lang: 'pt-br', title: 'Português', isCommunity: true },
+  { lang: 'sv', title: 'Svenska' },
+  { lang: 'zh-hans', title: '中文 (简体)' },
+  { lang: 'zh-hant', title: '中文 (繁體)' },
+  { lang: 'ja', title: '日本語', isCommunity: true },
+  { lang: 'ko', title: '한국어', isCommunity: true },
 ];
 
 const getLanguagePath = (language) => {
@@ -81,12 +45,17 @@ const loadStyles = async () => {
 };
 
 export default class LanguageBlock extends HTMLElement {
-  constructor(position, popoverId, block, type, languages = defaultLanguages) {
+  /**
+   * @param {'top'|'bottom'} position
+   * @param {string} popoverId
+   * @param {HTMLElement} block
+   * @param {Language[]|undefined} languages
+   */
+  constructor(position, popoverId, block, languages = defaultLanguages) {
     super();
     this.position = position;
     this.popoverId = popoverId;
     this.block = block;
-    this.type = type;
     this.languages = languages;
     this.title = getCell(this.block, 1, 1)?.firstChild.textContent;
     this.isCommunity = this.block.classList.contains('community');
@@ -94,9 +63,9 @@ export default class LanguageBlock extends HTMLElement {
 
   decorateButton = async () => {
     const languageHtml = htmlToElement(`
-    <button type="button" class="language-selector-button" aria-haspopup="true" aria-controls="language-picker-popover-${this.type}" aria-label="${this.title}">
+    <button type="button" class="language-selector-button" aria-haspopup="true" aria-controls="${this.popoverId}" aria-label="${this.title}">
       <span class="icon icon-globegrid"></span>
-    </button>        
+    </button>
   `);
     this.appendChild(languageHtml);
   };
