7cf0fbcdc40ae1e152af7f3b7de6ea787b97435b
Ahmed <musallam@adobe.com>
2024-09-06 14:09:36 -0500

    use firstChild to avoid potential issue if helix pipeline did not render an element

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -218,6 +218,9 @@ const hamburgerButton = (navWrapper, navOverlay) => {
  * @param {HTMLUListElement} ul
  */
 const buildNavItems = async (ul, level = 0) => {
+  /**
+   * @param {HTMLElement} navItem
+   */
   const decorateNavItem = async (navItem) => {
     const navItemClasses = ['nav-item'];
     if (level === 0) navItemClasses.push('nav-item-root');
@@ -226,7 +229,7 @@ const buildNavItems = async (ul, level = 0) => {
     const [content, secondaryContent] = navItem.querySelectorAll(':scope > ul');
 
     if (content) {
-      const firstEl = navItem.firstElementChild;
+      const firstEl = navItem.firstChild;
       const toggleClass = level === 0 ? 'nav-item-toggle nav-item-toggle-root' : 'nav-item-toggle';
       const toggler = htmlToElement(
         `<button class="${toggleClass}" aria-controls="${controlName}" aria-expanded="false">${firstEl.textContent}</button>`,
