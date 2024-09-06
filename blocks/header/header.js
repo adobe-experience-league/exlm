f5fedef845d0dde32b21dad17b40408478abdd34
Ahmed <musallam@adobe.com>
2024-09-06 14:43:41 -0500

    Revert "use firstChild instead of firstElement"
    This reverts commit 9f263cdce80cc0200bfa577c376e34824c33498f.

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -259,10 +259,6 @@ const hamburgerButton = (navWrapper, navOverlay) => {
  * @param {HTMLUListElement} ul
  */
 const buildNavItems = async (ul, level = 0) => {
-  /**
-   *
-   * @param {HTMLElement} navItem
-   */
   const decorateNavItem = async (navItem) => {
     const navItemClasses = ['nav-item'];
     if (level === 0) navItemClasses.push('nav-item-root');
@@ -270,7 +266,7 @@ const buildNavItems = async (ul, level = 0) => {
     const controlName = `content-${level}-${randomId()}`; // unique id
     const [content, secondaryContent] = navItem.querySelectorAll(':scope > ul');
     if (content) {
-      const firstEl = navItem.firstChild;
+      const firstEl = navItem.firstElementChild;
       const toggleClass = level === 0 ? 'nav-item-toggle nav-item-toggle-root' : 'nav-item-toggle';
       const toggler = htmlToElement(
         `<button class="${toggleClass}" aria-controls="${controlName}" aria-expanded="false">${firstEl.textContent}</button>`,
