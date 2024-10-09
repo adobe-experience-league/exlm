82eda06716609035ec45f4c41ad3a5ab84d7a4f9
Gautham Krishna <141001403+gauthamksn@users.noreply.github.com>
2024-10-09 18:23:33 +0530

    target fail conditions (#1346)

--- blocks/recently-reviewed/recently-reviewed.js
+++ blocks/recently-reviewed/recently-reviewed.js
@@ -1,5 +1,10 @@
 import { getConfig, fetchLanguagePlaceholders, htmlToElement } from '../../scripts/scripts.js';
-import { handleTargetEvent, checkTargetSupport, targetDataAdapter } from '../../scripts/target/target.js';
+import {
+  handleTargetEvent,
+  checkTargetSupport,
+  targetDataAdapter,
+  updateCopyFromTarget,
+} from '../../scripts/target/target.js';
 import BuildPlaceholder from '../../scripts/browse-card/browse-card-placeholder.js';
 import { buildCard, buildNoResultsContent } from '../../scripts/browse-card/browse-card.js';
 import Swiper from '../../scripts/swiper/swiper.js';
@@ -33,52 +38,54 @@ function renderNavigationArrows(titleContainer) {
 }
 
 export default async function decorate(block) {
-  block.style.display = 'none';
-  const [headingElement, descriptionElement] = [...block.children].map((row) => row.firstElementChild);
-  headingElement.classList.add('recently-reviewed-header');
-  descriptionElement.classList.add('recently-reviewed-description');
-  const titleContainer = document.createElement('div');
-  const navContainer = document.createElement('div');
-  block.appendChild(navContainer);
-  navContainer.classList.add('recently-viewed-nav-container');
-  navContainer.appendChild(titleContainer);
-  titleContainer.appendChild(headingElement);
-  titleContainer.appendChild(descriptionElement);
-  renderNavigationArrows(navContainer);
+  checkTargetSupport().then((targetSupport) => {
+    block.style.display = 'none';
+    const [headingElement, descriptionElement] = [...block.children].map((row) => row.firstElementChild);
+    headingElement.classList.add('recently-reviewed-header');
+    descriptionElement.classList.add('recently-reviewed-description');
+    const titleContainer = document.createElement('div');
+    const navContainer = document.createElement('div');
+    block.appendChild(navContainer);
+    navContainer.classList.add('recently-viewed-nav-container');
+    navContainer.appendChild(titleContainer);
+    titleContainer.appendChild(headingElement);
+    titleContainer.appendChild(descriptionElement);
+    renderNavigationArrows(navContainer);
 
-  const contentDiv = document.createElement('div');
-  contentDiv.className = 'browse-cards-block-content';
-  block.appendChild(contentDiv);
+    const contentDiv = document.createElement('div');
+    contentDiv.className = 'browse-cards-block-content';
+    block.appendChild(contentDiv);
 
-  const buildCardsShimmer = new BuildPlaceholder();
-  buildCardsShimmer.add(block);
+    const buildCardsShimmer = new BuildPlaceholder();
+    buildCardsShimmer.add(block);
 
-  if (window.hlx.aemRoot) {
-    block.style.display = 'block';
-    buildNoResultsContent(contentDiv, true, authorInfo);
-    buildCardsShimmer.remove();
-  }
-
-  const targetSupport = checkTargetSupport();
-  if (targetSupport) {
-    block.style.display = 'block';
-    handleTargetEvent(targetCriteriaIds.recentlyViewed).then(({ data }) => {
-      if (data.length) {
-        data.forEach((item) => {
-          const cardData = targetDataAdapter(item, placeholders);
-          const cardDiv = document.createElement('div');
-          buildCard(contentDiv, cardDiv, cardData);
-          contentDiv.appendChild(cardDiv);
-        });
-        const prevButton = block.querySelector('.recently-viewed-nav-section > .prev-nav');
-        const nextButton = block.querySelector('.recently-viewed-nav-section > .next-nav');
-        const items = contentDiv.querySelectorAll('.browse-cards-block-content > div');
-        // eslint-disable-next-line no-new
-        new Swiper(contentDiv, items, true, null, prevButton, nextButton);
-      } else {
-        buildNoResultsContent(contentDiv, true);
-      }
+    if (window.hlx.aemRoot) {
+      block.style.display = 'block';
+      buildNoResultsContent(contentDiv, true, authorInfo);
       buildCardsShimmer.remove();
-    });
-  }
+    }
+
+    if (targetSupport) {
+      block.style.display = 'block';
+      handleTargetEvent(targetCriteriaIds.recentlyViewed).then((resp) => {
+        updateCopyFromTarget(resp, headingElement, descriptionElement);
+        if (resp.data.length) {
+          resp.data.forEach((item) => {
+            const cardData = targetDataAdapter(item, placeholders);
+            const cardDiv = document.createElement('div');
+            buildCard(contentDiv, cardDiv, cardData);
+            contentDiv.appendChild(cardDiv);
+          });
+          const prevButton = block.querySelector('.recently-viewed-nav-section > .prev-nav');
+          const nextButton = block.querySelector('.recently-viewed-nav-section > .next-nav');
+          const items = contentDiv.querySelectorAll('.browse-cards-block-content > div');
+          // eslint-disable-next-line no-new
+          new Swiper(contentDiv, items, true, null, prevButton, nextButton);
+        } else {
+          buildNoResultsContent(contentDiv, true);
+        }
+        buildCardsShimmer.remove();
+      });
+    }
+  });
 }
