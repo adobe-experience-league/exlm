e72e67e588da0fef33c997d86d128489ca1b082b
Ruhi Singh <ruhis@adobe.com>
2024-10-21 19:43:07 +0530

    EXLM-2417 Hide empty section for recently reviewed block

--- blocks/recently-reviewed/recently-reviewed.js
+++ blocks/recently-reviewed/recently-reviewed.js
@@ -13,16 +13,7 @@ import { decorateIcons } from '../../scripts/lib-franklin.js';
 
 const UEAuthorMode = window.hlx.aemRoot || window.location.href.includes('.html');
 const { targetCriteriaIds } = getConfig();
-
-const authorInfo = 'Based on profile context, if the customer has enabled the necessary cookies';
-
 let placeholders = {};
-try {
-  placeholders = await fetchLanguagePlaceholders();
-} catch (err) {
-  // eslint-disable-next-line no-console
-  console.error('Error fetching placeholders:', err);
-}
 
 function renderNavigationArrows(titleContainer) {
   const navigationElements = htmlToElement(`
@@ -40,19 +31,38 @@ function renderNavigationArrows(titleContainer) {
 }
 
 export default async function decorate(block) {
+  try {
+    placeholders = await fetchLanguagePlaceholders();
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('Error fetching placeholders:', err);
+  }
+
   checkTargetSupport().then((targetSupport) => {
     const [headingElement, descriptionElement] = [...block.children].map((row) => row.firstElementChild);
     headingElement.classList.add('recently-reviewed-header');
     descriptionElement.classList.add('recently-reviewed-description');
+
     const titleContainer = document.createElement('div');
     const navContainer = document.createElement('div');
     const contentDiv = document.createElement('div');
     contentDiv.className = 'browse-cards-block-content';
     const buildCardsShimmer = new BuildPlaceholder();
 
-    if (window.hlx.aemRoot) {
+    function appendNavAndContent() {
+      navContainer.classList.add('recently-viewed-nav-container');
+      navContainer.appendChild(titleContainer);
+      titleContainer.appendChild(headingElement);
+      titleContainer.appendChild(descriptionElement);
+      renderNavigationArrows(navContainer);
+      block.appendChild(navContainer);
       block.appendChild(contentDiv);
+    }
+
+    if (window.hlx.aemRoot) {
+      appendNavAndContent();
       buildCardsShimmer.add(block);
+      const authorInfo = 'Based on profile context, if the customer has enabled the necessary cookies';
       buildNoResultsContent(contentDiv, true, authorInfo);
       buildCardsShimmer.remove();
     }
@@ -61,13 +71,7 @@ export default async function decorate(block) {
       handleTargetEvent(targetCriteriaIds.recentlyViewed).then((resp) => {
         updateCopyFromTarget(resp, headingElement, descriptionElement);
         if (resp?.data.length) {
-          block.appendChild(navContainer);
-          navContainer.classList.add('recently-viewed-nav-container');
-          navContainer.appendChild(titleContainer);
-          titleContainer.appendChild(headingElement);
-          titleContainer.appendChild(descriptionElement);
-          renderNavigationArrows(navContainer);
-          block.appendChild(contentDiv);
+          appendNavAndContent();
           buildCardsShimmer.add(block);
 
           resp.data.forEach((item) => {
@@ -76,6 +80,7 @@ export default async function decorate(block) {
             buildCard(contentDiv, cardDiv, cardData);
             contentDiv.appendChild(cardDiv);
           });
+
           const prevButton = block.querySelector('.recently-viewed-nav-section > .prev-nav');
           const nextButton = block.querySelector('.recently-viewed-nav-section > .next-nav');
           const items = contentDiv.querySelectorAll('.browse-cards-block-content > div');
