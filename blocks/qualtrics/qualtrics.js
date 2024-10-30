fab86fe3ce634c94ec34cb82695b5643e04da3f0
Ruhi Singh <ruhis@adobe.com>
2024-10-30 15:32:55 +0530

    EXLM-2439 Qualtrics block

--- blocks/qualtrics/qualtrics.js
+++ blocks/qualtrics/qualtrics.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
   const [textElement] = [...block.children].map((row) => row.firstElementChild);
-  const qualtricsSelector = textElement?.innerHTML || '';
+  const qualtricsSelector = textElement?.textContent || '';
 
   block.innerHTML = '';
   block.setAttribute('data-qualtrics-selector', qualtricsSelector);
