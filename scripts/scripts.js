42663a1b7fddedd954a727ac9583548ae06a3306
Noor <nmohamme@adobe.com>
2024-11-20 20:49:26 +0530

    ...

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -201,7 +201,7 @@ function addMiniToc(main) {
   tocSection.classList.add('mini-toc-section');
   const miniTocBlock = buildBlock('mini-toc', []);
   tocSection.append(miniTocBlock);
-  miniTocBlock.style.display = 'none';
+  // miniTocBlock.style.display = 'none';
   main.append(tocSection);
 }
 
@@ -900,7 +900,35 @@ async function loadRails() {
 export async function loadArticles() {
   if (isPerspectivePage) {
     loadCSS(`${window.hlx.codeBasePath}/scripts/articles/articles.css`);
-    loadDefaultModule('./articles/articles.js');
+    // loadDefaultModule('./articles/articles.js');
+    const main = document.querySelector('main');
+  main.classList.add('articles-wrapper');
+  const [marquee, articleBody] = main.children;
+  if (marquee) {
+    marquee.classList.add('articles-marquee');
+  }
+  if (articleBody) {
+    articleBody.classList.add('articles-body');
+  }
+  const contentContainer = document.createElement('div');
+  contentContainer.classList.add('article-content-container');
+  if (!document.querySelector('main > .article-content-section, main > .tab-section')) {
+    document.querySelector('main > .mini-toc-section').remove();
+  } else {
+    if (document.querySelector('.mini-toc')) {
+      document.querySelector('.mini-toc').style.display = null;
+    }
+    document
+      .querySelectorAll('main > .article-content-section, main > .tab-section, main > .mini-toc-section')
+      .forEach((section) => {
+        contentContainer.append(section);
+      });
+    if (document.querySelector('.article-header-section')) {
+      document.querySelector('.article-header-section').after(contentContainer);
+    } else {
+      document.querySelector('main').prepend(contentContainer);
+    }
+  }
   }
 }
 
@@ -1156,7 +1184,7 @@ async function loadPage() {
   await loadEager(document);
   createDocColumns();
   loadRails();
-  // loadArticles();
+  loadArticles();
   await loadLazy(document);
   loadDelayed();
   showSignupDialog();
