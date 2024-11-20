42ac0da8c654cc70299d78b443c10b227cf968da
Noor <nmohamme@adobe.com>
2024-11-20 20:56:00 +0530

    ...

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -900,35 +900,7 @@ async function loadRails() {
 export async function loadArticles() {
   if (isPerspectivePage) {
     loadCSS(`${window.hlx.codeBasePath}/scripts/articles/articles.css`);
-    // loadDefaultModule('./articles/articles.js');
-    const main = document.querySelector('main');
-  main.classList.add('articles-wrapper');
-  const [marquee, articleBody] = main.children;
-  if (marquee) {
-    marquee.classList.add('articles-marquee');
-  }
-  if (articleBody) {
-    articleBody.classList.add('articles-body');
-  }
-  const contentContainer = document.createElement('div');
-  contentContainer.classList.add('article-content-container');
-  if (!document.querySelector('main > .article-content-section, main > .tab-section')) {
-    document.querySelector('main > .mini-toc-section').remove();
-  } else {
-    if (document.querySelector('.mini-toc')) {
-      document.querySelector('.mini-toc').style.display = null;
-    }
-    document
-      .querySelectorAll('main > .article-content-section, main > .tab-section, main > .mini-toc-section')
-      .forEach((section) => {
-        contentContainer.append(section);
-      });
-    if (document.querySelector('.article-header-section')) {
-      document.querySelector('.article-header-section').after(contentContainer);
-    } else {
-      document.querySelector('main').prepend(contentContainer);
-    }
-  }
+    await loadDefaultModule('./articles/articles.js');
   }
 }
 
@@ -1184,8 +1156,8 @@ async function loadPage() {
   await loadEager(document);
   createDocColumns();
   loadRails();
-  loadArticles();
   await loadLazy(document);
+  loadArticles();
   loadDelayed();
   showSignupDialog();
 
