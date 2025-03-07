38765bf924383f91d4e588ea4375f3e525420685
gauthamksn <gsnair@adobe.com>
2025-03-07 13:54:39 +0530

    change feature flag name

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -529,7 +529,7 @@ async function buildPreMain(main) {
   const hideBanner = getMetadata('feature-flags')
     .split(',')
     .map((t) => t.toLowerCase().trim())
-    .includes('hide-notification-banner');
+    .includes('hide-fragment');
 
   if (currentPath.endsWith(fragmentPath) || hideBanner) {
     return; // do not load fragment if it is the same as the current page or feature flag is present
