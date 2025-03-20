86b628ef6e96713ce801dd962b16a6aa4a09c5e3
Ruhi Singh <ruhis@adobe.com>
2025-03-20 20:42:55 +0530

    EXLM-3191 Block options

--- blocks/block-quote/block-quote.js
+++ blocks/block-quote/block-quote.js
@@ -1,14 +1,3 @@
 export default function decorate(block) {
-  const [styleElement, textElement] = [...block.children].map((row) => row.firstElementChild);
-
-  const blockStyle = styleElement?.textContent.trim();
-  const blockText = textElement?.innerHTML;
-
-  block.innerHTML = '';
-  const contentDiv = document.createElement('div');
-  contentDiv.innerHTML = blockText;
-
-  // Append the content div to the block
-  block.append(contentDiv);
-  block.classList.add('block-quote-content-test', blockStyle);
+  block.classList.add('block-quote-content');
 }
