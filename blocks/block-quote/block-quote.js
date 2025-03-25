748627fff980c41eeb1faf00f9d0e38a22dfa9b7
Ruhi Singh <ruhis@adobe.com>
2025-03-25 14:18:02 +0530

    EXLM-3191 Block options

--- blocks/block-quote/block-quote.js
+++ blocks/block-quote/block-quote.js
@@ -1,3 +1,5 @@
 export default function decorate(block) {
-  block.classList.add('block-quote-content');
+  if (!block.classList.contains('block-quote-content')) {
+    block.classList.add('block-quote-content');
+  }
 }
