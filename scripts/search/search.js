422431abc3b816b039d9d6bf33675106b82e48e1
gauthamksn <gsnair@adobe.com>
2025-05-05 14:13:38 +0530

    refactor

--- scripts/search/search.js
+++ scripts/search/search.js
@@ -9,7 +9,8 @@ const solution = getMetadata('solution')?.split(',')[0].trim();
 
 // Redirects to the search page based on the provided search input and filters
 export const redirectToSearchPage = (searchUrl, searchInput, filters = '') => {
-  let targetUrlWithLanguage = searchUrl.includes('.html') ? `${searchUrl}?lang=${languageCode}` : searchUrl;
+  const isLegacySearch = searchUrl.includes('search.html');
+  let targetUrlWithLanguage = isLegacySearch ? `${searchUrl}?lang=${languageCode}` : searchUrl;
   const filterValue = filters && filters.toLowerCase() === 'all' ? '' : filters;
   if (searchInput) {
     const trimmedSearchInput = encodeURIComponent(searchInput.trim());
@@ -19,15 +20,13 @@ export const redirectToSearchPage = (searchUrl, searchInput, filters = '') => {
   }
   if (filterValue) {
     const filterValueEncoded = encodeURIComponent(filterValue);
-    targetUrlWithLanguage += targetUrlWithLanguage.includes('.html')
+    targetUrlWithLanguage += isLegacySearch
       ? `&f:@el_contenttype=[${filterValueEncoded}]`
       : `&f-@el_contenttype=${filterValueEncoded}`;
   }
   if (solution) {
     const solutionEncoded = encodeURIComponent(solution);
-    targetUrlWithLanguage += targetUrlWithLanguage.includes('.html')
-      ? `&f:el_product=[${solutionEncoded}]`
-      : `&f-el_product=${solutionEncoded}`;
+    targetUrlWithLanguage += isLegacySearch ? `&f:el_product=[${solutionEncoded}]` : `&f-el_product=${solutionEncoded}`;
   }
 
   window.location.href = targetUrlWithLanguage;
