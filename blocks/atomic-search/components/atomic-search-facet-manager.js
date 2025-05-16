6fecb4bb7ae7a8ae6c5f2c3d82d864bdce8dc62c
Varun A <varuna@adobe.com>
2025-05-16 20:09:11 +0530

    Remove facet

--- blocks/atomic-search/components/atomic-search-facet-manager.js
+++ blocks/atomic-search/components/atomic-search-facet-manager.js
@@ -1,6 +1,6 @@
 import { waitForChildElement, debounce, CUSTOM_EVENTS, isMobile } from './atomic-search-utils.js';
 
-const orderedFacetIds = ['facetContentType', 'facetStatus', 'facetProduct', 'facetRole', 'facetDate'];
+const orderedFacetIds = ['facetContentType', 'facetStatus', 'facetProduct', 'facetRole'];
 
 export default function atomicFacetManagerHandler(baseElement) {
   let resizeObserver;
