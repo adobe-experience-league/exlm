214e611bf23ea245555b8d1a16be4b5002fcf749
Varun A <varuna@adobe.com>
2025-05-16 20:07:52 +0530

    remove timeframe facet code

--- blocks/atomic-search/components/atomic-search-facet-manager.js
+++ blocks/atomic-search/components/atomic-search-facet-manager.js
@@ -7,9 +7,7 @@ export default function atomicFacetManagerHandler(baseElement) {
   let debounceTimer;
 
   const reorderFacets = () => {
-    const currentOrder = Array.from(baseElement.querySelectorAll('atomic-facet,atomic-timeframe-facet')).map(
-      (el) => el.id,
-    );
+    const currentOrder = Array.from(baseElement.querySelectorAll('atomic-facet')).map((el) => el.id);
     const isCorrectOrder = orderedFacetIds.every((id, index) => currentOrder[index] === id);
     if (isCorrectOrder) return;
 
