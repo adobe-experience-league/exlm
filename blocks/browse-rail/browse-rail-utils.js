948488a3f209905c44f3ac4e27a7d5e33fa03e8a
Ruhi Singh <ruhis@adobe.com>
2025-05-20 12:08:05 +0530

    Fix Browse rail sub page issue

--- blocks/browse-rail/browse-rail-utils.js
+++ blocks/browse-rail/browse-rail-utils.js
@@ -1,7 +1,7 @@
 import { getLink } from '../../scripts/scripts.js';
 // Utility function to filter sub-pages under a given path
 export const filterSubPages = (data, basePath) =>
-  data.filter((page) => page.path.startsWith(basePath) && page.path !== basePath);
+  data.filter((page) => page.path.startsWith(`${basePath}/`) && page.path !== basePath);
 
 // Utility function to build a multi-map from the filtered sub-pages
 export function convertToMultiMap(jsonData, page) {
