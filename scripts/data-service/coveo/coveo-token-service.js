76a79d98dc1be524a1a433abf5218ad5fb04f6d8
Joseph Ross <joross+adobe@adobe.com>
2025-05-23 11:00:10 -0400

    Readded setting coveo token in session for coveo analytics usage

--- scripts/data-service/coveo/coveo-token-service.js
+++ scripts/data-service/coveo/coveo-token-service.js
@@ -1,6 +1,8 @@
 import { getConfig } from '../../scripts.js';
+import { COVEO_TOKEN } from '../../session-keys.js';
 
 export default async function loadCoveoToken() {
   const { coveoToken } = getConfig();
+  sessionStorage.setItem(COVEO_TOKEN, coveoToken);
   return coveoToken;
 }
