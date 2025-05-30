c328ad01c34f5642860ea4d5c2403408ee5d16ca
GhnanaPrasoona <gyesireddy@adobe.com>
2025-05-30 11:06:22 +0530

    Embed Video into Marquee component

--- blocks/marquee/marquee.js
+++ blocks/marquee/marquee.js
@@ -213,7 +213,7 @@ export default async function decorate(block) {
       Object.assign(imgEl.style, {
         width: '100%',
         height: '100%',
-        objectFit: 'cover',
+        objectFit: 'contain',
         objectPosition: 'center',
       });
       subjectEl.appendChild(imgEl);
