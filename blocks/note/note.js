511b1c8ad6a36c06ca9927d0afa3d8dc8dc40934
Ruhi Singh <ruhis@adobe.com>
2025-06-04 17:13:23 +0530

    EXLM-3517 Admonition titles localization for Note Block

--- blocks/note/note.js
+++ blocks/note/note.js
@@ -1,4 +1,5 @@
-import { decorateIcons } from '../../scripts/lib-franklin.js';
+import { decorateIcons, toCamelCase } from '../../scripts/lib-franklin.js';
+import { fetchLanguagePlaceholders } from '../../scripts/scripts.js';
 
 const iconMapping = {
   important: 'alert',
@@ -9,7 +10,17 @@ const iconMapping = {
 };
 
 export default async function decorate(block) {
+  let placeholders = {};
+  try {
+    placeholders = await fetchLanguagePlaceholders();
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('Error fetching placeholders:', err);
+  }
+
   const [noteTypeElement] = [...block.children].map((row) => row.firstElementChild);
+  noteTypeElement.textContent =
+    placeholders?.[`note${toCamelCase(noteTypeElement.textContent)}Label`] || noteTypeElement.textContent;
 
   // Shouldn't apply to MD Github pages
   if (noteTypeElement && !noteTypeElement.querySelector('span.icon')) {
