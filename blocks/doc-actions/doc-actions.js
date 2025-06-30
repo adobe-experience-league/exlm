4c310404477c00f1589a7ed4d79110ee6a4f74c1
Sreeksha Akkala <sakkala@adobe.com>
2025-06-30 17:56:52 +0530

    EXLM-3593 | removed empty toggle and code clean up

--- blocks/doc-actions/doc-actions.js
+++ blocks/doc-actions/doc-actions.js
@@ -1,60 +1,9 @@
-import { loadBlocks, decorateIcons } from '../../scripts/lib-franklin.js';
-import {
-  createTag,
-  isDocPage,
-  htmlToElement,
-  decorateMain,
-  fetchLanguagePlaceholders,
-  getConfig,
-} from '../../scripts/scripts.js';
+import { decorateIcons } from '../../scripts/lib-franklin.js';
+import { createTag, isDocPage, fetchLanguagePlaceholders, getConfig } from '../../scripts/scripts.js';
 import { assetInteractionModel } from '../../scripts/analytics/lib-analytics.js';
 import UserActions from '../../scripts/user-actions/user-actions.js';
 
 const { automaticTranslationLink } = getConfig();
-
-async function getTranslatedDocContent() {
-  const docPath = `/en/${window.location.pathname.replace(/^(?:[^/]*\/){2}\s*/, '')}`;
-  const docResponse = await fetch(`${docPath}.plain.html`);
-  const translatedDoc = await docResponse.text();
-  const docElement = htmlToElement(`<div>${translatedDoc}</div>`);
-  decorateMain(docElement);
-  await loadBlocks(docElement);
-  return docElement.querySelector(':scope > div:first-child');
-}
-
-async function toggleContent(isChecked, docContainer) {
-  window.exl = window.exl || {};
-
-  if (isChecked && !window.exl.translatedDocElement) {
-    window.exl.translatedDocElement = await getTranslatedDocContent();
-  }
-  if (isChecked) {
-    const docActionsMobile = docContainer.querySelector('.doc-actions-mobile');
-    if (docActionsMobile) {
-      const createdByEl = window.exl.translatedDocElement.querySelector('.article-metadata-createdby-wrapper');
-      const articleMetaDataEl = window.exl.translatedDocElement.querySelector('.article-metadata-wrapper');
-      if (articleMetaDataEl.nextSibling === createdByEl) {
-        createdByEl.appendChild(docActionsMobile);
-      } else if (articleMetaDataEl) {
-        articleMetaDataEl.appendChild(docActionsMobile);
-      }
-    }
-    docContainer.replaceWith(window.exl.translatedDocElement);
-  } else {
-    const dc = document.querySelector('main > div:first-child');
-    const docActionsMobile = window.exl.translatedDocElement.querySelector('.doc-actions-mobile');
-    if (docActionsMobile) {
-      const createdByEl = docContainer.querySelector('.article-metadata-createdby-wrapper');
-      const articleMetaDataEl = docContainer.querySelector('.article-metadata-wrapper');
-      if (articleMetaDataEl.nextSibling === createdByEl) {
-        createdByEl.appendChild(docActionsMobile);
-      } else if (articleMetaDataEl) {
-        articleMetaDataEl.appendChild(docActionsMobile);
-      }
-    }
-    dc.replaceWith(docContainer);
-  }
-}
 
 async function decorateLanguageToggle(block, placeholders) {
   if (
@@ -66,34 +15,25 @@ async function decorateLanguageToggle(block, placeholders) {
       { class: 'doc-mt-toggle' },
       `<div class="doc-mt-checkbox">
       <span>${placeholders.automaticTranslation}</span>
-      <input type="checkbox"><a href="${automaticTranslationLink}" target="_blank"><span class="icon icon-info"></span></a>
+      <a href="${automaticTranslationLink}" target="_blank" title="${
+        placeholders.changeLanguageTooltip || 'to change language pls click on globe icon'
+      }"><span class="icon icon-info"></span></a>
       </div>
       <div class="doc-mt-feedback">
         <span class="prompt">${placeholders.automaticTranslationFeedback}</span>
         <div class="doc-mt-feedback-radio">
-          <label class="radio"><input type="radio" name="helpful-translation" value="yes">${placeholders.automaticTranslationFeedbackYes}</label>
-          <label class="radio"><input type="radio" name="helpful-translation" value="no">${placeholders.automaticTranslationFeedbackNo}</label>
+          <label class="radio"><input type="radio" name="helpful-translation" value="yes">${
+            placeholders.automaticTranslationFeedbackYes
+          }</label>
+          <label class="radio"><input type="radio" name="helpful-translation" value="no">${
+            placeholders.automaticTranslationFeedbackNo
+          }</label>
         </div>
       </div>`,
     );
     // addToDocActions(languageToggleElement, block);
     block.appendChild(languageToggleElement);
     decorateIcons(block);
-    const desktopAndMobileLangToggles = document.querySelectorAll(
-      '.doc-mt-toggle .doc-mt-checkbox input[type="checkbox"]',
-    );
-    const docContainer = document.querySelector('main > div:first-child');
-
-    [...desktopAndMobileLangToggles].forEach((langToggle) => {
-      if (!langToggle.parentElement.classList.contains('listener')) {
-        langToggle.addEventListener('change', async (e) => {
-          const { checked } = e.target;
-          await toggleContent(checked, docContainer);
-          assetInteractionModel(null, `automatic translation ${e.target.checked ? 'off' : 'on'}`);
-        });
-        langToggle.parentElement.classList.add('listener');
-      }
-    });
 
     const desktopAndMobileRadioFeedback = document.querySelectorAll(
       '.doc-mt-toggle .doc-mt-feedback input[type="radio"]',
