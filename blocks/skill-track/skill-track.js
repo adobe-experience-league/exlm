bf481c7517abcac412a8f0b9bd900f14a2f2b39b
GhnanaPrasoona <gyesireddy+adobe@adobe.com>
2025-08-05 15:00:09 +0530

    skill-track authoring

--- blocks/skill-track/skill-track.js
+++ blocks/skill-track/skill-track.js
@@ -3,16 +3,10 @@
  * @param {Element} block The skill track block element
  */
 export default function decorate(block) {
-  // Add container class
-  block.classList.add('skill-track');
-
   // Get all rows
   const rows = [...block.children];
 
-  // Process each lesson row
   rows.forEach((row) => {
-
-    row.textContent = '';
-
+    block.classList.add('skill-track-step');
   });
 }
