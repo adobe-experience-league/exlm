c6819e1b35920d25f8c6e4458c0930717f33ea82
GhnanaPrasoona <gyesireddy+adobe@adobe.com>
2025-08-05 14:39:22 +0530

    skill-track authoring

--- blocks/skill-track/skill-track.js
+++ blocks/skill-track/skill-track.js
@@ -1,5 +1,3 @@
-import { generateLessonDOM } from '../lesson/lesson.js';
-
 /**
  * Decorates the skill track block
  * @param {Element} block The skill track block element
@@ -13,15 +11,8 @@ export default function decorate(block) {
 
   // Process each lesson row
   rows.forEach((row) => {
-    row.dataset.blockName = 'lesson';
-    row.dataset.blockModel = 'lesson';
-    row.classList.add('lesson');
-
-    // Generate the lesson DOM
-    const lessonDOM = generateLessonDOM(row);
 
     row.textContent = '';
 
-    row.appendChild(lessonDOM);
   });
 }
