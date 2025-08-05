a0f20a17e55e6a82c69c18f9b2840d81d83352be
GhnanaPrasoona <gyesireddy+adobe@adobe.com>
2025-08-05 17:43:56 +0530

    skill-track authoring

--- blocks/skill-track-meta/skill-track-meta.js
+++ blocks/skill-track-meta/skill-track-meta.js
@@ -3,9 +3,9 @@ export function generateSkillTrackHeaderDOM(block) {
 
   Array.from(block.children).forEach((element, i) => {
     const div = document.createElement('div');
-    div.className = i === 0 ? 'skill-track-title' : 'skill-track-description';
 
     if (i === 0) {
+      div.className = 'skill-track-title';
       const heading = element.querySelector('h1,h2,h3,h4,h5,h6');
       div.append(heading || element.textContent.trim());
     } else {
