5f02599c029b4177529b6c51fe92653d0804a35b
Sreeksha Akkala <sakkala@adobe.com>
2025-09-12 20:40:22 +0530

    ios fix

--- blocks/flip-card/flip-card.js
+++ blocks/flip-card/flip-card.js
@@ -51,12 +51,6 @@ export default async function decorate(block) {
 
     const cardDivs = Array.from(card.children);
 
-    // New structure:
-    // cardDivs[0]: Front title with tag (e.g., <h3 id="front-title">Front Title</h3>)
-    // cardDivs[1]: Back title with tag (e.g., <h3 id="back-title">Back Title</h3>)
-    // cardDivs[2]: Front content
-    // cardDivs[3]: Back content
-
     // Using object destructuring for child elements
     const [frontTitleDiv, backTitleDiv, frontContentDiv, backContentDiv] = cardDivs;
 
@@ -87,11 +81,19 @@ export default async function decorate(block) {
     card.innerHTML = `
       <div class="${!hasFrontTitle && hasFrontContent ? 'content-only' : ''}">
         ${frontTitleDiv ? frontTitleDiv.innerHTML : ''}
-        ${frontContentDiv ? `<div class="flip-card-content">${frontContentDiv.innerHTML}</div>` : ''}
+        ${
+          frontContentDiv
+            ? `<div class="flip-card-content-wrapper"><div class="flip-card-content">${frontContentDiv.innerHTML}</div></div>`
+            : ''
+        }
       </div>
       <div class="${!hasBackTitle && hasBackContent ? 'content-only' : ''}">
         ${backTitleDiv ? backTitleDiv.innerHTML : ''}
-        ${backContentDiv ? `<div class="flip-card-content">${backContentDiv.innerHTML}</div>` : ''}
+        ${
+          backContentDiv
+            ? `<div class="flip-card-content-wrapper"><div class="flip-card-content">${backContentDiv.innerHTML}</div></div>`
+            : ''
+        }
       </div>
     `;
 
