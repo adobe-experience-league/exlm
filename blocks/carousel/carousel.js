8d0b78b2adf4d5315d264d10ee5566f6cbe488b2
Ruhi Singh <ruhis@adobe.com>
2025-10-29 18:57:53 +0530

    EXLM-4186 Fix for detailed teaser inside Carousel

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -36,7 +36,7 @@ export default function decorate(block) {
   // loop through all children blocks
   [...panels].forEach((panel, i) => {
     // generate the  panel
-    let [image, classList, ...rest] = panel.children;
+    const [image, classList, ...rest] = panel.children;
     const classesText = classList.textContent.trim();
     const classes = (classesText ? classesText.split(',') : []).map((c) => c && c.trim()).filter((c) => !!c);
     const blockType = [...classes].includes('detailed-teaser') ? 'detailed-teaser' : 'teaser';
@@ -45,9 +45,7 @@ export default function decorate(block) {
       const bgColor = `var(--${bgColorCls.substr(3)})`;
       panel.style.backgroundColor = bgColor;
     }
-    if (blockType === 'detailed-teaser') {
-      [image, classList, , ...rest] = panel.children;
-    }
+
     // check if we have to render teaser or a detailed teaser
     const teaserDOM =
       blockType === 'detailed-teaser'
