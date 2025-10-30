2e76adbaf5cdaddadb706fae5d97cb58688b47fc
Ruhi Singh <ruhis@adobe.com>
2025-10-30 12:45:54 +0530

    EXLM-3916 Placeholder text for UE mode

--- blocks/inprogress-courses/inprogress-courses.js
+++ blocks/inprogress-courses/inprogress-courses.js
@@ -5,7 +5,8 @@ import { createTag, fetchLanguagePlaceholders } from '../../scripts/scripts.js';
 const DEFAULT_NUM_COURSES = 2;
 const MAX_NUM_COURSES = 4;
 
-export default function build(block) {
+export default function decorate(block) {
+  const UEAuthorMode = window.hlx.aemRoot || window.location.href.includes('.html');
   const renderCourses = (contentDiv, courses, limit) => {
     const fragment = document.createDocumentFragment();
     contentDiv.innerHTML = '';
@@ -46,55 +47,58 @@ export default function build(block) {
 
     renderCourses(contentDiv, cardModels, visibleCourses);
   };
+  if (UEAuthorMode) {
+    block.innerHTML = `<div><h3>Keep up the Good work, firstName.</h3></div><div class="inprogress-courses-card-wrapper">This block will load the in progress courses from the profile.</div><div></div>`;
+  } else {
+    isSignedInUser().then(async (isUserSignedIn) => {
+      if (!isUserSignedIn) {
+        return;
+      }
+      const { getCurrentCourses, COURSE_STATUS } = await import('../../scripts/courses/course-profile.js');
+      Promise.all([getCurrentCourses(), defaultProfileClient.getMergedProfile()]).then(
+        async ([profileCourses, profileResult]) => {
+          const courseIdentifiers = Object.keys(profileCourses);
+          if (!courseIdentifiers?.length) {
+            block.classList.add('inprogress-courses-hidden');
+            return;
+          }
+          const [
+            { transformCourseMetaToCardModel, fetchCourseIndex },
+            { default: BrowseCardsCourseEnricher },
+            placeholders,
+          ] = await Promise.all([
+            import('../../scripts/courses/course-utils.js'),
+            import('../../scripts/browse-card/browse-cards-course-enricher.js'),
+            fetchLanguagePlaceholders(),
+          ]);
+          const firstName = profileResult?.first_name;
+          const headerText = placeholders?.inprogressCoursesHeader || 'Keep up the Good work';
+          block.innerHTML = `<div><h3>${headerText}, ${firstName}.</h3></div><div class="inprogress-courses-card-wrapper"></div><div></div>`;
+          const lang = document.querySelector('html').lang || 'en';
+          const allCourses = await fetchCourseIndex(lang);
+          const courseIds = courseIdentifiers.map((id) => `/${lang}/${id}`);
+          const filteredCourses = allCourses.filter((course) => courseIds.includes(course.path));
 
-  isSignedInUser().then(async (isUserSignedIn) => {
-    if (!isUserSignedIn) {
-      return;
-    }
-    const { getCurrentCourses, COURSE_STATUS } = await import('../../scripts/courses/course-profile.js');
-    Promise.all([getCurrentCourses(), defaultProfileClient.getMergedProfile()]).then(
-      async ([profileCourses, profileResult]) => {
-        const courseIdentifiers = Object.keys(profileCourses);
-        if (!courseIdentifiers?.length) {
-          block.classList.add('inprogress-courses-hidden');
-          return;
-        }
-        const [
-          { transformCourseMetaToCardModel, fetchCourseIndex },
-          { default: BrowseCardsCourseEnricher },
-          placeholders,
-        ] = await Promise.all([
-          import('../../scripts/courses/course-utils.js'),
-          import('../../scripts/browse-card/browse-cards-course-enricher.js'),
-          fetchLanguagePlaceholders(),
-        ]);
-        const firstName = profileResult?.first_name;
-        const headerText = placeholders?.inprogressCoursesHeader || 'Keep up the Good work';
-        block.innerHTML = `<div><h3>${headerText}, ${firstName}.</h3></div><div class="inprogress-courses-card-wrapper"></div><div></div>`;
-        const lang = document.querySelector('html').lang || 'en';
-        const allCourses = await fetchCourseIndex(lang);
-        const courseIds = courseIdentifiers.map((id) => `/${lang}/${id}`);
-        const filteredCourses = allCourses.filter((course) => courseIds.includes(course.path));
+          const cardModels = filteredCourses.map((model) => {
+            const path = model.path || '';
+            const [, id] = path.split(`/${lang}/`);
+            const course = profileCourses[id];
+            return transformCourseMetaToCardModel({ model, course, placeholders });
+          });
+          const courses = BrowseCardsCourseEnricher.enrichCardsWithCourseStatus(cardModels, profileCourses);
+          const inProgressCourses = courses
+            .filter((course) => course.meta?.courseInfo?.courseStatus === COURSE_STATUS.IN_PROGRESS)
+            .slice(0, MAX_NUM_COURSES);
+          if (inProgressCourses.length === 0) {
+            block.classList.add('inprogress-courses-hidden');
+            return;
+          }
 
-        const cardModels = filteredCourses.map((model) => {
-          const path = model.path || '';
-          const [, id] = path.split(`/${lang}/`);
-          const course = profileCourses[id];
-          return transformCourseMetaToCardModel({ model, course, placeholders });
-        });
-        const courses = BrowseCardsCourseEnricher.enrichCardsWithCourseStatus(cardModels, profileCourses);
-        const inProgressCourses = courses
-          .filter((course) => course.meta?.courseInfo?.courseStatus === COURSE_STATUS.IN_PROGRESS)
-          .slice(0, MAX_NUM_COURSES);
-        if (inProgressCourses.length === 0) {
-          block.classList.add('inprogress-courses-hidden');
-          return;
-        }
-
-        const [headerWrappper, contentDiv, ctaSectionWrapper] = Array.from(block.children);
-        headerWrappper.classList.add('inprogress-courses-header-wrapper');
-        setupCoursesUI(ctaSectionWrapper, inProgressCourses, contentDiv, placeholders);
-      },
-    );
-  });
+          const [headerWrappper, contentDiv, ctaSectionWrapper] = Array.from(block.children);
+          headerWrappper.classList.add('inprogress-courses-header-wrapper');
+          setupCoursesUI(ctaSectionWrapper, inProgressCourses, contentDiv, placeholders);
+        },
+      );
+    });
+  }
 }
