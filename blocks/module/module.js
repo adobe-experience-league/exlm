1f7aad09334810fb5e8ab9e415e4cb5e188f21bc
gauthamksn <gsnair@adobe.com>
2025-11-10 15:32:44 +0530

    redirect users from module meta

--- blocks/module/module.js
+++ blocks/module/module.js
@@ -1,9 +1,4 @@
-/**
- * Decorates the module block
- * @param {Element} block The module block element
- */
 export default function decorate(block) {
-  // Get all rows
   const rows = [...block.children];
 
   rows.forEach(() => {
