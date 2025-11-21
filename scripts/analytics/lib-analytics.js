61439ccfa9e5c171cb837966ac5058876f24164a
GhnanaPrasoona <gyesireddy@adobe.com>
2025-11-21 11:17:44 +0530

    Distinguish between the Grid vs List card styles in Events Hub Page

--- scripts/analytics/lib-analytics.js
+++ scripts/analytics/lib-analytics.js
@@ -903,7 +903,7 @@ export function pushBrowseCardClickEvent(eventName, cardData, cardHeader, cardPo
   const cardSolution = Array.isArray(product) ? product[0] : product?.split(',')[0]?.trim() || '';
 
   // Determining if the card is in list or grid view
-  const eventsBlock = document.querySelector('.upcoming-event-block');
+  const eventsBlock = document.querySelector('.upcoming-event-v2, .upcoming-event-block');
   const viewType = eventsBlock?.classList.contains('list') ? 'List' : 'Grid';
 
   const dataLayerEntry = {
