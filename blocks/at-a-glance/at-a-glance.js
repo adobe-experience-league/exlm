c71a0b59f96a24d89f158857f96013f29447d315
priyanshij2 <priyanshij@adobe.com>
2025-12-09 23:17:24 +0530

    main+block laout and anchor tag functionality

---
+++ blocks/at-a-glance/at-a-glance.js
@@ -0,0 +1,42 @@
+function generateFilteredEventURL(filterType, label) {
+  const baseURL = 'https://experienceleague-stage.adobe.com/en/test-folder/events';
+  const filterMap = {
+    product: 'f-el_product',
+    series: 'f-el_event_series',
+  };
+  
+  const key = filterType?.toLowerCase();
+  const mappedFilter = filterMap[key];
+
+  if (!mappedFilter) {
+    return baseURL;
+  }
+  return `${baseURL}#${mappedFilter}=${encodeURIComponent(label)}`;
+}
+
+export default function decorate(block) {
+  const pTags = block.querySelectorAll('.at-a-glance-wrapper p');
+
+  pTags.forEach((p) => {
+    const text = p.textContent;
+    const [label, values] = text.split(':');
+
+    if (!values) return;
+
+    const items = values.split(',').map((v) => v.trim());
+
+    // Clear existing <p> content
+    p.textContent = `${label}: `;
+
+    items.forEach((value, index) => {
+      const a = document.createElement('a');
+      a.textContent = value;
+      a.href = generateFilteredEventURL(label, value);
+      a.target = '_blank';
+      p.appendChild(a);
+      if (index < items.length - 1) {
+        p.appendChild(document.createTextNode(' '));
+      }
+    });
+  });
+}
