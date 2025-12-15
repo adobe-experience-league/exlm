b5e3c7c4148f2e60c22413a3766686e89c330ef6
Ruhi Singh <ruhis@adobe.com>
2025-12-15 16:22:48 +0530

    EXLM-4452 Temporary solution to update the IMS configuration to Prod for Premium Learning site in Dev

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -755,22 +755,26 @@ export function getConfig() {
   );
   const cdnHost = currentEnv?.cdn || defaultEnv.cdn;
   const communityHost = currentEnv?.community || defaultEnv.community;
-  const cdnOrigin = `https://${cdnHost}`;
+  let cdnOrigin = `https://${cdnHost}`;
   const lang = document.querySelector('html').lang || 'en';
   // Locale param for Community page URL
   const communityLocale = communityLangsMap.get(lang) || 'en';
   // Lang param for Adobe account URL
   const adobeAccountLang = adobeAccountLangsMap.get(lang) || 'en';
   const prodAssetsCdnOrigin = 'https://cdn.experienceleague.adobe.com';
-  const isProd = currentEnv?.env === 'PROD' || currentEnv?.authorUrl === 'author-p122525-e1219150.adobeaemcloud.com';
+  let isProd = currentEnv?.env === 'PROD' || currentEnv?.authorUrl === 'author-p122525-e1219150.adobeaemcloud.com';
   const isStage = currentEnv?.env === 'STAGE' || currentEnv?.authorUrl === 'author-p122525-e1219192.adobeaemcloud.com';
   // EXLM-4452 - Temporary solution to update the IMS configuration to Prod for Premium Learning site in the Dev environment.
   const urlParams = new URLSearchParams(window.location.search);
   const enableProdIms = !isProd && !isStage && urlParams.get('ims') === 'prod';
-  const ppsOrigin = isProd || enableProdIms ? 'https://pps.adobe.io' : 'https://pps-stage.adobe.io';
+  if (enableProdIms) {
+    isProd = true;
+    cdnOrigin = `https://experienceleague.adobe.com`;
+  }
+  const ppsOrigin = isProd ? 'https://pps.adobe.io' : 'https://pps-stage.adobe.io';
   const ims = {
     client_id: 'ExperienceLeague',
-    environment: isProd || enableProdIms ? 'prod' : 'stg1',
+    environment: isProd ? 'prod' : 'stg1',
     debug: !isProd,
   };
 
