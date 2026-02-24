7a569571cb0a7a3014ac27603022f1d979b1b9cb
gauthamksn <gsnair@adobe.com>
2026-02-24 16:29:30 +0530

    variation 1

---
+++ blocks/media-panel/media-panel.js
@@ -0,0 +1,33 @@
+export default function decorate(block) {
+    const [
+        image,
+        eyebrow,
+        heading,
+        description,
+        cta,
+        highlightHeading,
+        highlightDescription
+    ] = block.children || [];
+
+    image.classList.add('image');
+    eyebrow.classList.add('eyebrow');
+    heading.classList.add('heading');
+    description.classList.add('description');
+    cta.classList.add('cta');
+    highlightHeading.classList.add('highlight-heading');
+    highlightDescription.classList.add('highlight-description');
+
+    if (block.classList.contains('left') && block.classList.contains('right')) {
+        block.classList.remove('left');
+    }
+
+    const highlightContainer = document.createElement('div');
+    highlightContainer.classList.add('highlight-container');
+    block.append(highlightContainer);
+    highlightContainer.append(image, highlightHeading, highlightDescription);
+
+    const contentContainer = document.createElement('div');
+    contentContainer.classList.add('content-container');
+    block.append(contentContainer);
+    contentContainer.append(eyebrow, heading, description, cta);
+}
