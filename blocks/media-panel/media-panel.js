7e5ffe86e137971cb4acf636d49d9ac601f41cf0
gauthamksn <gsnair@adobe.com>
2026-02-26 10:42:37 +0530

    fix formating

--- blocks/media-panel/media-panel.js
+++ blocks/media-panel/media-panel.js
@@ -1,34 +1,26 @@
 export default function decorate(block) {
-    const [
-        image,
-        eyebrow,
-        heading,
-        description,
-        cta,
-        podcastHeading,
-        podcastDescription
-    ] = block.children || [];
+  const [image, eyebrow, heading, description, cta, podcastHeading, podcastDescription] = block.children || [];
 
-    image.classList.add('image');
-    eyebrow.classList.add('eyebrow');
-    heading.classList.add('heading');
-    description.classList.add('description');
-    cta.classList.add('cta');
-    podcastHeading.classList.add('podcast-heading');
-    podcastDescription.classList.add('podcast-description');
+  image.classList.add('image');
+  eyebrow.classList.add('eyebrow');
+  heading.classList.add('heading');
+  description.classList.add('description');
+  cta.classList.add('cta');
+  podcastHeading.classList.add('podcast-heading');
+  podcastDescription.classList.add('podcast-description');
 
-    const podcastContainer = document.createElement('div');
-    podcastContainer.classList.add('podcast-container');
-    block.append(podcastContainer);
-    podcastContainer.append(image, podcastHeading, podcastDescription);
+  const podcastContainer = document.createElement('div');
+  podcastContainer.classList.add('podcast-container');
+  block.append(podcastContainer);
+  podcastContainer.append(image, podcastHeading, podcastDescription);
 
-    const contentContainer = document.createElement('div');
-    contentContainer.classList.add('content-container');
-    block.append(contentContainer);
-    contentContainer.append(eyebrow, heading, description, cta);
+  const contentContainer = document.createElement('div');
+  contentContainer.classList.add('content-container');
+  block.append(contentContainer);
+  contentContainer.append(eyebrow, heading, description, cta);
 
-    if (!block.classList.contains('podcast-card')) {
-        podcastHeading.remove();
-        podcastDescription.remove();
-    }
+  if (!block.classList.contains('podcast-card')) {
+    podcastHeading.remove();
+    podcastDescription.remove();
+  }
 }
