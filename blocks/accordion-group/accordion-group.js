4f3aca643e68760354b8559acd5e1609483ab3e6
Nitin Venkat Rachabathuni <reachnitinvenkat@gmail.com>
2026-03-03 12:51:49 +0530

    feat: accordion block level changes EXLM-4688

--- blocks/accordion-group/accordion-group.js
+++ blocks/accordion-group/accordion-group.js
@@ -1,4 +1,17 @@
-import { generateAccordionDOM } from '../accordion/accordion.js';
+function generateAccordionDOM(block) {
+  const details = document.createElement('details');
+  const summary = document.createElement('summary');
+  details.append(summary);
+  Array.from(block.children).forEach((element, i) => {
+    if (i === 0) {
+      const heading = element.querySelector('h2,h3,h4,h5,h6');
+      summary.append(heading || element.textContent.trim());
+    } else {
+      details.append(element);
+    }
+  });
+  return details;
+}
 
 export default function decorate(block) {
   // each row is an accordion entry
@@ -10,8 +23,7 @@ export default function decorate(block) {
     const accordionDOM = generateAccordionDOM(accordion);
     // empty the content ,keep root element with UE instrumentation
     accordion.textContent = '';
-    // add block classes
-    accordion.classList.add('accordion', 'block');
+    accordion.classList.add('accordion');
     accordion.append(accordionDOM);
   });
 
