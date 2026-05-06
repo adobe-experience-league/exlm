79fc39f654a4c5dfb83d6e669fd38c0ed0727522
Ruhi Singh <ruhis@adobe.com>
2026-05-06 20:45:43 +0530

    LOC tags

--- blocks/tags/tags.js
+++ blocks/tags/tags.js
@@ -2,8 +2,8 @@ import { fetchLanguagePlaceholders } from '../../scripts/scripts.js';
 import { getMetadata } from '../../scripts/lib-franklin.js';
 import isFeatureEnabled from '../../scripts/utils/feature-flag-utils.js';
 
-function getPreferredMetadata(tqMetaKey, locLegacyMetaKey, legacyMetaKey) {
-  return getMetadata(tqMetaKey) || getMetadata(locLegacyMetaKey) || getMetadata(legacyMetaKey);
+function getPreferredMetadata(...keys) {
+  return keys.map(getMetadata).find(Boolean);
 }
 
 export default async function decorate(block) {
@@ -46,9 +46,9 @@ export default async function decorate(block) {
         ),
       ].join(',') || getMetadata('product_v2');
 
-    features = getPreferredMetadata('loc-legacy-feature', 'feature', 'feature_v2');
-    roles = getPreferredMetadata('loc-legacy-role', 'role', 'role_v2');
-    experienceLevels = getPreferredMetadata('loc-legacy-level', 'level', 'level_v2');
+    features = getPreferredMetadata('loc-legacy-feature', 'loc-feature', 'feature', 'feature_v2');
+    roles = getPreferredMetadata('loc-legacy-role', 'loc-role', 'role', 'role_v2');
+    experienceLevels = getPreferredMetadata('loc-legacy-level', 'loc-level', 'level', 'level_v2');
   }
 
   function createTagsHTML(values) {
